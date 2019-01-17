6a84bd976ad0911f28b7c98e3d04db48cb41d685
Stephen Inglis <si5@sanger.ac.uk>
2019-01-17 14:18:47 +0000

    Rubocopped. Added travis releases.

--- app/controllers/v1/samples_controller.rb
+++ app/controllers/v1/samples_controller.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 module V1
+  # SamplesController
   class SamplesController < ApplicationController
     def create
       @sample_factory = SampleFactory.new(params_names)
       if @sample_factory.save
         @sample_resources = @sample_factory.samples.map { |sample| SampleResource.new(sample, nil) }
-        render json: JSONAPI::ResourceSerializer.new(SampleResource).serialize_to_hash(@sample_resources), status: :created
+        render json:
+          JSONAPI::ResourceSerializer.new(SampleResource).serialize_to_hash(@sample_resources),
+               status: :created
       else
-        render json: { errors: @sample_factory.errors.messages}, status: :unprocessable_entity
+        render json: { errors: @sample_factory.errors.messages }, status: :unprocessable_entity
       end
     end
 
-    # def index
-    #   render json:  JSONAPI::ResourceSerializer.new(SampleResource).serialize_to_hash(Sample.all)
-    # end
-
     def params_names
-      params.require(:data).require(:attributes)[:samples].map{|param| param.permit(:sequencescape_request_id, :name, :state, :species).to_h}
+      params.require(:data).require(:attributes)[:samples].map do |param|
+        param.permit(:sequencescape_request_id, :name, :state, :species).to_h
+      end
     end
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :v1 do
-    jsonapi_resources :samples, only: [:create, :index]
+    jsonapi_resources :samples, only: %i[create index]
   end
 end
