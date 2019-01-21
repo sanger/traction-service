c00d33724c6180579059ea6ea0ebab459d2d6260
Harriet Craven <hc6@sanger.ac.uk>
2019-01-21 16:42:04 +0000

    Rubocopped - apart from the 3 remaining as not sure what to do

--- app/controllers/v1/libraries_controller.rb
+++ app/controllers/v1/libraries_controller.rb
@@ -1,19 +1,23 @@
-module V1
-  class LibrariesController < ApplicationController
+# frozen_string_literal: true
 
+module V1
+  # LibrariesController
+  class LibrariesController < ApplicationController
     def create
       @library_factory = LibraryFactory.new(params_names)
       if @library_factory.save
-        @library_resources = @library_factory.libraries.map { |lib| LibraryResource.new(lib, nil)}
-        render json: JSONAPI::ResourceSerializer.new(LibraryResource).serialize_to_hash(@library_resources), status: :created
+        @resources = @library_factory.libraries.map { |lib| LibraryResource.new(lib, nil) }
+        body = JSONAPI::ResourceSerializer.new(LibraryResource).serialize_to_hash(@resources)
+        render json: body, status: :created
       else
-        render json: { errors: @library_factory.errors.messages}, status: :unprocessable_entity
+        render json: { errors: @library_factory.errors.messages }, status: :unprocessable_entity
       end
     end
 
     def params_names
-      params.require(:data).require(:attributes)[:libraries].map { |param| param.permit(:state, :sample_id).to_h }
+      params.require(:data).require(:attributes)[:libraries].map do |param|
+        param.permit(:state, :sample_id).to_h
+      end
     end
-
   end
 end
--- app/models/library.rb
+++ app/models/library.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Library
 class Library < ApplicationRecord
   include Material
 
