0cd342c86f646b2f2a221ca6364e5b1c061f0a56
Harriet Craven <hc6@sanger.ac.uk>
2019-01-23 14:05:28 +0000

    Added ability to soft delete/ deactivate libraries

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,6 +13,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
              headers: :any,
-             methods: %i[get post put patch options]
+             methods: %i[get post put patch options delete]
   end
 end
