6d7fe39a211fca09a4e3109810d1d56a91e577ad
Stephen Inglis <si5@sanger.ac.uk>
2019-02-11 14:46:45 +0000

    Don't run seeds when in test environment.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,8 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
 # Adding enzymes to the database
-["Nb.BbvCI", "Nb.BsmI", "Nb.BsrDI", "Nt.BspQI", "Nb.BssSI", "DLE-1"].each do |enzyme_name|
-    Enzyme.create(name: enzyme_name)
+unless Rails.env.test?
+  ["Nb.BbvCI", "Nb.BsmI", "Nb.BsrDI", "Nt.BspQI", "Nb.BssSI", "DLE-1"].each do |enzyme_name|
+      Enzyme.create(name: enzyme_name)
+  end
 end
