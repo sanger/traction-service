fd065ce8c0b6b0b5467b30f42012ef2bd8589d4c
Harriet Craven <hc6@sanger.ac.uk>
2019-02-14 10:33:25 +0000

    Finish create request for runs - also creates chip with two flowcells

--- spec/models/run_factory_spec.rb
+++ spec/models/run_factory_spec.rb
@@ -9,18 +9,18 @@ RSpec.describe RunFactory, type: :model do
       factory = RunFactory.new(attributes)
       expect(factory.runs.count).to eq(1)
       expect(factory.runs[0].chip).to be_present
-      expect(factory.runs[0].chip.flowcells.length).to eq 2
     end
 
   end
 
   context '#save' do
-    it 'creates a run for each set of attributes' do
+    it 'creates a run, with a chip and two flowcells for each set of attributes' do
       factory = RunFactory.new(attributes)
       expect(factory).to be_valid
       expect(factory.save).to be_truthy
       expect(Run.all.count).to eq(attributes.length)
       expect(Run.first.chip.run_id).to eq Run.first.id
+      expect(Run.first.chip.flowcells.length).to eq 2
     end
 
   end
--- spec/models/run_spec.rb
+++ spec/models/run_spec.rb
@@ -92,18 +92,16 @@ RSpec.describe Run, type: :model do
   end
 
   context 'run relationships' do
-    it 'can have a chip with two flowcells each with a library' do
+    it 'can have a chip with two flowcells' do
       run = create(:run)
       chip = create(:chip, run: run)
-      flowcell1 = create(:flowcell, chip: chip, position: 1)
-      flowcell2 = create(:flowcell, chip: chip, position: 2)
-      library1 = create(:library, flowcell: flowcell1)
-      library2 = create(:library, flowcell: flowcell2)
 
       expect(run.chip).to eq chip
-      expect(run.chip.flowcells).to eq [flowcell1, flowcell2]
-      expect(run.chip.flowcells[0].library).to eq(library1)
-      expect(run.chip.flowcells[1].library).to eq(library2)
+      expect(run.chip.flowcells.length).to eq 2
+      expect(run.chip.flowcells[0].position).to eq(1)
+      expect(run.chip.flowcells[0].chip).to eq(chip)
+      expect(run.chip.flowcells[1].position).to eq(2)
+      expect(run.chip.flowcells[1].chip).to eq(chip)
     end
   end
 
