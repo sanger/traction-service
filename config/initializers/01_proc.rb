89ff948d2267eb1355b0f84763fdf4f9046b3605
Stephen Inglis <si5@sanger.ac.uk>
2019-02-27 08:23:03 +0000

    Added dependent loader which will load enzymes when table is empty. Added a rake task for creating dummy runs. Rationalised migrations Fixes #15.

--- config/initializers/01_proc.rb
+++ config/initializers/01_proc.rb
@@ -1,11 +1,21 @@
-#www.mattsears.com/articles/2011/11/27ruby-blocks-as-dynamic-callbacks
+# frozen_string_literal: true
+
+# www.mattsears.com/articles/2011/11/27ruby-blocks-as-dynamic-callbacks
+# rubocop:disable Style/CaseEquality, Style/MethodMissingSuper
 class Proc
   def callback(callable, *args)
     self === Class.new do
       method_name = callable.to_sym
       define_method(method_name) { |&block| block ? block.call(*args) : true }
       define_method("#{method_name}?") { true }
-      def method_missing(*args, &block) false; end
+      def method_missing(*_args)
+        false
+      end
+
+      def respond_to_missing?(*)
+        true
+      end
     end.new
   end
 end
+# rubocop:enable Style/CaseEquality, Style/MethodMissingSuper
--- config/initializers/create_enzymes.rb
+++ config/initializers/create_enzymes.rb
@@ -1,19 +1,18 @@
-require File.join(Rails.root, 'lib', 'dependent_loader')
+# frozen_string_literal: true
+
+require Rails.root.join('lib', 'dependent_loader')
 
 DependentLoader.start(:enzymes) do |on|
-
   on.success do
-
     Enzyme.create(
       [
-        { name: "Nb.BbvCI"},
-        { name: "Nb.BsmI"},
-        { name: "Nb.BsrDI"},
-        { name: "Nt.BspQI"},
-        { name: "Nb.BssSI"},
-        { name: "DLE-1"}
+        { name: 'Nb.BbvCI' },
+        { name: 'Nb.BsmI' },
+        { name: 'Nb.BsrDI' },
+        { name: 'Nt.BspQI' },
+        { name: 'Nb.BssSI' },
+        { name: 'DLE-1' }
       ]
     )
   end
-
 end
