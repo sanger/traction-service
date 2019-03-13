49d02534af960e2b2f304baa0e3955b805870511
Harriet Craven <hc6@sanger.ac.uk>
2019-03-13 10:50:11 +0000

    Add deactivated at to sample and lirbary resource

--- app/resources/v1/sample_resource.rb
+++ app/resources/v1/sample_resource.rb
@@ -3,7 +3,7 @@
 module V1
   # SampleResource
   class SampleResource < JSONAPI::Resource
-    attributes :name, :sequencescape_request_id, :species, :barcode, :created_at
+    attributes :name, :sequencescape_request_id, :species, :barcode, :created_at, :deactivated_at
     has_many :libraries, always_include_linkage_data: true
     has_one :tube
 
@@ -13,6 +13,10 @@ module V1
 
     def created_at
       @model.created_at.strftime('%m/%d/%Y %I:%M')
+    end
+
+    def deactivated_at
+      @model&.deactivated_at&.strftime('%m/%d/%Y %I:%M')
     end
   end
 end
