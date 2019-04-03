8faa3f8597ed1d9d35353c32fba1ff20c0a15254
Stephen Inglis <si5@sanger.ac.uk>
2019-04-03 15:44:56 +0100

    A dded serial number to chip updated from barcode. Made chip barcode mandatory of at least length 16. Added study id to sample and made it required.

--- app/models/run_factory.rb
+++ app/models/run_factory.rb
@@ -4,8 +4,9 @@
 class RunFactory
   include ActiveModel::Model
 
+  #TODO: the barcode will need to be removed once the ui is refactored to build a new run with validation
   def initialize(attributes = [])
-    attributes.each { |run| runs << Run.new(run.merge!(chip: Chip.new)) }
+    attributes.each { |run| runs << Run.new(run.merge!(chip: Chip.new(barcode: 'FLEVEAOLPTOWPNWU20319131581014320190911XXXXXXXXXXXXX'))) }
   end
 
   def runs
