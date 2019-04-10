52d5e9f3727ea99e3832037da2f7c288f996ac47
Stephen Inglis <si5@sanger.ac.uk>
2019-04-10 14:55:44 +0100

    Moved message to broker.

--- config/initializers/broker_initializer.rb
+++ config/initializers/broker_initializer.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
-require 'broker'
-
 if Rails.configuration.bunny['enabled']
-  BrokerHandle = Broker.new(Rails.configuration.bunny)
+  BrokerHandle = Messages::Broker.new(Rails.configuration.bunny)
   BrokerHandle.create_connection
 else
   # Create an "empty" class definition when bunny are disabled
-  BrokerHandle = Broker.new(Rails.configuration.bunny).tap do |obj|
+  BrokerHandle = Messages::Broker.new(Rails.configuration.bunny).tap do |obj|
     obj.instance_eval do
     end
   end
