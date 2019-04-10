c068f8c603607b4899f4b3afdb39bd298a48384c
Stephen Inglis <si5@sanger.ac.uk>
2019-04-10 14:31:56 +0100

    Added message.

--- app/messages/messages/message.rb
+++ app/messages/messages/message.rb
@@ -1,20 +1,31 @@
-# frozen_string_literal: true
-
 module Messages
-  # This class is responsible for building messages to go to RabbitMQ
   class Message
-    attr_accessor :run
+    include ActiveModel::Model 
 
-    def initialize(run)
-      @run = run
+    attr_accessor :object, :configuration
+
+    def timestamp
+      Time.current
     end
 
-    def generate_json
-      {
-        'id' => @run.id,
-        'name' => @run.name,
-        'chip_barcode' => @run&.chip&.barcode
-      }.to_json
+    def content
+      {}.tap do |result|
+        result[configuration.key] = configuration.fields.each_with_object({}) do |(k,v), result|
+          result[k] = instance_value(v)
+        end
+        result[configuration.key][:updated_at] = timestamp
+        result[:lims] = configuration.lims
+      end 
+    end
+
+    def payload
+      content.to_json
+    end
+
+    private
+
+    def instance_value(chain)
+      chain.split('.').inject(object, :send)
     end
   end
 end
