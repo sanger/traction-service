d4d017a280de1c7a8b533d2db155fa49fc91994e
Stephen Inglis <si5@sanger.ac.uk>
2019-04-10 14:31:56 +0100

    Added message.

--- spec/messages/message_spec.rb
+++ spec/messages/message_spec.rb
@@ -1,24 +1,75 @@
-# frozen_string_literal: true
+require "rails_helper"
 
-require 'rails_helper'
+class ObjectB
+  attr_reader :attr_d
 
-RSpec.describe Messages::Message do
-  let(:run) { create(:run_with_chip) }
-  let(:msg) {  Messages::Message.new(run) }
+  def initialize(attr_d)
+    @attr_d = attr_d
+  end
+end
 
-  describe 'init' do
-    it 'can be initialized with a run' do
-      expect(msg.run).to eq run
-    end
+class ObjectA
+  attr_reader :attr_a, :attr_b, :attr_c
+
+  def initialize(attr_a, attr_b, attr_c)
+    @attr_a = attr_a
+    @attr_b = attr_b
+    @attr_c = attr_c
+  end
+end
+
+class Configuration
+  def key
+    'a_table'
   end
 
-  describe '#generate_json' do
-    
-    it 'creates the json' do
-      json = JSON.parse(msg.generate_json)
-      expect(json['id']).to eq run.id
-      expect(json['name']).to eq run.name
-      expect(json['chip_barcode']).to eq run.chip.barcode
-    end
+  def lims
+    'a_lims'
   end
+
+  def fields
+    {
+      field_a: 'attr_a',
+      field_b: 'attr_b',
+      field_c: 'attr_c.attr_d'
+    }
+  end
+end
+
+RSpec.describe Messages::Message, type: :model do
+
+  let(:object_b) { ObjectB.new('attr_d')}
+  let(:object_a) { ObjectA.new('attr_a', 'attr_b', object_b)}
+  let(:configuration) { Configuration.new }
+  let(:message) { Messages::Message.new(object: object_a, configuration: configuration ) }
+  let(:timestamp) { Time.parse('Mon, 08 Apr 2019 09:15:11 UTC +00:00') }
+
+  before(:each) do
+    allow(Time).to receive(:current).and_return timestamp
+  end
+
+  it 'works' do
+    expect(true).to be_truthy
+  end
+
+  it 'has a timestamp' do
+    expect(message.timestamp).to eq(timestamp)
+  end
+
+  it 'has some content' do
+    expect(message.content).to eq(
+      configuration.key => {
+        field_a: 'attr_a', 
+        field_b: 'attr_b', 
+        field_c: 'attr_d',
+        updated_at: timestamp
+      },
+      lims: configuration.lims
+    )
+  end
+
+  it 'has a payload' do
+    expect(message.payload).to_not be_nil
+  end
+
 end
