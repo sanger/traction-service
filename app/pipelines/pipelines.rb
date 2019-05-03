955f985e7fbe04abca78af9734c3e389f2c3be37
Stephen Inglis <si5@sanger.ac.uk>
2019-05-03 13:56:40 +0100

    pipelines configuration will now be loaded on application startup.

--- app/pipelines/pipelines.rb
+++ app/pipelines/pipelines.rb
@@ -2,4 +2,18 @@
 
 # Pipelines
 module Pipelines
+  # InstanceMethodCreator
+  module InstanceMethodCreator
+    def create_instance_method(key, &block)
+      self.class.send(:define_method, key, block)
+    end
+  end
+
+  def self.configure(pipelines)
+    configuration = Configuration.new(pipelines)
+    configuration.pipelines.each do |pipeline|
+      # TODO: how do I use the create_instance_method method
+      self.class.send(:define_method, pipeline, proc { configuration.send(pipeline) })
+    end
+  end
 end
---
+++ spec/pipelines/pipelines_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe Pipelines, type: :model do
+  let(:params) {
+    {
+      'pipeline_a': {
+        'lims': 'lims_a',
+        'instrument_name': 'bert'
+      },
+      'pipeline_b': {
+        'lims': 'lims_b',
+        'instrument_name': 'ernie'
+      }
+    }
+  }
+
+  before(:each) do
+    Pipelines.configure(params)
+  end
+
+  it 'will create a method for each pipeline' do
+    expect(Pipelines.pipeline_a).to be_present
+    expect(Pipelines.pipeline_a.lims).to eq('lims_a')
+
+    expect(Pipelines.pipeline_b).to be_present
+    expect(Pipelines.pipeline_b.lims).to eq('lims_b')
+  end
+
+
+end
