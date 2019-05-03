203da5ab0eb7f1ba6c0f8f798eba9e439eda4cb1
Stephen Inglis <si5@sanger.ac.uk>
2019-05-03 13:56:40 +0100

    pipelines configuration will now be loaded on application startup.

--- app/pipelines/pipelines/configuration.rb
+++ app/pipelines/pipelines/configuration.rb
@@ -3,13 +3,6 @@
 require 'ostruct'
 
 module Pipelines
-  # InstanceMethodCreator
-  module InstanceMethodCreator
-    def create_instance_method(key, &block)
-      self.class.send(:define_method, key, block)
-    end
-  end
-
   # Configuration
   class Configuration
     include InstanceMethodCreator
@@ -17,7 +10,12 @@ module Pipelines
     def initialize(pipelines)
       pipelines.with_indifferent_access.each do |key, pipeline|
         create_instance_method(key) { Item.new(pipeline) }
+        self.pipelines << key
       end
+    end
+
+    def pipelines
+      @pipelines ||= []
     end
 
     # Configuration::Item
