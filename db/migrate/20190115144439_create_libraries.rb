3bc5ec556f3ca6fe1396b72010de0327b3bade53
Stephen Inglis <si5@sanger.ac.uk>
2019-05-23 08:35:15 +0100

    Fixes #38.

--- db/migrate/20190115144439_create_libraries.rb
+++ db/migrate/20190115144439_create_libraries.rb
@@ -2,8 +2,8 @@ class CreateLibraries < ActiveRecord::Migration[5.2]
   def change
     create_table :libraries do |t|
       t.string :state
-      t.belongs_to :sample, foreign_key: true
-      t.belongs_to :enzyme, foreign_key: true
+      t.belongs_to :sample, index: true
+      t.belongs_to :enzyme, index: true
       t.datetime :deactivated_at
       t.timestamps
     end
