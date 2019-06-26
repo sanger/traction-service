12267f5f1ebae23209b9da635936e6f834a831f5
Stephen Inglis <si5@sanger.ac.uk>
2019-06-26 09:43:27 +0100

    Added request and some of the fields.

---
+++ spec/factories/pacbio/request.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :pacbio_request, class: Pacbio::Request do
+    library_type { 'library_type_1' }
+    estimate_of_gb_required { 100 }
+    number_of_smrt_cells { 3 }
+    cost_code { 'PSD1234' }
+    external_study_id { 1 }
+  end
+end
---
+++ spec/models/pacbio/request_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Pacbio::Request, type: :model, pacbio: true do
+
+  it 'must have a library type' do
+    expect(build(:pacbio_request, library_type: nil)).to_not be_valid
+  end
+
+  it 'must have an estimate of number of gigabases required' do
+    expect(build(:pacbio_request, estimate_of_gb_required: nil)).to_not be_valid
+  end
+
+  it 'must have number of smrt cells' do
+    expect(build(:pacbio_request, number_of_smrt_cells: nil)).to_not be_valid
+  end
+
+  it 'must have a cost code' do
+    expect(build(:pacbio_request, cost_code: nil)).to_not be_valid
+  end
+
+  it 'must have an external study id' do
+    expect(build(:pacbio_request, external_study_id: nil)).to_not be_valid
+  end
+
+end
