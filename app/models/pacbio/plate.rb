741062f01010fb5462482483f3a8c9970a11c0ad
Harriet Craven <hc6@sanger.ac.uk>
2019-07-02 11:16:36 +0100

    Added controller and resource for Pacbio run

--- app/models/pacbio/plate.rb
+++ app/models/pacbio/plate.rb
@@ -3,7 +3,7 @@
 module Pacbio
   # Plate
   class Plate < ApplicationRecord
-    belongs_to :run, class_name: 'Pacbio::Run', foreign_key: :pacbio_run_id,
+    belongs_to :run, foreign_key: :pacbio_run_id,
                      inverse_of: :plate
 
     has_many :wells, class_name: 'Pacbio::Well', foreign_key: :pacbio_plate_id,
