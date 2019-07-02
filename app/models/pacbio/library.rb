b7dedf9ea25c9c8df9893217a37cd7a7b9c1d2e6
Harriet Craven <hc6@sanger.ac.uk>
2019-07-02 16:06:46 +0100

    update pacbio so a library can be in multiple wells

--- app/models/pacbio/library.rb
+++ app/models/pacbio/library.rb
@@ -7,8 +7,9 @@ module Pacbio
 
     belongs_to :tag, class_name: 'Pacbio::Tag', foreign_key: :pacbio_tag_id,
                      optional: false, inverse_of: false
-    belongs_to :well, class_name: 'Pacbio::Well', foreign_key: :pacbio_well_id,
-                      inverse_of: :library, optional: true
+
+    has_many :wells, class_name: 'Pacbio::Well', foreign_key: :pacbio_library_id,
+                     inverse_of: :library, dependent: :nullify
 
     belongs_to :sample
   end
