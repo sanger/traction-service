5baec2433c86c9ec03b6b3e6553edb77424ab3d3
Stephen Inglis <si5@sanger.ac.uk>
2019-07-03 15:17:05 +0100

    A bit of rubocoppery.

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# request
 class Request < ApplicationRecord
   belongs_to :sample
   belongs_to :requestable, polymorphic: true
--- app/models/saphyr/request.rb
+++ app/models/saphyr/request.rb
@@ -1,14 +1,13 @@
-module Saphyr
-  class Request < ApplicationRecord
+# frozen_string_literal: true
 
+module Saphyr
+  # Request
+  class Request < ApplicationRecord
     include Material
 
     validates :external_study_id, presence: true
 
-    belongs_to :sample, optional: true
     belongs_to :request, polymorphic: true, optional: true
-    has_one :request, class_name: '::Request', as: :requestable
-
-    # validates_associated :sample
+    has_one :request, class_name: '::Request', as: :requestable, dependent: :nullify
   end
 end
