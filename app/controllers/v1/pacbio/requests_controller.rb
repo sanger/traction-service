1f51ba6ee188b5582ed1bb383eb420b94b4514fc
Stephen Inglis <si5@sanger.ac.uk>
2019-07-09 09:31:50 +0100

    Abstracted out controller methods for requests.

--- app/controllers/v1/pacbio/requests_controller.rb
+++ app/controllers/v1/pacbio/requests_controller.rb
@@ -4,41 +4,7 @@ module V1
   module Pacbio
     # RequestsController
     class RequestsController < ApplicationController
-      def create
-        @request_factory = ::Pacbio::RequestFactory.new(params_names)
-
-        if @request_factory.save
-
-          @resources = @request_factory.requestables.map do |request|
-            RequestResource.new(request, nil)
-          end
-          body = JSONAPI::ResourceSerializer.new(RequestResource).serialize_to_hash(@resources)
-          render json: body, status: :created
-        else
-          render json: { data: { errors: @request_factory.errors.messages } },
-                 status: :unprocessable_entity
-        end
-      end
-
-      def destroy
-        pipeline_request.destroy
-        head :no_content
-      rescue StandardError => e
-        render json: { data: { errors: e.message } }, status: :unprocessable_entity
-      end
-
-      private
-
-      def pipeline_request
-        @pipeline_request = (params[:id] && ::Pacbio::Request.find_by(id: params[:id]))
-      end
-
-      def params_names
-        params.require(:data).require(:attributes)[:requests].map do |param|
-          param.permit(:library_type, :estimate_of_gb_required, :number_of_smrt_cells, :cost_code,
-                       :external_study_id, :name, :external_id, :species).to_h
-        end
-      end
+      include Pipelines::Requestor::Controller
     end
   end
 end
--- app/controllers/v1/saphyr/requests_controller.rb
+++ app/controllers/v1/saphyr/requests_controller.rb
@@ -4,40 +4,7 @@ module V1
   module Saphyr
     # RequestsController
     class RequestsController < ApplicationController
-      def create
-        @request_factory = ::Saphyr::RequestFactory.new(params_names)
-
-        if @request_factory.save
-
-          @resources = @request_factory.requestables.map do |request|
-            RequestResource.new(request, nil)
-          end
-          body = JSONAPI::ResourceSerializer.new(RequestResource).serialize_to_hash(@resources)
-          render json: body, status: :created
-        else
-          render json: { data: { errors: @request_factory.errors.messages } },
-                 status: :unprocessable_entity
-        end
-      end
-
-      def destroy
-        pipeline_request.destroy
-        head :no_content
-      rescue StandardError => e
-        render json: { data: { errors: e.message } }, status: :unprocessable_entity
-      end
-
-      private
-
-      def pipeline_request
-        @pipeline_request = (params[:id] && ::Saphyr::Request.find_by(id: params[:id]))
-      end
-
-      def params_names
-        params.require(:data).require(:attributes)[:requests].map do |param|
-          param.permit(:external_study_id, :name, :external_id, :species).to_h
-        end
-      end
+      include Pipelines::Requestor::Controller
     end
   end
 end
