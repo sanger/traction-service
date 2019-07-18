92b97e084b8f42984a298396733a8a43029242a6
Stephen Inglis <si5@sanger.ac.uk>
2019-07-18 10:40:42 +0100

    Fixed issues with request resource. Removed create from sample. Removed sample factory. Removed material from sample.

---
+++ app/resources/v1/request_resource.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module V1
+  # MaterialResource
+  class RequestResource < JSONAPI::Resource
+  end
+end
--- spec/factories/tube.rb
+++ spec/factories/tube.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :tube do
-    material { create(:sample) }
+    material { create(:saphyr_request) }
     sequence(:barcode) { |n| "TRAC-#{n}" }
 
     factory :tube_with_saphyr_library do
