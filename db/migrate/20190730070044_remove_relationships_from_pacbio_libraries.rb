2458a563bfa240a5310593fe0e00c36d622ea0c0
Stephen Inglis <si5@sanger.ac.uk>
2019-07-31 07:40:30 +0100

    Modified migrations.

--- db/migrate/20190730070044_remove_relationships_from_pacbio_libraries.rb
+++ db/migrate/20190730070044_remove_relationships_from_pacbio_libraries.rb
@@ -1,9 +1,9 @@
 class RemoveRelationshipsFromPacbioLibraries < ActiveRecord::Migration[5.2]
   def change
-    change_table :pacbio_libraries do |t|
-      t.remove_references :tag
-      t.remove_references :request
-    end
+    # change_table :pacbio_libraries do |t|
+    #   t.remove_references :tag
+    #   t.remove_references :request
+    # end
 
   end
 end
