bbfeacbd12d317d076a66c91b07693ea5f9ea7ba
Stephen Inglis <si5@sanger.ac.uk>
2019-10-29 10:09:57 +0000

    Started to add support for adding multiple wells in controller.

---
+++ app/models/pacbio/well_factory.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+# Pacbio namespace
+module Pacbio
+  # WellFactory
+  # A well factory can create multiple wells
+  # Each of those wells must have a plate
+  # Each well can have multiple libraries which must exist
+  class WellFactory
+    include ActiveModel::Model
+
+    validate :check_wells
+
+    def initialize(attributes = [])
+      build_wells(attributes)
+    end
+
+    def wells
+      @wells ||= []
+    end
+
+    def save
+      return false unless valid?
+
+      wells.collect(&:save)
+      true
+    end
+
+    private
+
+    def build_wells(wells_attributes)
+      wells_attributes.each do |well_attributes|
+        well = Pacbio::Well.new(well_attributes.except(:relationships))
+        well.plate = Pacbio::Plate.find_by(id: well_attributes.dig(:relationships, :plate, :id))
+        libraries = well_attributes.dig(:relationships, :libraries)
+        add_libraries(well, libraries) unless libraries.nil?
+        wells << well
+      end
+    end
+
+    def add_libraries(well, libraries)
+      libraries.each do |library|
+        well.libraries << Pacbio::Library.find(library[:id])
+      end
+    end
+
+    def check_wells
+      if wells.empty?
+        errors.add('libraries', 'there were no libraries')
+        return
+      end
+
+      wells.each do |well|
+        next if well.valid?
+
+        well.errors.each do |k, v|
+          errors.add(k, v)
+        end
+      end
+    end
+  end
+end
---
+++ spec/models/pacbio/well_factory_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Pacbio::WellFactory, type: :model, pacbio: true do
+
+  let(:plate)           { create(:pacbio_plate) }
+  let(:libraries)       { create_list(:pacbio_library, 3) }
+  let(:wells_attributes) { 
+                          [
+                            attributes_for(:pacbio_well).except(:plate).merge({ relationships: { plate: {type: 'plate', id: plate.id}, libraries: [{type: 'libraries', id: libraries.first.id}]}}),
+                            attributes_for(:pacbio_well).except(:plate).merge({ relationships: { plate: {type: 'plate', id: plate.id}, libraries: [{type: 'libraries', id: libraries[1].id}]}}),
+                            attributes_for(:pacbio_well).except(:plate).merge({ relationships: { plate: {type: 'plate', id: plate.id}, libraries: [{type: 'libraries', id: libraries.last.id}]}})
+                        ]}
+
+  context '#initialize' do
+    it 'creates an object for each given well' do
+      factory = Pacbio::WellFactory.new(wells_attributes)
+      expect(factory.wells.count).to eq(3)
+
+      well = factory.wells.first
+      expect(well.plate).to eq(plate)
+      expect(well.libraries.length).to eq(1)
+    end
+
+    it 'produces error messages if any of the wells are not valid' do
+      wells_attributes << attributes_for(:pacbio_well).except(:plate)
+      factory = Pacbio::WellFactory.new(wells_attributes)
+      expect(factory).to_not be_valid
+      expect(factory.errors.full_messages).to_not be_empty
+    end
+
+  end
+
+  context '#save' do
+    it 'if valid creates the wells' do
+      factory = Pacbio::WellFactory.new(wells_attributes)
+      expect(factory).to be_valid
+      expect(factory.save).to be_truthy
+      expect(Pacbio::Well.count).to eq(3)
+      expect(Pacbio::Well.first.libraries.count).to eq(1)
+    end
+
+    it 'if invalid wont create the wells' do
+      wells_attributes << attributes_for(:pacbio_well).except(:plate)
+      factory = Pacbio::WellFactory.new(wells_attributes)
+      expect(factory).to_not be_valid
+      expect(factory.save).to be_falsey
+      expect(Pacbio::Well.count).to eq(0)
+    end
+
+  end
+
+end
