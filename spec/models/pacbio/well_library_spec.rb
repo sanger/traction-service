b1fd93270703a0ef7a97845ca3fc24bfe4df4885
Harriet Craven <hc6@sanger.ac.uk>
2019-11-11 16:57:52 +0000

    Change well_library resource to only return necessary barcode attribute

--- spec/models/pacbio/well_library_spec.rb
+++ spec/models/pacbio/well_library_spec.rb
@@ -13,14 +13,10 @@ RSpec.describe Pacbio::WellLibrary, type: :model, pacbio: true do
   it 'will have some library attributes' do
     request = create(:pacbio_request)
     library = create(:pacbio_library)
-    create(:pacbio_request_library, request: request, library: library)
-    library = create(:pacbio_well_library, library: library)
+    tube_with_library = create(:tube, material: library)
+    well_library = create(:pacbio_well_library, library: library)
     
-    expect(library.volume).to be_present
-    expect(library.concentration).to be_present
-    expect(library.library_kit_barcode).to be_present
-    expect(library.fragment_size).to be_present
-    expect(library.sample_names).to be_present
+    expect(well_library.barcode).to be_present
   end
   
 end
