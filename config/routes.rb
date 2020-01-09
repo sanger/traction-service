1987fe72f1b25268e4110df86830f68d2105c6ec
Harriet Craven <hc6@sanger.ac.uk>
2020-01-09 16:13:53 +0000

    Refactored jsonapi routes for pacbio libraries

--- config/routes.rb
+++ config/routes.rb
@@ -24,14 +24,8 @@ Rails.application.routes.draw do
       end
       jsonapi_resources :plates,        only: %i[index create update destroy]
       jsonapi_resources :wells,         only: %i[index create update destroy] do
-        scope module: :wells do
-          # TODO: converting this to jsonapi_resources causes a resource not
-          # found error.
-          resources :libraries, only: %i[create]
-        end
       end
-      # TODO: converting this to jsonapi_resources causes test failure
-      resources :libraries,     only: %i[index create destroy]
+      jsonapi_resources :libraries,     only: %i[index create destroy]
       jsonapi_resources :requests,      only: %i[index create update destroy]
       jsonapi_resources :wells,         only: %i[index create update destroy]
       jsonapi_resources :tubes, only: %i[index]
