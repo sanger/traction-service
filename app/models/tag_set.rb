2984718b7126d8975c938a976cfd1bb5e845e092
Katy Taylor <kt17@sanger.ac.uk>
2020-01-24 11:43:12 +0000

    Rubocop wanted a comment and a 'dependent' option

--- app/models/tag_set.rb
+++ app/models/tag_set.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Represents a collection of tags, generally bought together as a product
+# Also known as a barcode set
 class TagSet < ApplicationRecord
-  has_many :tags
+  has_many :tags, dependent: :restrict_with_error
 end
