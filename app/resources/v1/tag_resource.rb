5625f55474553bda75bf8f4cf89ae76aba7f123a
Katy Taylor <kt17@sanger.ac.uk>
2020-01-24 14:42:56 +0000

    tests for tag set json api resource - the failure responses are slightly different to in tags_spec because we are using the default controller - not sure if should alter controller so that responses are consistent with tags resource

--- app/resources/v1/tag_resource.rb
+++ app/resources/v1/tag_resource.rb
@@ -6,8 +6,8 @@ module V1
     attributes :oligo, :group_id, :tag_set_id
 
     # originally put 'belongs_to' to match the model, but got following warning from jsonapi:
-    # In V1::TagResource you exposed a `has_one` relationship  using the `belongs_to` class method...
-    # We think `has_one` is more appropriate. If you know what you're doing, and don't want to see this warning again, override the `belongs_to` class method on your resource.
+    # ...you exposed a `has_one` relationship  using the `belongs_to` class method...
+    # We think `has_one` is more appropriate... etc.
     has_one :tag_set
   end
 end
