e2e5b3c20b2800047addc88254c701f1ef08649e
Katy Taylor <kt17@sanger.ac.uk>
2020-01-30 12:54:00 +0000

    add rollback for migration as rails can't reverse 'add_column' by default

--- db/migrate/20200123164052_change_tag.rb
+++ db/migrate/20200123164052_change_tag.rb
@@ -1,8 +1,15 @@
 class ChangeTag < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_table :tags do |t|
       t.belongs_to :tag_set
       t.remove :set_name
+    end
+  end
+
+  def down
+    change_table :tags do |t|
+      t.column :set_name, :string
+      t.remove_references :tag_set
     end
   end
 end
