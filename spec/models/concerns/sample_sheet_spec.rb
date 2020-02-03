714031960561ef9c20f2ebb5871503b1f5eb8655
Katy Taylor <kt17@sanger.ac.uk>
2020-02-03 09:55:46 +0000

    update the sample sheet tests to match the latest version of the sample sheet methods

--- spec/models/concerns/sample_sheet_spec.rb
+++ spec/models/concerns/sample_sheet_spec.rb
@@ -12,35 +12,23 @@ RSpec.describe SampleSheet do
         request_library = create(:pacbio_request_library_with_tag)
         well.libraries << request_library.library
         tag_group_id = request_library.tag.group_id
-        sample_name = well.sample_names
-        expected = "#{tag_group_id}--#{tag_group_id};#{sample_name}"
-        expect(well.barcode_name).to eq expected
-      end
-
-      it 'returns a string of library tags when the well has many libraries' do
-        result = well_with_request_libraries.barcode_name
-        pairs = result.split('|')
-        expect(pairs.length).to eq well_with_request_libraries.request_libraries.length
+        expected = "#{tag_group_id}--#{tag_group_id}"
+        expect(request_library.barcode_name).to eq expected
       end
 
       it 'returns nothing if the libraries are not tagged' do
         request_library = create(:pacbio_request_library)
         well.libraries << request_library.library
-        expect(well.barcode_name).to be_nil
+        expect(request_library.barcode_name).to be_nil
       end
     end
 
     context 'barcode_set' do
-      it 'returns a string of the library request tags group id when the well has one library' do
+      it 'returns the tag set uuid' do
         request_library = create(:pacbio_request_library_with_tag)
         well.libraries << request_library.library
-        expected_set_name = request_library.tag.set_name
+        expected_set_name = request_library.tag.tag_set.uuid
         expect(well.barcode_set).to eq expected_set_name
-      end
-
-      it 'returns a string of the library request tags group ids when the well has many libraries' do
-        expected_set_name = well_with_request_libraries.request_libraries.first.tag.set_name
-        expect(well_with_request_libraries.barcode_set).to eq expected_set_name
       end
 
       it 'returns nothing if the libraries are not tagged' do
@@ -77,20 +65,6 @@ RSpec.describe SampleSheet do
     context 'same_barcodes_on_both_ends_of_sequence' do
       it 'returns true' do
         expect(well.same_barcodes_on_both_ends_of_sequence).to eq true
-      end
-    end
-
-    context 'bio_sample_name' do
-      it 'returns the wells library request sample name when the well has one library' do
-        request_library = create(:pacbio_request_library_with_tag).library
-        well.libraries << request_library
-        expected = well.libraries[0].request_libraries.first.request.sample.name
-        expect(well.bio_sample_name).to eq expected
-      end
-
-      it 'returns the wells library requests sample names when the well has many libraries' do
-        expected = well_with_request_libraries.request_libraries.collect(&:request).collect(&:sample_name).join(',')
-        expect(well_with_request_libraries.bio_sample_name).to eq expected
       end
     end
 
