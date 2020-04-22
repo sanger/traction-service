e4b78e6316f8b6a0640437b1c582148207c0fcf2
Christopher Friend <cf17@sanger.ac.uk>
2020-04-22 12:13:53 +0100

    Add update well mutation

---
+++ app/graphql/mutations/update_well.rb
@@ -0,0 +1,26 @@
+module Mutations
+  class UpdateWell < BaseMutation
+    argument :well_id, ID, required: true
+    argument :position, String, required: false
+
+    field :well, Types::WellType, null: true
+    field :errors, [String], null: false
+
+    def resolve(well_id:, position:)
+      well = Well.find(well_id)
+      well.position = position
+
+      if well.save
+        {
+          well: well,
+          errors: [],
+        }
+      else
+        {
+          well: nil,
+          errors: well.errors.full_messages
+        }
+      end
+    end
+  end
+end
