5cc37ab2ea5a897f71389c0b975d1a699474a82c
Stuart McHattie <sm49@sanger.ac.uk>
2020-04-22 15:39:09 +0100

    Let Rubocop apply fixes to style

--- app/graphql/types/well_query_type.rb
+++ app/graphql/types/well_query_type.rb
@@ -3,17 +3,13 @@
 module Types
   class WellQueryType < Types::BaseObject
     field :wells, [WellType], null: false do
-      description "Find all wells, optionally those associated with a plate."
+      description 'Find all wells, optionally those associated with a plate.'
       argument :plate_id, Int, required: false
     end
 
     def wells(plate_id: nil)
       wells = Well.all
-
-      unless (plate_id.nil?)
-        wells = wells.select { |well| well.plate_id == plate_id }
-      end
-
+      wells = wells.select { |well| well.plate_id == plate_id } unless plate_id.nil?
       wells
     end
   end
