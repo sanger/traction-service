5d38b69d4d1d06195e8df9358209bce70c22873a
Christopher Friend <cf17@sanger.ac.uk>
2020-04-22 13:38:50 +0100

    Rename container join table to container_material

--- app/models/concerns/receptacle.rb
+++ app/models/concerns/receptacle.rb
@@ -5,7 +5,7 @@ module Receptacle
   extend ActiveSupport::Concern
 
   included do
-    has_one :container, as: :receptacle, dependent: :destroy
-    delegate :material, to: :container
+    has_one :container_material, as: :receptacle, dependent: :destroy
+    delegate :material, to: :container_material
   end
 end
