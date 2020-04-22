6c8731eac88e7dce134d5a3dcafc3690d3c1dd43
Christopher Friend <cf17@sanger.ac.uk>
2020-04-22 12:13:22 +0100

    Add well type fetch directly to query_type.rb (should probably be in it's own file?)

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -9,5 +9,19 @@ module Types
     def test_field
       "Hello World!"
     end
+  
+    # First describe the field signature:
+    field :well, WellType, null: true do
+      description "Find a well by ID"
+      argument :id, ID, required: true
+      argument :position, String, required: false
+      # fields should be queried in camel-case (this will be `truncatedPreview`)
+      argument :plate_id, Int, required: false
+    end
+    
+    # Then provide an implementation:
+    def well(id:)
+      Well.find(id)
+    end
   end
 end
