831d3922583a0017fbd704b51b277604bdbdae2a
Christopher Friend <cf17@sanger.ac.uk>
2020-04-22 12:14:14 +0100

    Register the update well mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,14 @@
 module Types
   class MutationType < Types::BaseObject
+
+    field :update_well, mutation: Mutations::UpdateWell
+    
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+    # field :test_field, String, null: false,
+    #   description: "An example field added by the generator"
+    
+    # def test_field
+    #   "Hello World"
+    # end
   end
 end
