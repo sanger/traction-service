e209667251f7644c24dd5ad26ab879f5786d03a6
Stuart McHattie <sm49@sanger.ac.uk>
2020-04-22 17:42:50 +0100

    Make updating the well have an optional position argument

--- app/graphql/mutations/update_well_mutation.rb
+++ app/graphql/mutations/update_well_mutation.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 
 module Mutations
-  # Mutation to handle the update of a well position for a well with a given ID.
-  class UpdateWellPositionMutation < BaseMutation
+  # Mutation to handle the update of a Well.
+  class UpdateWellMutation < BaseMutation
     argument :id, ID, required: true
-    argument :position, String, required: true
+    argument :position, String, required: false
 
     field :well, Types::WellType, null: true
     field :errors, [String], null: false
 
-    def resolve(id:, position:)
+    def resolve(id:, position: nil)
       well = Well.find(id)
+      well.position = position unless position.nil?
 
-      if well.update(position: position)
+      if well.save
         { well: well, errors: [] }
       else
         { well: nil, errors: well.errors.full_messages }
--- app/graphql/types/well_mutation_type.rb
+++ app/graphql/types/well_mutation_type.rb
@@ -3,6 +3,6 @@
 module Types
   # The type for Well mutations.
   class WellMutationType < BaseObject
-    field :update_well_position, mutation: Mutations::UpdateWellPositionMutation
+    field :update_well, mutation: Mutations::UpdateWellMutation
   end
 end
