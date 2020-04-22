7948fe91484ebf38118e04de58c456efff014b47
Christopher Friend <cf17@sanger.ac.uk>
2020-04-22 12:13:43 +0100

    Add comments to base_mutation class

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -1,8 +1,11 @@
 module Mutations
   class BaseMutation < GraphQL::Schema::RelayClassicMutation
     argument_class Types::BaseArgument
+    # This is used for return fields on the mutation's payload
     field_class Types::BaseField
+    # This is used for generating the `input: { ... }` object type
     input_object_class Types::BaseInputObject
+    # This is used for generating payload types
     object_class Types::BaseObject
   end
 end
