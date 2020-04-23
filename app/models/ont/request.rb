f2530b8b79367c1a2b5b8ff8d2986e9bc0fec952
Christopher Friend <cf17@sanger.ac.uk>
2020-04-23 08:43:24 +0100

    Add the request/requestable/sample relationship to Ont::Request

--- app/models/ont/request.rb
+++ app/models/ont/request.rb
@@ -4,5 +4,10 @@ module Ont
   # Ont::Request
   class Request < ApplicationRecord
     include Material
+
+    has_one :request, class_name: '::Request', as: :requestable, dependent: :nullify
+    has_one :sample, through: :request
+
+    delegate :name, to: :sample, prefix: :sample
   end
 end
