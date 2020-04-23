ab10b9b472baeb5f8e214665056a117a89009c7c
Christopher Friend <cf17@sanger.ac.uk>
2020-04-23 08:43:47 +0100

    Update the Ont::Request spec factory to include a sample

--- spec/factories/ont/requests.rb
+++ spec/factories/ont/requests.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :ont_request, class: Ont::Request do
+    after(:create) do |req|
+      req.request = create(:request, requestable: req, sample: create(:sample))
+    end
   end
 end
