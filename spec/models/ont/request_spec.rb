dbf3a3dea6c45e2f2e92c4d2fe02ba2dfda41f57
Christopher Friend <cf17@sanger.ac.uk>
2020-04-23 08:44:11 +0100

    Add ont request test for sample's name

--- spec/models/ont/request_spec.rb
+++ spec/models/ont/request_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Ont::Request, type: :model, ont: true do
     let(:material_model) { :ont_request }
     it_behaves_like 'material'
   end
+
+  it 'returns sample name' do
+    request = create(:ont_request)
+    expect(request.sample_name).to be_present
+    expect(request.sample_name).to eq(request.sample.name)
+  end
 end
