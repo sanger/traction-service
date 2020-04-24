98b63f9bbeb4b2171b149bd3895d15f37fdaf513
Stuart McHattie <sm49@sanger.ac.uk>
2020-04-24 16:33:02 +0100

    Update container and material tests

--- spec/support/material.rb
+++ spec/support/material.rb
@@ -5,4 +5,9 @@ RSpec.shared_examples "material" do
     expect(container_material.container).to be_present
     expect(material.container).to eq(container_material.container)
   end
+
+  it "produces nil for delegated container if it doesn't exist" do
+    material = create(material_model, container_material: nil)
+    expect(material.container).to be_nil
+  end
 end
