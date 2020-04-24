893a3bd4fce89265e00b2b781989b8298adc9930
Stuart McHattie <sm49@sanger.ac.uk>
2020-04-24 16:25:42 +0100

    Create better condition reports in spec tests

--- spec/requests/v2/wells_spec.rb
+++ spec/requests/v2/wells_spec.rb
@@ -5,58 +5,62 @@ require "rails_helper"
 RSpec.describe 'GraphQL', type: :request do
 
   context 'get well' do
-    let!(:well) { create(:well) }
+    context 'when there is a valid well' do
+      let!(:well) { create(:well) }
 
-    it 'returns the well with valid ID' do
-      post v2_path, params: { query: '{ well(id: 1) { id plateId } }' }
-      expect(response).to have_http_status(:success)
-      json = ActiveSupport::JSON.decode(response.body)
-      expect(json['data']['well']).to include(
-        'id' => '1',
-        'plateId' => 1
-      )
-    end
+      it 'returns the well with valid ID' do
+        post v2_path, params: { query: '{ well(id: 1) { id plateId } }' }
+        expect(response).to have_http_status(:success)
+        json = ActiveSupport::JSON.decode(response.body)
+        expect(json['data']['well']).to include(
+          'id' => '1',
+          'plateId' => 1
+        )
+      end
 
-    it 'returns null when well invalid ID' do
-      post v2_path, params: { query: '{ well(id: 10) { id } }' }
-      expect(response).to have_http_status(:success)
-      json = ActiveSupport::JSON.decode(response.body)
-      expect(json['data']['well']).to be_nil
+      it 'returns null when well invalid ID' do
+        post v2_path, params: { query: '{ well(id: 10) { id } }' }
+        expect(response).to have_http_status(:success)
+        json = ActiveSupport::JSON.decode(response.body)
+        expect(json['data']['well']).to be_nil
+      end
     end
   end
 
   context 'get wells' do
-    let!(:plate_1) { create(:plate_with_wells, well_count: 2) }
-    let!(:plate_2) { create(:plate_with_wells, well_count: 1) }
+    context 'when there are two plates with wells' do
+      let!(:plate_1) { create(:plate_with_wells, well_count: 2) }
+      let!(:plate_2) { create(:plate_with_wells, well_count: 1) }
 
-    it 'returns all wells' do
-      post v2_path, params: { query: '{ wells { id } }' }
-      expect(response).to have_http_status(:success)
-      json = ActiveSupport::JSON.decode(response.body)
-      expect(json['data']['wells'].length).to eq(3)
-    end
+      it 'returns all wells' do
+        post v2_path, params: { query: '{ wells { id } }' }
+        expect(response).to have_http_status(:success)
+        json = ActiveSupport::JSON.decode(response.body)
+        expect(json['data']['wells'].length).to eq(3)
+      end
 
-    it 'returns wells for plate 1' do
-      post v2_path, params: { query: "{ wells(plateId: #{plate_1.id}) { id plateId } }" }
-      expect(response).to have_http_status(:success)
-      json = ActiveSupport::JSON.decode(response.body)
-      expect(json['data']['wells'].length).to eq(2)
-      expect(json['data']['wells'].map { |well| well['plateId'] }).to contain_exactly(plate_1.id, plate_1.id)
-    end
+      it 'returns wells for plate 1' do
+        post v2_path, params: { query: "{ wells(plateId: #{plate_1.id}) { plateId } }" }
+        expect(response).to have_http_status(:success)
+        json = ActiveSupport::JSON.decode(response.body)
+        expect(json['data']['wells'].length).to eq(2)
+        expect(json['data']['wells'].map { |well| well['plateId'] }).to contain_exactly(plate_1.id, plate_1.id)
+      end
 
-    it 'returns well for plate 2' do
-      post v2_path, params: { query: "{ wells(plateId: #{plate_2.id}) { id plateId } }" }
-      expect(response).to have_http_status(:success)
-      json = ActiveSupport::JSON.decode(response.body)
-      expect(json['data']['wells'].length).to eq(1)
-      expect(json['data']['wells'].first['plateId']).to eq(plate_2.id)
-    end
+      it 'returns well for plate 2' do
+        post v2_path, params: { query: "{ wells(plateId: #{plate_2.id}) { plateId } }" }
+        expect(response).to have_http_status(:success)
+        json = ActiveSupport::JSON.decode(response.body)
+        expect(json['data']['wells'].length).to eq(1)
+        expect(json['data']['wells'].first['plateId']).to eq(plate_2.id)
+      end
 
-    it 'returns no well for invalid plate ID' do
-      post v2_path, params: { query: "{ wells(plateId: 10) { id } }" }
-      expect(response).to have_http_status(:success)
-      json = ActiveSupport::JSON.decode(response.body)
-      expect(json['data']['wells'].length).to eq(0)
+      it 'returns no well for invalid plate ID' do
+        post v2_path, params: { query: "{ wells(plateId: 10) { id } }" }
+        expect(response).to have_http_status(:success)
+        json = ActiveSupport::JSON.decode(response.body)
+        expect(json['data']['wells'].length).to eq(0)
+      end
     end
   end
 
