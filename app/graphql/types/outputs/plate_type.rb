c8822d4677791daea521dedc3de7fac2a3f6427b
Christopher Friend <cf17@sanger.ac.uk>
2020-04-27 08:05:14 +0100

    Extract out a CommonOutputObject class to include fields common to most outputs

--- app/graphql/types/outputs/plate_type.rb
+++ app/graphql/types/outputs/plate_type.rb
@@ -3,11 +3,7 @@
 module Types
   module Outputs
     # The type for Plate objects.
-    class PlateType < BaseObject
-      field :id, ID, null: false
-      field :created_at, String, null: false
-      field :updated_at, String, null: false
-
+    class PlateType < CommonOutputObject
       field :barcode, String, null: false
       field :wells, [WellType], null: false
     end
