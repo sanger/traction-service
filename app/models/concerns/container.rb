cc34ef40648e973734b3d14f7506c302d30483be
Christopher Friend <cf17@sanger.ac.uk>
2020-04-28 13:43:56 +0100

    Allow a container to have many materials

--- app/models/concerns/container.rb
+++ app/models/concerns/container.rb
@@ -5,7 +5,10 @@ module Container
   extend ActiveSupport::Concern
 
   included do
-    has_one :container_material, as: :container, dependent: :destroy
-    delegate :material, to: :container_material, allow_nil: true
+    has_many :container_materials, as: :container, dependent: :destroy
+
+    def materials
+      return container_materials.map { |container_material| container_material.material }
+    end
   end
 end
