6ddcd4a04d85a85683b7c8966204099bc299588e
Christopher Friend <cf17@sanger.ac.uk>
2020-04-28 10:51:50 +0100

    Add taggable shared tests

---
+++ spec/support/taggable.rb
@@ -0,0 +1,24 @@
+RSpec.shared_examples "taggable" do
+  it 'returns empty tags with no tag taggables' do
+    taggable = create(taggable_model, tags_count: 0)
+    expect(taggable.tags).to be_empty
+  end
+
+  it 'returns all tags with many tag taggables' do
+    taggable = create(taggable_model)
+    expect(taggable.tags).to_not be_empty
+    expect(taggable.tags).to eq(taggable.tag_taggables.map { |tag_taggable| tag_taggable.tag })
+  end
+
+  it 'on destroy destroys tag taggables, not taggables' do
+    numTags = 3
+    taggable = create(taggable_model, tags_count: numTags)
+    # sanity check
+    expect(Tag.all.count).to eq(numTags)
+    # destroy the taggable
+    taggable.destroy
+    # test outcome
+    expect(TagTaggable.all.count).to eq(0)
+    expect(Tag.all.count).to eq(numTags)
+  end
+end
