5eb29722b19b6cd66340b84406eead23324e4c57
Christopher Friend <cf17@sanger.ac.uk>
2020-04-29 08:53:27 +0100

    Nest plate_with_ont_samples factory in plate factory. Use well_with_ont_samples_factory

--- spec/factories/plates.rb
+++ spec/factories/plates.rb
@@ -11,5 +11,17 @@ FactoryBot.define do
         end
       end
     end
+
+    factory :plate_with_ont_samples do
+      transient do
+        wells { [ { position: 'A1', samples: [ { name: 'Sample in A1' } ] } ] }
+      end
+  
+      after :create do |plate, options|
+        options.wells.each do |well_spec|
+          well = create(:well_with_ont_samples, plate: plate, position: well_spec[:position], samples: well_spec[:samples])
+        end
+      end
+    end
   end
 end
