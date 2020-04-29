4f8bd8ef0c36a5231d4a83fc56dad9c530aba54a
Christopher Friend <cf17@sanger.ac.uk>
2020-04-29 13:54:31 +0100

    Update request factory to tag ont_requests if given a tag set is provided in attributes

--- app/models/ont/request_factory.rb
+++ app/models/ont/request_factory.rb
@@ -8,21 +8,23 @@ module Ont
   class RequestFactory
     include ActiveModel::Model
 
-    validate :check_request
+    validate :check_request, :check_tag
 
     def initialize(attributes = {})
       @well = attributes[:well]
+      @tag_set = attributes[:tag_set]
       return unless attributes.key?(:request_attributes)
 
       build_request(attributes[:request_attributes])
     end
 
-    attr_reader :well, :request, :well_request_join
+    attr_reader :well, :tag_set, :tag_taggable, :request, :well_request_join
 
     def save
       return false unless valid?
 
       request.save
+      tag_taggable&.save
       well_request_join.save
       true
     end
@@ -32,14 +34,20 @@ module Ont
     def build_request(request_attributes)
       constants_accessor = Pipelines::ConstantsAccessor.new(Pipelines.ont.covid)
       sample = build_or_fetch_sample(request_attributes, constants_accessor)
-      @request = ::Request.new(
-        requestable: Ont::Request.new(
-          external_study_id: constants_accessor.external_study_id
-        ),
-        sample: sample
-      )
-      @well_request_join = ::ContainerMaterial.new({ container: well,
-                                                     material: request.requestable })
+      ont_request = build_ont_request(request_attributes, constants_accessor)
+      @request = ::Request.new(requestable: ont_request, sample: sample)
+      @well_request_join = ::ContainerMaterial.new(container: well, material: request.requestable)
+    end
+
+    def build_ont_request(request_attributes, constants_accessor)
+      ont_request = Ont::Request.new(external_study_id: constants_accessor.external_study_id)
+      unless tag_set.nil?
+        if request_attributes.key?(:tag_group_id)
+          tag = ::Tag.find_by(tag_set_id: tag_set.id, group_id: request_attributes[:tag_group_id])
+          @tag_taggable = ::TagTaggable.new(taggable: ont_request, tag: tag)
+        end
+      end
+      ont_request
     end
 
     def build_or_fetch_sample(request_attributes, constants_accessor)
@@ -58,6 +66,21 @@ module Ont
       return if request.valid?
 
       request.errors.each do |k, v|
+        errors.add(k, v)
+      end
+    end
+
+    def check_tag
+      return if tag_set.nil?
+
+      if tag_taggable.nil?
+        errors.add('request', 'must have a tag')
+        return
+      end
+
+      return if tag_taggable.valid?
+
+      tag_taggable.errors.each do |k, v|
         errors.add(k, v)
       end
     end
