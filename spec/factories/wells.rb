e49528b58f10ddeedb07fe2f6cb322a4223d073d
Christopher Friend <cf17@sanger.ac.uk>
2020-04-29 08:51:28 +0100

    Add well_with_ont_samples factory nested in wells factory

--- spec/factories/wells.rb
+++ spec/factories/wells.rb
@@ -2,5 +2,20 @@ FactoryBot.define do
   factory :well do
     plate 
     position { 'A1' }
+
+    factory :well_with_ont_samples do
+      transient do
+        samples { [{ name: 'Sample in A1' }] }
+      end
+  
+      after :create do |well, options|
+        options.samples.each do |sample_spec|
+          sample = create(:sample, name: sample_spec[:name])
+          request = create(:request, sample: sample)
+          ont_request = create(:ont_request, request: request)
+          create(:container_material, container: well, material: ont_request)
+        end
+      end
+    end
   end
 end
