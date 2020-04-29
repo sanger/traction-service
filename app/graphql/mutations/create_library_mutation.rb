684ae4531fe98607cbf1983316ba751e0801b6fa
Harriet Craven <hc6@sanger.ac.uk>
2020-04-29 14:21:12 +0100

    rubocop #2

--- app/graphql/mutations/create_library_mutation.rb
+++ app/graphql/mutations/create_library_mutation.rb
@@ -8,8 +8,8 @@ module Mutations
     field :library, Types::Outputs::LibraryType, null: true
     field :errors, [String], null: false
 
-    def resolve(arguments:)
-      # plate_id = arguments.plate_id
-    end
+    # def resolve(arguments:)
+    #   arguments.plate_id
+    # end
   end
 end
