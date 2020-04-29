016e75a3ecfd1b6d6a2f5f5fe9b77959ee96a93b
Harriet Craven <hc6@sanger.ac.uk>
2020-04-29 16:32:42 +0100

    Added tagset for sample pooling, added comments, rubocopped#3

--- app/graphql/mutations/pool_samples_mutation.rb
+++ app/graphql/mutations/pool_samples_mutation.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 module Mutations
-  # Mutation to handle the creation of a library, by pooling samples
+  # Mutation to handle the creation of a library(s), by pooling samples
   class PoolSamplesMutation < BaseMutation
     argument :arguments, Types::Inputs::PoolSamplesInputType, required: true
 
     field :library, [Types::Outputs::LibraryType], null: true
     field :errors, [String], null: false
 
-    def resolve(arguments:)
-      debugger
-      # arguments.plate_id
-    end
+    # def resolve(arguments:)
+    #   debugger
+    # end
   end
 end
--- app/graphql/types/inputs/pool_samples_input_type.rb
+++ app/graphql/types/inputs/pool_samples_input_type.rb
@@ -4,8 +4,8 @@ module Types
   module Inputs
     # The input arguments required to pool samples.
     class PoolSamplesInputType < BaseInputObject
-      argument :plate_id, Int, required: false
-      # Add tag set argument
+      argument :plate_id, Int, required: true
+      argument :tag_set, Int, required: true # 24 or 96
     end
   end
 end
