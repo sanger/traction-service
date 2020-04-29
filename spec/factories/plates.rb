f8d4d432e0d6763e98ef62cc15adb380103eb269
Christopher Friend <cf17@sanger.ac.uk>
2020-04-29 08:07:18 +0100

    Nest plate_with_wells factory inside plate factory

--- spec/factories/plates.rb
+++ spec/factories/plates.rb
@@ -1,4 +1,15 @@
 FactoryBot.define do
   factory :plate do
+    factory :plate_with_wells do
+      transient do
+        well_count { 1 }
+      end
+  
+      after :create do |plate, options|
+        options.well_count.times do |i|
+          create(:well, position: "A#{i}", plate: plate)
+        end
+      end
+    end
   end
 end
