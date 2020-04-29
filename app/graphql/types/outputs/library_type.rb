55059e3c4fb13ec214736a88a9f3ccb557b185b8
Harriet Craven <hc6@sanger.ac.uk>
2020-04-29 16:06:11 +0100

    Added minimal library creation graphql api, for front end development

--- app/graphql/types/outputs/library_type.rb
+++ app/graphql/types/outputs/library_type.rb
@@ -2,9 +2,15 @@
 
 module Types
   module Outputs
-    # The type for Plate objects.
+    # The type for Library objects.
     class LibraryType < CommonOutputObject
       field :id, Int, null: false
+      field :tubeBarcode, String, null: true
+      field :plateBarcode, String, null: true
+      field :pool, String, null: true
+      field :name, String, null: true
+      field :wells, String, null: true
+      field :tagSet, Integer, null: true
     end
   end
 end
