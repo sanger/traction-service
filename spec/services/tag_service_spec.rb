576125044c6b52459e434fe06c53b9e59650fb44
Stuart McHattie <sm49@sanger.ac.uk>
2020-04-30 18:26:30 +0100

    Fix tag service tests

--- spec/services/tag_service_spec.rb
+++ spec/services/tag_service_spec.rb
@@ -1,38 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe TagService do
-  let!(:tag1) { create(:tag) }
-  let!(:tag2) { create(:tag) }
+  let!(:tag_set) { create(:tag_set_with_tags, number_of_tags: 2)}
 
   it 'returns nil when fetching tag with no matching group id' do
-    tag_service = TagService.new(tag1.tag_set)
+    tag_service = TagService.new(tag_set)
     tag = tag_service.find_and_register_tag('invalid group id')
     expect(tag).to be_nil
   end
 
   it 'returns existing tag' do
-    tag_service = TagService.new(tag1.tag_set)
-    tag = tag_service.find_and_register_tag(tag1.group_id)
-    expect(tag).to eq(tag1)
+    tag_service = TagService.new(tag_set)
+    tag = tag_service.find_and_register_tag(tag_set.tags.first.group_id)
+    expect(tag).to eq(tag_set.tags.first)
   end
 
   it 'is not complete without registering all tags in a set' do
-    tag_service = TagService.new(tag1.tag_set)
-    tag_service.find_and_register_tag(tag1.group_id)
+    tag_service = TagService.new(tag_set)
+    tag_service.find_and_register_tag(tag_set.tags.first.group_id)
     expect(tag_service.complete?).to be_falsey
   end
 
   it 'is not complete having registered a tag more than once' do
-    tag_service = TagService.new(tag1.tag_set)
-    tag_service.find_and_register_tag(tag1.group_id)
-    tag_service.find_and_register_tag(tag1.group_id)
+    tag_service = TagService.new(tag_set)
+    tag_service.find_and_register_tag(tag_set.tags.first.group_id)
+    tag_service.find_and_register_tag(tag_set.tags.first.group_id)
     expect(tag_service.complete?).to be_falsey
   end
 
   it 'is complete with all tags in a set registered' do
-    tag_service = TagService.new(tag1.tag_set)
-    tag_service.find_and_register_tag(tag1.group_id)
-    tag_service.find_and_register_tag(tag2.group_id)
+    tag_service = TagService.new(tag_set)
+    tag_service.find_and_register_tag(tag_set.tags.first.group_id)
+    tag_service.find_and_register_tag(tag_set.tags.second.group_id)
     expect(tag_service.complete?).to be_truthy
   end
 end
