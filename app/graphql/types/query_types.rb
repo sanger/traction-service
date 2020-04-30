82a72ac417c02260bcdee310f27d3346cd481307
Harriet Craven <hc6@sanger.ac.uk>
2020-04-30 10:30:36 +0100

    tube_barcode comment

--- app/graphql/types/query_types.rb
+++ app/graphql/types/query_types.rb
@@ -41,16 +41,17 @@ module Types
 
     def libraries
       # ONT::Library.all
+      # tube_barcode = 'TRAC-<tube/plate>-id-poolnumber'
       [
-        { id: 1, tube_barcode: 'TRAC-2-1', plate_barcode: 'TRAC-1-1', pool: 1,
+        { id: 1, tube_barcode: 'TRAC-2-1-1', plate_barcode: 'TRAC-1-1', pool: 1,
           name: 'TRAC-1-1-1', wells: 'A1-H3', tag_set: 24 },
-        { id: 2, tube_barcode: 'TRAC-2-2', plate_barcode: 'TRAC-1-1', pool: 2,
+        { id: 2, tube_barcode: 'TRAC-2-2-2', plate_barcode: 'TRAC-1-1', pool: 2,
           name: 'TRAC-1-1-2', wells: 'A4-H6', tag_set: 24 },
-        { id: 3, tube_barcode: 'TRAC-2-3', plate_barcode: 'TRAC-1-1', pool: 3,
+        { id: 3, tube_barcode: 'TRAC-2-3-3', plate_barcode: 'TRAC-1-1', pool: 3,
           name: 'TRAC-1-1-3', wells: 'A7-H9', tag_set: 24 },
-        { id: 4, tube_barcode: 'TRAC-2-4', plate_barcode: 'TRAC-1-1', pool: 4,
+        { id: 4, tube_barcode: 'TRAC-2-4-4', plate_barcode: 'TRAC-1-1', pool: 4,
           name: 'TRAC-1-1-4', wells: 'A10-H12', tag_set: 24 },
-        { id: 5, tube_barcode: 'TRAC-2-5', plate_barcode: 'TRAC-1-2', pool: 1,
+        { id: 5, tube_barcode: 'TRAC-2-5-1', plate_barcode: 'TRAC-1-2', pool: 1,
           name: 'TRAC-1-2-1', wells: 'A1-H12', tag_set: 96 }
       ]
     end
