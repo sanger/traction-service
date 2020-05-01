eb12ce5d7419c35770a78642cfd74c6109c03f12
Christopher Friend <cf17@sanger.ac.uk>
2020-05-01 11:49:23 +0100

    Have tag service handle having a nil tag_set

--- app/services/tag_service.rb
+++ app/services/tag_service.rb
@@ -11,12 +11,16 @@ class TagService
   attr_reader :tag_set
 
   def find_and_register_tag(group_id)
+    return if tag_set.nil?
+
     tag = Tag.find_by(tag_set_id: tag_set.id, group_id: group_id)
     tags << tag unless tag.nil?
     tag
   end
 
   def complete?
+    return false if tag_set.nil?
+
     tag_set.tags.count == tags.uniq.count
   end
 
