00a18dbbfeca59966aa30265d19f31a6d72c8415
Christopher Friend <cf17@sanger.ac.uk>
2020-05-01 14:19:53 +0100

    Allow getting a row and column from a well

--- spec/models/well_spec.rb
+++ spec/models/well_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe Well, type: :model do
   it 'must have a position' do
     expect(build(:well, position: nil)).to_not be_valid
   end
+
+  it 'gets row from position' do
+    well = create(:well, position: 'B6')
+    expect(well.row).to eq('B')
+  end
+
+  it 'gets column from position' do
+    well = create(:well, position: 'B6')
+    expect(well.column).to eq(6)
+  end
+
+  it 'gets two digit column from position' do
+    well = create(:well, position: 'B12')
+    expect(well.column).to eq(12)
+  end
 end
