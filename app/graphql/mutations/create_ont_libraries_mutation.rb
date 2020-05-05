829f0b5c1d98671d567fdf76dda5086aec15cb3f
Christopher Friend <cf17@sanger.ac.uk>
2020-05-05 08:52:51 +0100

    Add and register a mutation to create Ont libraries

---
+++ app/graphql/mutations/create_ont_libraries_mutation.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Mutations
+  # Mutation to handle the update of a Well.
+  class CreateOntLibraries < BaseMutation
+    argument :arguments, Types::Inputs::Ont::LibrariesInputType, required: true
+
+    field :libraries, [Types::Outputs::Ont::LibraryType], null: false
+    field :errors, [String], null: false
+
+    def resolve(arguments:)
+      factory = Ont::LibraryFactory.new(arguments.to_h)
+
+      if factory.save
+        { libraries: factory.libraries, errors: [] }
+      else
+        { libraries: [], errors: factory.errors.full_messages }
+      end
+    end
+  end
+end
