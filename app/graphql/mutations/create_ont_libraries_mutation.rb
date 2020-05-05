32ba2deb86c5a7c00e3057f5f1818510bfc2da6d
Christopher Friend <cf17@sanger.ac.uk>
2020-05-05 13:38:36 +0100

    Update create ont libraries mutation to return libraries in tubes

--- app/graphql/mutations/create_ont_libraries_mutation.rb
+++ app/graphql/mutations/create_ont_libraries_mutation.rb
@@ -5,16 +5,16 @@ module Mutations
   class CreateOntLibrariesMutation < BaseMutation
     argument :arguments, Types::Inputs::Ont::LibrariesInputType, required: true
 
-    field :libraries, [Types::Outputs::Ont::LibraryType], null: false
+    field :tubes, [Types::Outputs::TubeType], null: false
     field :errors, [String], null: false
 
     def resolve(arguments:)
       factory = Ont::LibraryFactory.new(arguments.to_h)
 
       if factory.save
-        { libraries: factory.libraries, errors: [] }
+        { tubes: factory.tubes, errors: [] }
       else
-        { libraries: [], errors: factory.errors.full_messages }
+        { tubes: [], errors: factory.errors.full_messages }
       end
     end
   end
