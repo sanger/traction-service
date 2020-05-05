eb577e6fff5ad9a55fd9060e5cbc42f91e5b7d79
Christopher Friend <cf17@sanger.ac.uk>
2020-05-05 13:38:36 +0100

    Update create ont libraries mutation to return libraries in tubes

--- spec/requests/v2/ont/libraries_spec.rb
+++ spec/requests/v2/ont/libraries_spec.rb
@@ -34,49 +34,52 @@ RSpec.describe 'GraphQL', type: :request do
           }
         )
         {
-          libraries { name pool wellRange poolSize }
+          tubes { barcode materials { ... on Library { name pool wellRange poolSize } } }
           errors
         }
       }
       GQL
     end
 
-    it 'creates libraries with provided parameters' do
-      libraries = create_list(:ont_library, 4).each_with_index do |library, i|
+    it 'creates tubes with libraries with provided parameters' do
+      libraries = create_list(:ont_library_in_tube, 4).each_with_index do |library, i|
         library.pool = i+1
         library.name = "PLATE-1-1234-#{i+1}"
         library.well_range = "A#{(i*2)+1}-H#{(i+1)*2}"
+        library.save
       end
 
       allow_any_instance_of(Ont::LibraryFactory).to receive(:save).and_return(true)
-      allow_any_instance_of(Ont::LibraryFactory).to receive(:libraries).and_return(libraries)
+      allow_any_instance_of(Ont::LibraryFactory).to receive(:tubes).and_return(Tube.all)
 
       post v2_path, params: { query: valid_query }
       expect(response).to have_http_status(:success)
       json = ActiveSupport::JSON.decode(response.body)
 
       mutation_json = json['data']['createOntLibraries']
-      libraries_json = mutation_json['libraries']
+      tubes_json = mutation_json['tubes']
 
-      expect(libraries_json[0]['name']).to eq('PLATE-1-1234-1')
-      expect(libraries_json[0]['pool']).to eq(1)
-      expect(libraries_json[0]['wellRange']).to eq('A1-H2')
-      expect(libraries_json[0]['poolSize']).to eq(24)
+      expect(tubes_json[0]['barcode']).to eq('TRAC-2-1')
+      expect(tubes_json[0]['materials']).to contain_exactly(
+        { 'name' => 'PLATE-1-1234-1', 'pool' => 1, 'wellRange' => 'A1-H2', 'poolSize' => 24 }
+      )
 
-      expect(libraries_json[1]['name']).to eq('PLATE-1-1234-2')
-      expect(libraries_json[1]['pool']).to eq(2)
-      expect(libraries_json[1]['wellRange']).to eq('A3-H4')
-      expect(libraries_json[1]['poolSize']).to eq(24)
+      expect(tubes_json[1]['barcode']).to eq('TRAC-2-2')
+      expect(tubes_json[1]['materials']).to contain_exactly(
+        { 'name' => 'PLATE-1-1234-2', 'pool' => 2, 'wellRange' => 'A3-H4', 'poolSize' => 24 }
+      )
 
-      expect(libraries_json[2]['name']).to eq('PLATE-1-1234-3')
-      expect(libraries_json[2]['pool']).to eq(3)
-      expect(libraries_json[2]['wellRange']).to eq('A5-H6')
-      expect(libraries_json[2]['poolSize']).to eq(24)
+      expect(tubes_json[2]['barcode']).to eq('TRAC-2-3')
+      expect(tubes_json[2]['materials']).to contain_exactly(
+        { 'name' => 'PLATE-1-1234-3', 'pool' => 3, 'wellRange' => 'A5-H6', 'poolSize' => 24 }
+      )
 
-      expect(libraries_json[3]['name']).to eq('PLATE-1-1234-4')
-      expect(libraries_json[3]['pool']).to eq(4)
-      expect(libraries_json[3]['wellRange']).to eq('A7-H8')
-      expect(libraries_json[3]['poolSize']).to eq(24)
+      expect(tubes_json[3]['barcode']).to eq('TRAC-2-4')
+      expect(tubes_json[3]['materials']).to contain_exactly(
+        { 'name' => 'PLATE-1-1234-4', 'pool' => 4, 'wellRange' => 'A7-H8', 'poolSize' => 24 }
+      )
+
+      expect(mutation_json['errors']).to be_empty
     end
 
     it 'responds with errors provided by the request factory' do
@@ -91,7 +94,7 @@ RSpec.describe 'GraphQL', type: :request do
       json = ActiveSupport::JSON.decode(response.body)
 
       mutation_json = json['data']['createOntLibraries']
-      expect(mutation_json['libraries']).to be_empty
+      expect(mutation_json['tubes']).to be_empty
       expect(mutation_json['errors']).to contain_exactly('Libraries {:message=>"This is a test error"}')
     end
 
