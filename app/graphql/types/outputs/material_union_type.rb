5743788ae11fe6e0f2ad174bca8b22a8391a4feb
Christopher Friend <cf17@sanger.ac.uk>
2020-05-05 13:15:29 +0100

    Update MaterialUnionType with ont library type

--- app/graphql/types/outputs/material_union_type.rb
+++ app/graphql/types/outputs/material_union_type.rb
@@ -4,14 +4,16 @@ module Types
   module Outputs
     # The type for polymorphic Material objects.
     class MaterialUnionType < BaseUnion
-      possible_types Types::Outputs::Ont::RequestType
+      possible_types Types::Outputs::Ont::RequestType, Types::Outputs::Ont::LibraryType
 
       def self.resolve_type(object, _context)
-        unless object.is_a?(::Ont::Request)
-          raise "Can't determine GraphQL type for: #{object.inspect}"
+        if object.is_a?(::Ont::Request)
+          Types::Outputs::Ont::RequestType
+        elsif object.is_a?(::Ont::Library)
+          Types::Outputs::Ont::LibraryType
+        else
+          raise "Can't determine GraphQL type for: #{object.inspect}"  
         end
-
-        Types::Outputs::Ont::RequestType
       end
     end
   end
