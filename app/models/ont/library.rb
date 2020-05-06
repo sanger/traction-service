3619000c1dee9e80961c90333fafeef3b383043e
Christopher Friend <cf17@sanger.ac.uk>
2020-05-06 07:47:47 +0100

    Update Ont::Library to add plate_barcode. Remove the relationship between library and request

--- app/models/ont/library.rb
+++ app/models/ont/library.rb
@@ -5,8 +5,12 @@ module Ont
   class Library < ApplicationRecord
     include Material
 
-    has_many :requests, foreign_key: :ont_library_id, inverse_of: :library, dependent: :destroy
+    # has_many :requests, foreign_key: :ont_library_id, inverse_of: :library, dependent: :destroy
 
-    validates :name, :pool, :well_range, :pool_size, presence: true
+    validates :name, :plate_barcode, :pool, :well_range, :pool_size, presence: true
+
+    # Dynamically calculated/transient attributes:
+    # - tag_set_name
+    # - tube_barcode
   end
 end
