1958e4493ee9f44b1294ad775283b8e44ffb8774
Christopher Friend <cf17@sanger.ac.uk>
2020-05-07 08:43:42 +0100

    Add Ont::Flowcell

---
+++ app/models/ont/flowcell.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Ont
+  # Ont::Flowcell
+  class Flowcell < ApplicationRecord
+    include Uuidable
+
+    belongs_to :run, foreign_key: :ont_run_id, inverse_of: :flowcells, dependent: :destroy
+    has_one :library, foreign_key: :ont_flowcell_id, inverse_of: :flowcell, dependent: :nullify
+
+    validates :position, presence: true
+  end
+end
