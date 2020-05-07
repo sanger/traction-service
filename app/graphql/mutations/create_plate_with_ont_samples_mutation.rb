052556bc1b4771501bc0c3b33978f70d9241fef5
Stuart McHattie <sm49@sanger.ac.uk>
2020-05-07 15:39:46 +0100

    Update invalid comments on existing mutations

--- app/graphql/mutations/create_plate_with_ont_samples_mutation.rb
+++ app/graphql/mutations/create_plate_with_ont_samples_mutation.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Mutations
-  # Mutation to handle the update of a Well.
+  # Mutation to create a plate with ONT samples.
   class CreatePlateWithOntSamplesMutation < BaseMutation
     argument :arguments, Types::Inputs::PlateWithSamplesInputType, required: true
 
