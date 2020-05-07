6e107e528eef665583c167e811fe0e764dcbf7b5
Christopher Friend <cf17@sanger.ac.uk>
2020-05-07 09:07:37 +0100

    Update relationship between flowcell and library

--- db/migrate/20200507071910_create_ont_flowcells.rb
+++ db/migrate/20200507071910_create_ont_flowcells.rb
@@ -4,11 +4,8 @@ class CreateOntFlowcells < ActiveRecord::Migration[6.0]
       t.integer :position
       t.string :uuid
       t.belongs_to :ont_run, index: true
+      t.belongs_to :ont_library, index: true
       t.timestamps
-    end
-
-    change_table :ont_libraries do |t|
-      t.belongs_to :ont_flowcell, null: true, index: true
     end
   end
 end
