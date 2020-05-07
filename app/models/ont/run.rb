98379793144915cc9ffc06a2148f0effdc6ab667
Christopher Friend <cf17@sanger.ac.uk>
2020-05-07 08:18:43 +0100

    Add additional functionality to an Ont::Run and test

--- app/models/ont/run.rb
+++ app/models/ont/run.rb
@@ -4,5 +4,17 @@ module Ont
   # Ont::Run
   class Run < ApplicationRecord
     include Stateful
+
+    scope :active, -> { where(deactivated_at: nil) }
+
+    def active?
+      deactivated_at.nil?
+    end
+
+    def cancel
+      return true unless active?
+
+      update(deactivated_at: DateTime.current)
+    end
   end
 end
