97d53fb70e102dd8755d76604c6beb7394391c64
Christopher Friend <cf17@sanger.ac.uk>
2020-05-07 10:47:35 +0100

    Allow a request to exist in one library only

--- app/models/ont/library_request.rb
+++ app/models/ont/library_request.rb
@@ -5,7 +5,7 @@ module Ont
   class LibraryRequest < ApplicationRecord
     belongs_to :library, foreign_key: :ont_library_id,
                          inverse_of: :library_requests, dependent: :destroy
-    belongs_to :request, foreign_key: :ont_request_id, inverse_of: :library_requests
+    belongs_to :request, foreign_key: :ont_request_id, inverse_of: :library_request
     belongs_to :tag, foreign_key: :tag_id, inverse_of: false
   end
 end
--- app/models/ont/request.rb
+++ app/models/ont/request.rb
@@ -8,8 +8,8 @@ module Ont
 
     has_one :request, class_name: '::Request', as: :requestable, dependent: :nullify
     has_one :sample, through: :request
-    has_many :library_requests, foreign_key: :ont_request_id,
-                                inverse_of: :request, dependent: :destroy
+    has_one :library_request, foreign_key: :ont_request_id,
+                              inverse_of: :request, dependent: :destroy
 
     validates :external_study_id, presence: true
 
