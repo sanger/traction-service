00743210e313c9fde887c77e4d6386bb3a681e41
Christopher Friend <cf17@sanger.ac.uk>
2020-05-12 09:08:33 +0100

    Allow generating a plate with tagged ont requests

--- spec/factories/plates.rb
+++ spec/factories/plates.rb
@@ -10,7 +10,7 @@ FactoryBot.define do
         fail if options.row_count > 8
         ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'].first(options.row_count).each do |row|
           (1..options.column_count).each do |col|
-            create(:well, position: "#{row}#{col}", plate: plate)
+            create(:well, position: "#{row}#{col}", plate: plate,)
           end
         end
       end
@@ -24,6 +24,22 @@ FactoryBot.define do
       after :create do |plate, options|
         options.wells.each do |well_spec|
           plate.wells << create(:well_with_ont_requests, plate: plate, position: well_spec[:position], requests: well_spec[:requests])
+        end
+      end
+    end
+
+    factory :plate_with_tagged_ont_requests do
+      transient do
+        row_count { 1 }
+        column_count { 3 }
+      end
+  
+      after :create do |plate, options|
+        fail if options.row_count > 8
+        ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'].first(options.row_count).each do |row|
+          (1..options.column_count).each do |col|
+            create(:well_with_ont_requests, position: "#{row}#{col}", plate: plate, requests: [create(:ont_request_with_tags, tags_count: 1)])
+          end
         end
       end
     end
