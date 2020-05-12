efcf9b3e164b48bcb3430250f69bf5182672de56
Christopher Friend <cf17@sanger.ac.uk>
2020-05-12 08:45:58 +0100

    Update covid libraries creation through graphql

--- spec/requests/v2/ont/libraries_spec.rb
+++ spec/requests/v2/ont/libraries_spec.rb
@@ -51,14 +51,10 @@ RSpec.describe 'GraphQL', type: :request do
     end
 
     it 'creates tubes with libraries with provided parameters' do
-      libraries = create_list(:ont_library_in_tube, 4).each_with_index do |library, i|
-        library.pool = i+1
-        library.name = "PLATE-1-1234-#{i+1}"
-        library.save
-      end
+      libraries = create(:ont_library_in_tube)
 
       allow_any_instance_of(Ont::LibraryFactory).to receive(:save).and_return(true)
-      allow_any_instance_of(Ont::LibraryFactory).to receive(:tubes).and_return(Tube.all)
+      allow_any_instance_of(Ont::LibraryFactory).to receive(:tube).and_return(Tube.first)
 
       post v2_path, params: { query: valid_query }
       expect(response).to have_http_status(:success)
@@ -69,22 +65,7 @@ RSpec.describe 'GraphQL', type: :request do
 
       expect(tubes_json[0]['barcode']).to be_present
       expect(tubes_json[0]['materials']).to contain_exactly(
-        { 'name' => 'PLATE-1-1234-1', 'pool' => 1, 'poolSize' => 24 }
-      )
-
-      expect(tubes_json[1]['barcode']).to be_present
-      expect(tubes_json[1]['materials']).to contain_exactly(
-        { 'name' => 'PLATE-1-1234-2', 'pool' => 2, 'poolSize' => 24 }
-      )
-
-      expect(tubes_json[2]['barcode']).to be_present
-      expect(tubes_json[2]['materials']).to contain_exactly(
-        { 'name' => 'PLATE-1-1234-3', 'pool' => 3, 'poolSize' => 24 }
-      )
-
-      expect(tubes_json[3]['barcode']).to be_present
-      expect(tubes_json[3]['materials']).to contain_exactly(
-        { 'name' => 'PLATE-1-1234-4', 'pool' => 4, 'poolSize' => 24 }
+        { 'name' => 'PLATE-1-123456-2', 'pool' => 2, 'poolSize' => 24 }
       )
 
       expect(mutation_json['errors']).to be_empty
