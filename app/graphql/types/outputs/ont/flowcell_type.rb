e0568dd285ce2e9ab37717dca50b6e420240b605
Stuart McHattie <sm49@sanger.ac.uk>
2020-05-12 16:55:02 +0100

    Add descriptions to new GraphQL endpoints

--- app/graphql/types/outputs/ont/flowcell_type.rb
+++ app/graphql/types/outputs/ont/flowcell_type.rb
@@ -5,9 +5,10 @@ module Types
     module Ont
       # The type for Ont::Flowcell objects.
       class FlowcellType < CommonOutputObject
-        field :position, Integer, null: false
-        field :uuid, String, null: false
-        field :library, LibraryType, null: false
+        field :position, Integer, 'The numerical position of this flowcell in the GriION machine.',
+              null: false
+        field :uuid, String, 'The UUID of this flowcell.', null: false
+        field :library, LibraryType, 'The library loaded onto this flowcell.', null: false
       end
     end
   end
