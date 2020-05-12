faf3a84c464f7d71fb96287987ccd8d684ab8cf1
Christopher Friend <cf17@sanger.ac.uk>
2020-05-12 13:23:22 +0100

    Remove references to instrument_name

--- spec/factories/ont/runs.rb
+++ spec/factories/ont/runs.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :ont_run, class: Ont::Run do
-    instrument_name { 'GridION' }
-
     after(:create) do |run|
       run.flowcells = [
         create(:ont_flowcell, position: 2, run: run),
