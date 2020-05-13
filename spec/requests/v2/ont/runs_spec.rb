e1e23a5ee8041ec7a6171bfae03c0e806cefe06c
Stuart McHattie <sm49@sanger.ac.uk>
2020-05-13 13:33:30 +0100

    Add runs endpoint to GraphQL

--- spec/requests/v2/ont/runs_spec.rb
+++ spec/requests/v2/ont/runs_spec.rb
@@ -1,27 +1,56 @@
 # frozen_string_literal: true
 
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe 'GraphQL', type: :request do
-  let(:run_factory) { instance_double("Ont::RunFactory") }
+  context 'get runs' do
+    it 'returns empty array when no runs exist' do
+      post v2_path, params: { query: '{ ontRuns { id } }' }
+      expect(response).to have_http_status(:success)
+      json = ActiveSupport::JSON.decode(response.body)
+      expect(json['data']['ontRuns']).to be_empty
+    end
 
-  before do
-    allow(Ont::RunFactory).to receive(:new).and_return(run_factory)
+    it 'returns single run when one exists' do
+      run = create(:ont_run)
+      post v2_path, params: { query:
+        '{ ontRuns { id state deactivatedAt flowcells { id } } }' }
+      expect(response).to have_http_status(:success)
+      json = ActiveSupport::JSON.decode(response.body)
+      expect(json['data']['ontRuns']).to contain_exactly(
+        { 'id' => run.id.to_s, 'state' => run.state.to_s, 'deactivatedAt' => run.deactivated_at,
+          'flowcells' => run.flowcells.map { |fc| { 'id' => fc.id.to_s } } }
+      )
+    end
+
+    it 'returns all runs when many exist' do
+      create_list(:ont_run, 3)
+      post v2_path, params: { query: '{ ontRuns { id } }' }
+      expect(response).to have_http_status(:success)
+      json = ActiveSupport::JSON.decode(response.body)
+      expect(json['data']['ontRuns'].length).to eq(3)
+    end
   end
 
   context 'create run' do
+    let(:run_factory) { instance_double('Ont::RunFactory') }
+
+    before do
+      allow(Ont::RunFactory).to receive(:new).and_return(run_factory)
+    end
+
     def valid_query
       <<~GQL
-      mutation {
-        createCovidRun(
-          input: {
-            flowcells: []
+        mutation {
+          createCovidRun(
+            input: {
+              flowcells: []
+            }
+          ) {
+            run { id state flowcells { position library { name } } }
+            errors
           }
-        ) {
-          run { id state flowcells { position library { name } } }
-          errors
         }
-      }
       GQL
     end
 
@@ -43,7 +72,7 @@ RSpec.describe 'GraphQL', type: :request do
       flowcell_json = run_json['flowcells']
       expect(flowcell_json.count).to eq(run.flowcells.count)
       expect(flowcell_json.map { |fc| fc['position'] })
-        .to match_array(run.flowcells.map { |fc| fc.position })
+        .to match_array(run.flowcells.map(&:position))
 
       expect(flowcell_json.map { |fc| fc['library']['name'] })
         .to match_array(run.flowcells.map { |fc| fc.library.name })
@@ -52,7 +81,7 @@ RSpec.describe 'GraphQL', type: :request do
     it 'responds with errors provided by the run factory' do
       errors = Class.new do
         def full_messages
-          [ 'test error' ]
+          ['test error']
         end
       end.new
 
