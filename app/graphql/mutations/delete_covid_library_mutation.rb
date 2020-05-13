09dae94d1902518190209340bfc37397cc4bf8bd
Christopher Friend <cf17@sanger.ac.uk>
2020-05-13 09:00:44 +0100

    Add DeleteCovidLibrary mutation

---
+++ app/graphql/mutations/delete_covid_library_mutation.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Mutations
+  # Mutation to delete a single ONT library
+  class DeleteCovidLibraryMutation < BaseMutation
+    argument :library_name, String, 'The name of the library to delete.', required: true
+
+    field :success, Boolean, 'Whether the library was successfully deleted.', null: false
+    field :errors, [String], 'An array of error messages thrown when deleting the library',
+          null: false
+
+    def resolve(library_name:)
+      library = Ont::Library.find_by(library_name: library_name)
+
+      if library.nil?
+        { success: false, errors: ["Library with name '#{library_name}' does not exist"] }
+      elsif !library.flowcell.nil?
+        { success: false, errors: ['Cannot delete a library that is used in a run'] }
+      elsif library.destroy
+        { success: true, errors: [] }
+      else
+        { success: false, errors: flowcell.errors.full_messages }
+      end
+    end
+  end
+end
