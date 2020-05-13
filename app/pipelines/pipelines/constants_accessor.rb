cc789acfab481d64f160653ec4777fcc88e06ca7
Christopher Friend <cf17@sanger.ac.uk>
2020-05-13 07:51:47 +0100

    Revert "Remove constants accessor"
    This reverts commit f09b2b8f3d6baef8587e0c2dec81ed2b46cd2739.

---
+++ app/pipelines/pipelines/constants_accessor.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Pipelines
+  # An accessor for constant values in the pipeline for configuration options
+  class ConstantsAccessor
+    def initialize(base)
+      @constants_base = base
+    end
+
+    def external_study_id
+      @constants_base.external_study_id
+    end
+
+    def species
+      @constants_base.species
+    end
+  end
+end
