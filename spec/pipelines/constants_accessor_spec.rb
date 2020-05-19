48fca8654f241279952e5c464718f35e5843581c
Christopher Friend <cf17@sanger.ac.uk>
2020-05-19 09:12:37 +0100

    Allow getting tag set hostname from constants accessor

--- spec/pipelines/constants_accessor_spec.rb
+++ spec/pipelines/constants_accessor_spec.rb
@@ -7,8 +7,16 @@ RSpec.describe Pipelines::ConstantsAccessor, type: :model do
     let(:constants_accessor) {
       Pipelines::ConstantsAccessor.new(
         Class.new do
-          def pcr_tag_set_name
-            'test tag set name'
+          def pcr_tag_set
+            Class.new do
+              def name
+                'test tag set name'
+              end
+
+              def hostname
+                'test hostname'
+              end
+            end.new
           end
         end.new
       )
@@ -16,6 +24,10 @@ RSpec.describe Pipelines::ConstantsAccessor, type: :model do
 
     it 'will return the pcr tag set name' do
       expect(constants_accessor.pcr_tag_set_name).to eq('test tag set name')
+    end
+
+    it 'will return the pcr tag set hostname' do
+      expect(constants_accessor.pcr_tag_set_hostname).to eq('test hostname')
     end
   end
 end
