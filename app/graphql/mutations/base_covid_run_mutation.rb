46c9238ef3571db395388362aa8356be3d52b81e
Christopher Friend <cf17@sanger.ac.uk>
2020-05-21 10:04:54 +0100

    Add performance improvements methods to Ont::Run

--- app/graphql/mutations/base_covid_run_mutation.rb
+++ app/graphql/mutations/base_covid_run_mutation.rb
@@ -6,9 +6,7 @@ module Mutations
     protected
 
     def send_messages(run:)
-      resolved_run = Ont::Run.includes(flowcells: { library: { requests: { tags: :tag_set } } })
-                             .find_by(id: run.id)
-      Messages.publish(resolved_run, Pipelines.ont.message)
+      Messages.publish(run.resolved_run, Pipelines.ont.message)
     end
   end
 end
