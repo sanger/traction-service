d75ea6764a60856bdf7b37e3a9e97e9e3c34d7be
Christopher Friend <cf17@sanger.ac.uk>
2020-05-27 16:04:49 +0100

    Rubocop fixes

--- app/models/ont/plate_bulk_inserter.rb
+++ app/models/ont/plate_bulk_inserter.rb
@@ -6,11 +6,11 @@ module Ont
   # A service for bulk inserting a plate and all of its dependents
   class PlateBulkInserter
     def serialise_request(request)
-      # TODO (27/05/2020) - implement
+      # TODO: (27/05/2020) - implement
     end
 
     def serialise_well(well)
-      # TODO (27/05/2020) - implement
+      # TODO: (27/05/2020) - implement
     end
   end
 end
