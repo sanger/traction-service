6747c221cc86f0c1763985f36b6f3a0c79775977
Christopher Friend <cf17@sanger.ac.uk>
2020-05-27 15:16:07 +0100

    Update the request factory to instead respond to bulk_insert_serialise method calls

--- spec/models/ont/request_factory_spec.rb
+++ spec/models/ont/request_factory_spec.rb
@@ -3,7 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe Ont::RequestFactory, type: :model, ont: true do
-  let(:well) { create(:well) }
   let(:tag_set_name) { 'OntWell96Samples' }
   let(:tag_set) { create(:tag_set_with_tags, name: tag_set_name) }
 
@@ -14,16 +13,8 @@ RSpec.describe Ont::RequestFactory, type: :model, ont: true do
   end
 
   context '#initialise' do
-    it 'is not valid if given no well' do
-      attributes = { request_attributes: { external_id: '1' } }
-      factory = Ont::RequestFactory.new(attributes)
-      expect(factory).to_not be_valid
-      expect(factory.errors.full_messages).to_not be_empty
-    end
-
     it 'is not valid if given no attributes' do
-      attributes = { well: well }
-      factory = Ont::RequestFactory.new(attributes)
+      factory = Ont::RequestFactory.new()
       expect(factory).to_not be_valid
       expect(factory.errors.full_messages).to_not be_empty
     end
@@ -31,11 +22,8 @@ RSpec.describe Ont::RequestFactory, type: :model, ont: true do
     it 'is not valid if the generated ont request is not valid' do
       # request attributes should include a name
       attributes = {
-        well: well,
-        request_attributes: {
-          external_id: '1',
-          tag_oligoo: tag_set.tags.first.oligo
-        }
+        external_id: '1',
+        tag_oligoo: tag_set.tags.first.oligo
       }
       factory = Ont::RequestFactory.new(attributes)
       expect(factory).to_not be_valid
@@ -44,12 +32,9 @@ RSpec.describe Ont::RequestFactory, type: :model, ont: true do
 
     it 'is not valid if no matching tag exists' do
       attributes = {
-        well: well,
-        request_attributes: {
-          name: 'sample 1',
-          external_id: '1',
-          tag_oligo: 'NOT_AN_OLIGO'
-        }
+        name: 'sample 1',
+        external_id: '1',
+        tag_oligo: 'NOT_AN_OLIGO'
       }
       factory = Ont::RequestFactory.new(attributes)
       expect(factory).to_not be_valid
@@ -59,12 +44,9 @@ RSpec.describe Ont::RequestFactory, type: :model, ont: true do
     it 'is not valid if the tag is not in the correct tag set' do
       wrong_tag_set = create(:tag_set_with_tags, name: 'WrongTagSet')
       attributes = {
-        well: well,
-        request_attributes: {
-          name: 'sample 1',
-          external_id: '1',
-          tag_oligo: wrong_tag_set.tags.first.oligo
-        }
+        name: 'sample 1',
+        external_id: '1',
+        tag_oligo: wrong_tag_set.tags.first.oligo
       }
       factory = Ont::RequestFactory.new(attributes)
       expect(factory).to_not be_valid
@@ -72,77 +54,48 @@ RSpec.describe Ont::RequestFactory, type: :model, ont: true do
     end
   end
 
-  context '#save' do
+  context '#bulk_insert_serialise' do
+    let(:plate_bulk_inserter) {
+      Class.new do
+        def serialise_request(_request)
+          { example: 'serialisation' }
+        end
+      end.new
+    }
     context 'valid build' do
       let(:attributes) do
         {
-          well: well,
-          request_attributes: {
-            name: 'sample 1',
-            external_id: '1',
-            tag_oligo: tag_set.tags.first.oligo
-          }
+          name: 'sample 1',
+          external_id: '1',
+          tag_oligo: tag_set.tags.first.oligo
         }
       end
       let(:factory) { Ont::RequestFactory.new(attributes) }
-
-      before do
-        factory.save
-      end
 
       it 'is valid with given attributes' do
         expect(factory).to be_valid
       end
 
-      it 'creates ont request' do
-        expect(Ont::Request.all.count).to eq(1)
-        expect(Ont::Request.first.name).to eq('sample 1')
-        expect(Ont::Request.first.external_id).to eq('1')
-      end
-
-      it 'creates tag_taggable' do
-        expect(::TagTaggable.count).to eq(1)
-        expect(::TagTaggable.first.tag).to eq(tag_set.tags.first)
-        expect(::TagTaggable.first.taggable).to eq(Ont::Request.first)
-      end
-
-      it 'creates a container material' do
-        expect(factory.save).to be_truthy
-        expect(::ContainerMaterial.all.count).to eq(1)
-        expect(::ContainerMaterial.first.container).to eq(::Well.where(position: 'A1').first)
-        expect(::ContainerMaterial.first.material).to eq(Ont::Request.first)
-      end
-
-      it 'validates the tag taggable only once by default' do
-        validation_count = 0
-        allow_any_instance_of(TagTaggable).to receive(:valid?) { |_| validation_count += 1 }
-        factory.save
-        expect(validation_count).to eq(1)
-      end
-
-      it 'validates the container material join only once by default' do
-        validation_count = 0
-        allow_any_instance_of(ContainerMaterial).to receive(:valid?) { |_| validation_count += 1 }
-        factory.save
-        expect(validation_count).to eq(1)
+      it 'has expected response' do
+        response = factory.bulk_insert_serialise(plate_bulk_inserter)
+        expect(response).to eq({
+          request: { example: 'serialisation' },
+          tag_id: tag_set.tags.first.id
+        })
       end
 
       it 'validates the ONT request only once by default' do
         validation_count = 0
         allow_any_instance_of(Ont::Request).to receive(:valid?) { |_| validation_count += 1 }
-        factory.save
+        factory.bulk_insert_serialise(plate_bulk_inserter)
         expect(validation_count).to be >= 1
         expect(validation_count).to eq(1)
       end
 
       it 'validates no children when (validate: false) is passed' do
         validation_count = 0
-        allow_any_instance_of(Request).to receive(:valid?) { |_| validation_count += 1 }
-        allow_any_instance_of(TagTaggable).to receive(:valid?) { |_| validation_count += 1 }
-        allow_any_instance_of(ContainerMaterial).to receive(:valid?) { |_| validation_count += 1 }
         allow_any_instance_of(Ont::Request).to receive(:valid?) { |_| validation_count += 1 }
-        allow_any_instance_of(Sample).to receive(:valid?) { |_| validation_count += 1 }
-        factory.save(validate: false)
+        factory.bulk_insert_serialise(plate_bulk_inserter, validate: false)
         expect(validation_count).to eq(0)
       end
     end
@@ -150,28 +103,13 @@ RSpec.describe Ont::RequestFactory, type: :model, ont: true do
     context 'invalid build' do
       let(:factory) { Ont::RequestFactory.new({}) }
 
-      before do
-        factory.save
-      end
-
       it 'is invalid' do
         expect(factory).to_not be_valid
       end
 
-      it 'returns false on save' do
-        expect(factory.save).to be_falsey
-      end
-
-      it 'does not create an ont request' do
-        expect(Ont::Request.all.count).to eq(0)
-      end
-
-      it 'does not create a join' do
-        expect(::ContainerMaterial.all.count).to eq(0)
-      end
-
-      it 'does not create a tag taggable' do
-        expect(TagTaggable.all.count).to eq(0)
+      it 'returns false' do
+        response = factory.bulk_insert_serialise(plate_bulk_inserter)
+        expect(response).to be_falsey
       end
     end
   end
