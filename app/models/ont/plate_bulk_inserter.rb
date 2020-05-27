a24be03308930f0c0af0444e3806de9520e24cc9
Christopher Friend <cf17@sanger.ac.uk>
2020-05-27 16:00:59 +0100

    Add partial, unimplemented Ont::PlateBulkInserter

---
+++ app/models/ont/plate_bulk_inserter.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Ont namespace
+module Ont
+  # PlateBulkInserter
+  # A service for bulk inserting a plate and all of its dependents
+  class PlateBulkInserter
+    def serialise_request(request)
+      # TODO (27/05/2020) - implement
+    end
+
+    def serialise_well(well)
+      # TODO (27/05/2020) - implement
+    end
+  end
+end
