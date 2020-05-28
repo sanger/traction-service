df3cfa7bbc2cb5ae7ef467f563fd2760545eaf8f
Christopher Friend <cf17@sanger.ac.uk>
2020-05-28 09:57:34 +0100

    Update bulk inserter to wrap around the existing plate factory

--- app/models/ont/plate_with_samples_factory.rb
+++ app/models/ont/plate_with_samples_factory.rb
@@ -5,42 +5,45 @@ module Ont
   # PlateWithSamplesFactory
   # A factory for bulk inserting a plate and all of its dependents
   class PlateWithSamplesFactory
-    def initialize
-      time = DateTime.now
-      @timestamps = { created_at: time, updated_at: time }
+    include ActiveModel::Model
+
+    validate :check_plate_factory
+
+    def initialize(attributes = {})
+      @attributes = attributes
+    end
+
+    def process
+      @plate_factory = Ont::PlateFactory.new(attributes)
+    end
+
+    def save(**options)
+      return false unless options[:validate] == false || valid?
+
+      @serialised_plate_data = plate_factory.bulk_insert_serialise(self, validate: false)
+      bulk_insert
     end
 
     def ont_request_data(ont_request, tag_id)
-      {
-        ont_request: serialise_ont_request(ont_request),
-        tag_id: tag_id
-      }
+      { ont_request: serialise_ont_request(ont_request), tag_id: tag_id }
     end
 
     def well_data(well, request_data)
-      {
-        well: serialise_well(well),
-        request_data: request_data
-      }
+      { well: serialise_well(well), request_data: request_data }
     end
 
     def plate_data(plate, well_data)
-      {
-        plate: serialise_plate(plate),
-        well_data: well_data
-      }
-    end
-
-    def bulk_insert(plate_data)
-      # insert plate (save)
-      # update wells with plate id
-      # insert wells
-      # other things
+      { plate: serialise_plate(plate), well_data: well_data }
     end
 
     private
 
-    attr_reader :timestamps
+    attr_reader :attributes, :timestamps, :serialised_plate_data
+
+    def timestamps
+      time = DateTime.now
+      @timestamps ||= { created_at: time, updated_at: time }
+    end
 
     def serialise_ont_request(ont_request)
       {
@@ -56,6 +59,26 @@ module Ont
 
     def serialise_plate(plate)
       { barcode: plate.barcode }.merge(timestamps)
+    end
+
+    def check_plate_factory
+      return if plate_factory.valid?
+
+      plate_factory.errors.each do |k, v|
+        errors.add(k, v)
+      end
+    end
+
+    def bulk_insert
+      # return the plate on success; otherwise false. Unsuccessful inserts should also generate errors
+      ActiveRecord::Base.transaction do
+        # insert plate (save)
+        # update wells with plate id
+        # insert wells
+        # other things
+        plate
+      end
+      false
     end
   end
 end
--- spec/models/ont/plate_with_samples_factory_spec.rb
+++ spec/models/ont/plate_with_samples_factory_spec.rb
@@ -2,62 +2,65 @@ require 'rails_helper'
 
 RSpec.describe Ont::PlateWithSamplesFactory, type: :model, ont: true do
   let(:factory) { Ont::PlateWithSamplesFactory.new }
-  let(:time) { DateTime.now }
 
-  before do
-    allow(DateTime).to receive(:now).and_return(time)
-  end
+  context 'bulk insert helper' do
+    let(:time) { DateTime.now }
 
-  context 'ont_request_data' do
-    let(:request) { create(:ont_request) }
-    let(:tag_id) { 'test tag id' }
-
-    it 'returns expected serialisation' do
-      ont_request_data = factory.ont_request_data(request, tag_id)
-      expect(ont_request_data).to eq({
-        ont_request: {
-          uuid: request.uuid,
-          external_id: request.external_id,
-          name: request.name,
-          created_at: time,
-          updated_at: time
-        },
-        tag_id: tag_id
-      })
+    before do
+      allow(DateTime).to receive(:now).and_return(time)
     end
-  end
-
-  context 'well_data' do
-    let(:well) { create(:well) }
-    let(:request_data) { 'test request data' }
-
-    it 'returns expected serialisation' do
-      well_data = factory.well_data(well, request_data)
-      expect(well_data).to eq({
-        well: {
-          position: well.position,
-          created_at: time,
-          updated_at: time
-        },
-        request_data: request_data
-      })
+    
+    context 'ont_request_data' do
+      let(:request) { create(:ont_request) }
+      let(:tag_id) { 'test tag id' }
+  
+      it 'returns expected serialisation' do
+        ont_request_data = factory.ont_request_data(request, tag_id)
+        expect(ont_request_data).to eq({
+          ont_request: {
+            uuid: request.uuid,
+            external_id: request.external_id,
+            name: request.name,
+            created_at: time,
+            updated_at: time
+          },
+          tag_id: tag_id
+        })
+      end
     end
-  end
-
-  context 'plate_data' do
-    let(:plate) { create(:plate) }
-    let(:well_data) { 'test well data' }
-
-    it 'returns expected serialisation' do
-      plate_data = factory.plate_data(plate, well_data)
-      expect(plate_data).to eq({
-        plate: {
-          barcode: plate.barcode,
-          created_at: time,
-          updated_at: time
-        },
-        well_data: well_data
-      })
+  
+    context 'well_data' do
+      let(:well) { create(:well) }
+      let(:request_data) { 'test request data' }
+  
+      it 'returns expected serialisation' do
+        well_data = factory.well_data(well, request_data)
+        expect(well_data).to eq({
+          well: {
+            position: well.position,
+            created_at: time,
+            updated_at: time
+          },
+          request_data: request_data
+        })
+      end
+    end
+  
+    context 'plate_data' do
+      let(:plate) { create(:plate) }
+      let(:well_data) { 'test well data' }
+  
+      it 'returns expected serialisation' do
+        plate_data = factory.plate_data(plate, well_data)
+        expect(plate_data).to eq({
+          plate: {
+            barcode: plate.barcode,
+            created_at: time,
+            updated_at: time
+          },
+          well_data: well_data
+        })
+      end
     end
   end
 end
