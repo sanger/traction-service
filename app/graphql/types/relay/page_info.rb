aa167d784f672ae1eee175b988543214b4bfe289
Stuart McHattie <sm49@sanger.ac.uk>
2020-05-29 11:51:22 +0100

    Add total page count to the page info object

---
+++ app/graphql/types/relay/page_info.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Types
+  module Relay
+    # The return type of a connection's `pageInfo` field
+    class PageInfo < GraphQL::Types::Relay::BaseObject
+      default_relay true
+      description 'Information about pagination in a connection.'
+
+      field :has_next_page, Boolean,
+            null: false, description: 'When paginating forwards, are there more items?'
+
+      field :has_previous_page, Boolean,
+            null: false, description: 'When paginating backwards, are there more items?'
+
+      field :page_count, Integer,
+            null: false, description: 'The number of pages available at the current page size.'
+    end
+  end
+end
