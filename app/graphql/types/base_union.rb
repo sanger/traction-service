baf7f6b4d6cc1044f305b16317a46e31da315000
Harriet Craven <hc6@sanger.ac.uk>
2020-06-05 16:29:28 +0100

    Reset to ca345e63a81e4914efa399a601c1afad9ca63da9

--- app/graphql/types/base_union.rb
+++ app/graphql/types/base_union.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 module Types
-  # The base union for GraphQL.
   class BaseUnion < GraphQL::Schema::Union
-    connection_type_class(Types::Connections::BaseConnectionObject)
   end
 end
--- spec/requests/v2/ont/runs_spec.rb
+++ spec/requests/v2/ont/runs_spec.rb
@@ -35,27 +35,33 @@ RSpec.describe 'GraphQL', type: :request do
     end
   end
 
-  describe 'get runs' do
-    describe 'paginated graphql query' do
-      let(:paginated_model) { :ont_run }
-      let(:graphql_method) { 'ontRuns' }
-      it_behaves_like 'paginated_query'
+  context 'get runs' do
+    it 'returns empty array when no runs exist' do
+      post v2_path, params: { query: '{ ontRuns { id } }' }
+      expect(response).to have_http_status(:success)
+      json = ActiveSupport::JSON.decode(response.body)
+      expect(json['data']['ontRuns']).to be_empty
     end
 
-    context 'when single run with flowcells' do
-      let!(:run) { create(:ont_run) }
+    it 'returns single run when one exists' do
+      run = create(:ont_run)
+      post v2_path, params: { query:
+        '{ ontRuns { id state deactivatedAt flowcells { id } } }' }
+      expect(response).to have_http_status(:success)
+      json = ActiveSupport::JSON.decode(response.body)
+      expect(json['data']['ontRuns']).to contain_exactly(
+        { 'id' => run.id.to_s, 'state' => run.state.to_s.upcase,
+          'deactivatedAt' => run.deactivated_at,
+          'flowcells' => run.flowcells.map { |fc| { 'id' => fc.id.to_s } } }
+      )
+    end
 
-      it 'returns single run with flowcell ids' do
-        post v2_path, params: { query:
-          '{ ontRuns { nodes { id state deactivatedAt flowcells { id } } } }' }
-        expect(response).to have_http_status(:success)
-        json = ActiveSupport::JSON.decode(response.body)
-        expect(json['data']['ontRuns']['nodes']).to contain_exactly(
-          { 'id' => run.id.to_s, 'state' => run.state.to_s.upcase,
-            'deactivatedAt' => run.deactivated_at,
-            'flowcells' => run.flowcells.map { |fc| { 'id' => fc.id.to_s } } }
-        )
-      end
+    it 'returns all runs when many exist' do
+      create_list(:ont_run, 3)
+      post v2_path, params: { query: '{ ontRuns { id } }' }
+      expect(response).to have_http_status(:success)
+      json = ActiveSupport::JSON.decode(response.body)
+      expect(json['data']['ontRuns'].length).to eq(3)
     end
   end
 
