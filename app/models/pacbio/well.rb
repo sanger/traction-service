8bcd46f15d755d9b209ab610f6f4b9ca1393032b
Katy Taylor <kt17@sanger.ac.uk>
2020-08-10 15:37:07 +0100

    oops, accidentally made the field required by adding this validation - allow blank

--- app/models/pacbio/well.rb
+++ app/models/pacbio/well.rb
@@ -21,7 +21,7 @@ module Pacbio
     validates :movie_time,
               numericality: { greater_than_or_equal_to: 0.1, less_than_or_equal_to: 30 }
     validates :insert_size, numericality: { greater_than_or_equal_to: 10 }
-    validates :pre_extension_time, numericality: { only_integer: true }
+    validates :pre_extension_time, numericality: { only_integer: true }, :allow_blank => true
 
     def position
       "#{row}#{column}"
