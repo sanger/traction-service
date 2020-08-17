7a2b47d264be85a71151de6955941fe58b2dbdeb
Ben Topping <ben.topping@outlook.com>
2020-08-17 14:22:29 +0100

    automation parameters in sample sheet now checks for pre-extension time 0 and sequencing mode CLR

--- spec/models/concerns/sample_sheet_spec.rb
+++ spec/models/concerns/sample_sheet_spec.rb
@@ -87,5 +87,13 @@ RSpec.describe SampleSheet do
         expect(well.automation_parameters).to be_nil
       end
     end
+
+    context 'pre-extension time 0 and sequencing mode CLR' do
+      let(:well) { create(:pacbio_well, pre_extension_time: 0, sequencing_mode: 'CLR') }
+
+      it 'automation parameters is blank' do
+        expect(well.automation_parameters).to be_nil
+      end
+    end
   end
 end
