cfc3d80099e9982d8eb439dbf2f35647a32aa883
Ben Topping <ben.topping@outlook.com>
2020-10-26 16:28:53 +0000

    linted

---
+++ spec/requests/v1/pacbio/request_library_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.describe 'RequestLibraryController', type: :request do
+
+    context '#update' do
+        let(:tag) { create(:tag) }
+        let(:request_library) { create(:pacbio_request_library) }
+
+        context 'on success' do
+            let(:body) do
+                {
+                data: {
+                    type: "tags",
+                    id: request_library.id,
+                    attributes: {
+                    tag_id: tag.id
+                    }
+                }
+                }.to_json
+            end
+
+            it 'has a ok status' do
+                patch v1_pacbio_request_library_path(request_library), params: body, headers: json_api_headers
+                expect(response).to have_http_status(:ok)
+            end
+
+            it 'returns the correct attributes' do
+                patch v1_pacbio_request_library_path(request_library), params: body, headers: json_api_headers
+                json = ActiveSupport::JSON.decode(response.body)
+                expect(json['data']['id']).to eq request_library.id.to_s
+            end
+        end
+
+        #context 'on failure' cannot happen since there is no validation as you cannot send a requestLibrary without a tag_id
+    end
+
+end
