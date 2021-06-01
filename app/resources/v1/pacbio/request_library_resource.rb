9fd894b3c052c7102b13cb4559bb7a62ca5defda
James Glover <james.glover@sanger.ac.uk>
2021-06-01 15:09:50 +0100

    Ensure correct records loaded

--- app/resources/v1/pacbio/request_library_resource.rb
+++ app/resources/v1/pacbio/request_library_resource.rb
@@ -8,7 +8,7 @@ module V1
 
       attributes :sample_name, :tag_oligo, :tag_group_id, :tag_set_name, :tag_id
 
-      def self.records(*args)
+      def self.records_for_populate(*_args)
         super.preload(tag: :tag_set, request: :sample)
       end
     end
