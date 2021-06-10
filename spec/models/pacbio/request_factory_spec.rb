3bdfa81aa3bd063fb96f3f152d9321dcc82d2f06
James Glover <james.glover@sanger.ac.uk>
2021-06-10 15:57:46 +0100

    Allow request factory to set barcode

--- spec/models/pacbio/request_factory_spec.rb
+++ spec/models/pacbio/request_factory_spec.rb
@@ -3,10 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe Pacbio::RequestFactory, type: :model, pacbio: true do
-  let(:attributes) { [attributes_for(:sample).merge(attributes_for(:pacbio_request)), 
-                      attributes_for(:sample).merge(attributes_for(:pacbio_request)),
-                      attributes_for(:sample).merge(attributes_for(:pacbio_request))] }
+  let(:attributes) do
+    [
+      attributes_for(:sample).merge(attributes_for(:pacbio_request, barcode: 'SE108532I')),
+      attributes_for(:sample).merge(attributes_for(:pacbio_request, barcode: 'SE108533J')),
+      attributes_for(:sample).merge(attributes_for(:pacbio_request, barcode: 'SE108534K'))
+    ]
+  end
 
   it_behaves_like 'requestor factory'
-
 end
