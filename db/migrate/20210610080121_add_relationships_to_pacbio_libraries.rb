a8e45bdda9e089c9aac405daed1231e57437319c
Stephen Inglis <519327+stevieing@users.noreply.github.com>
2021-06-14 13:37:21 +0100

    Trying to fix migration issues.

--- db/migrate/20210610080121_add_relationships_to_pacbio_libraries.rb
+++
@@ -1,7 +0,0 @@
-class AddRelationshipsToPacbioLibraries < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :pacbio_libraries, :pacbio_request, index: true
-    add_reference :pacbio_libraries, :tag, index: true
-    add_reference :pacbio_libraries, :pacbio_pool, index: true
-  end
-end
