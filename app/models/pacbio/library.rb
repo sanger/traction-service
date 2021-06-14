122c23219a40a1d382334087337595d8ec86b6fd
Stephen Inglis <519327+stevieing@users.noreply.github.com>
2021-06-14 16:00:19 +0100

    Rubocopped. Added disable.

--- app/models/pacbio/library.rb
+++ app/models/pacbio/library.rb
@@ -31,7 +31,6 @@ module Pacbio
 
     delegate :barcode, to: :tube, allow_nil: true
 
-
     belongs_to :request, class_name: 'Pacbio::Request', foreign_key: :pacbio_request_id,
                          optional: true, inverse_of: :libraries
     belongs_to :tag, optional: true
--- app/models/pacbio/request_library.rb
+++ app/models/pacbio/request_library.rb
@@ -11,8 +11,12 @@ module Pacbio
   class RequestLibrary < ApplicationRecord
     include SampleSheet
 
+    # TODO: remove these relationships totallt.
+    # rubocop:disable Rails/InverseOf
     belongs_to :request, class_name: 'Pacbio::Request', foreign_key: :pacbio_request_id
     belongs_to :library, class_name: 'Pacbio::Library', foreign_key: :pacbio_library_id
+    # rubocop:enable Rails/InverseOf
+
     # TODO: make this taggable
     belongs_to :tag, class_name: '::Tag', inverse_of: false, optional: true
 
