c46e4cc5e097526f454342b9c254c5f220205e44
James Glover <james.glover@sanger.ac.uk>
2021-07-16 14:52:13 +0100

    Refactor parameters permissions

--- app/controllers/v1/pacbio/runs/wells_controller.rb
+++ app/controllers/v1/pacbio/runs/wells_controller.rb
@@ -5,6 +5,11 @@ module V1
     module Runs
       # WellsController
       class WellsController < ApplicationController
+        PERMITTED_WELL_PARAMETERS = %i[
+          movie_time insert_size row on_plate_loading_concentration
+          column comment pre_extension_time generate_hifi ccs_analysis_output
+        ].freeze
+
         def create
           @well_factory = ::Pacbio::WellFactory.new(create_params)
 
@@ -51,54 +56,45 @@ module V1
           Messages.publish(@well_factory.plate, Pipelines.pacbio.message)
         end
 
+        def permitted_update_params
+          data_params.require(:attributes)
+                     .permit(*PERMITTED_WELL_PARAMETERS, :id)
+        end
+
         def update_params
-          permitted_update_params.to_h.tap do |well_param|
-            well_param[:id] = data_params[:id]
-            if data_params[:relationships].present?
-              well_param[:pools] =
-                pool_param_names(data_params)
-            end
+          permitted_update_params.to_h.tap do |well|
+            well[:id] = data_params[:id]
+            well[:pools] = pool_param_names(data_params) if data_params.dig(:relationships, :pools)
           end
         end
 
-        def permitted_update_params
-          params.require(:data)
-                .require(:attributes)
-                .permit(
-                  :movie_time, :insert_size, :row, :on_plate_loading_concentration,
-                  :column, :comment, :id, :pre_extension_time, :generate_hifi,
-                  :ccs_analysis_output
-                )
-        end
-
         def create_params
-          params.require(:data).require(:attributes).fetch(:wells, []).map do |param|
+          data_params.require(:attributes).fetch(:wells, []).map do |param|
             well_params_names(param)
           end
         end
 
         def well_params_names(well_params)
-          well_params.permit(:movie_time, :insert_size, :row,
-                             :on_plate_loading_concentration, :column,
-                             :comment, :relationships,
-                             :pre_extension_time, :generate_hifi,
-                             :ccs_analysis_output).to_h.tap do |well|
+          well_params.permit(*PERMITTED_WELL_PARAMETERS).to_h.tap do |well|
             if well_params[:relationships].present?
               well[:plate] = plate_params_names(well_params)
-              well[:pools] = pool_param_names(well_params) unless well_params.dig(:relationships,
-                                                                                  :pools).nil?
+              well[:pools] = pool_param_names(well_params) if well_params.dig(
+                :relationships, :pools
+              )
             end
           end
         end
 
         def plate_params_names(params)
-          params.require(:relationships)[:plate].require(:data).permit(:id, :type).to_h
+          params.require(:relationships)
+                .require(:plate)
+                .require(:data).permit(:id, :type).to_h
         end
 
         def pool_param_names(params)
           params.require(:relationships)[:pools][:data].map do |pool|
             pool.permit(:id, :type).to_h
-          end.flatten
+          end
         end
 
         def well
