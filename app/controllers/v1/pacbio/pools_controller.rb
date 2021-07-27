15b681071dfbb2c9c8f9a362dbd2c33cde0f5366
James Glover <james.glover@sanger.ac.uk>
2021-07-27 17:05:36 +0100

    Created pools can include related resources

--- app/controllers/v1/pacbio/pools_controller.rb
+++ app/controllers/v1/pacbio/pools_controller.rb
@@ -4,25 +4,6 @@ module V1
   module Pacbio
     # PoolsController
     class PoolsController < ApplicationController
-      def create
-        @pool_creator = ::Pacbio::PoolCreator.new(pool_params)
-        if @pool_creator.save!
-          render json: PlateResource.new(@pool_creator.pool, nil), status: :created
-        else
-          render json: { data: { errors: @pool_creator.errors.messages } },
-                 status: :unprocessable_entity
-        end
-      end
     end
   end
-end
-
-private
-
-def pool_params
-  params.require(:data)['attributes'].permit(
-    { libraries:
-        %i[template_prep_kit_box_barcode volume concentration fragment_size pacbio_request_id
-           tag_id] }
-  ).to_h
 end
--- app/resources/v1/pacbio/pool_resource.rb
+++ app/resources/v1/pacbio/pool_resource.rb
@@ -12,8 +12,25 @@ module V1
       attributes :volume, :concentration, :template_prep_kit_box_barcode,
                  :fragment_size, :source_identifier, :created_at, :updated_at
 
+      attribute :libraries, delegate: :libraries
+
+      def libraries=(library_parameters)
+        @model.libraries = library_parameters.map do |library|
+          library.permit(:volume, :template_prep_kit_box_barcode,
+                         :concentration, :fragment_size, :pacbio_request_id, :tag_id)
+        end
+      end
+
+      def fetchable_fields
+        super - [:libraries]
+      end
+
       def self.records_for_populate(*_args)
         super.preload(:source_wells)
+      end
+
+      def self.create_model(*_opt)
+        ::Pacbio::PoolCreator.new
       end
     end
   end
