f161252aa83e68b950afe94bc8ce85d1f9c887e8
Ben Topping <ben.topping@outlook.com>
2021-07-27 16:01:14 +0100

    added pool run resource, updated rake task

---
+++ app/resources/v1/pacbio/runs/pool_resource.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module V1
+    module Pacbio
+        module Runs
+            # PoolResource
+            class PoolResource < V1::Pacbio::PoolResource
+            end
+        end
+    end
+end
+  
