3e874ce5318feb07d4be95eb3090b618faa669dc
James Glover <james.glover@sanger.ac.uk>
2022-01-11 13:46:08 +0000

    Separate sample sheet module into two
    Each method was only used in the context of a single class,
    or had complicated logic to handle differences between the two.
    
    Given most methods wouldn't even work if called in an invalid
    context, it felt like it made more sense to split them.

--- app/models/concerns/sample_sheet.rb
+++ app/models/concerns/sample_sheet.rb
@@ -1,69 +1,72 @@
 # frozen_string_literal: true
 
 # SampleSheet module
-# provides the group of methods
-# used to generate a pacbio run's sample sheet csv
-# TODO: This is included in a couple of places, but only some of the methods work in each.
+# Namespace for helper modules to assist ActiveRecord classes in rendering
+# sample sheets
 module SampleSheet
-  # include ActiveSupport::Concern
-
-  # Sample Well field
-  def position_leading_zero
-    "#{row}#{column.rjust(2, '0')}"
-  end
-
-  # Barcode Name field
-  # Used in context of Request Library model
-  def barcode_name
-    tag&.barcode_name
-  end
-
-  # Barcode Set field
-  def barcode_set
-    # Assuming each request libraries tag has the same set name
-    return unless all_libraries_tagged
-
-    libraries.first.tag.barcode_set
-  end
-
-  # Sample bio Name field
-  def find_sample_name
-    if defined?(sample_names) # When row type is well
-      return sample_names unless sample_is_barcoded
-    elsif defined?(request.sample_name) # When row type is library (sample)
-      return request.sample_name
+  # Provides well helper methods for sample sheet generation
+  module Well
+    # Sample Well field
+    def position_leading_zero
+      "#{row}#{column.rjust(2, '0')}"
     end
-    ''
+
+    # Barcode Set field
+    def barcode_set
+      # Assuming each request libraries tag has the same set name
+      return unless all_libraries_tagged
+
+      libraries.first.tag.barcode_set
+    end
+
+    # Determines rendering of a row-per sample
+    def all_libraries_tagged
+      libraries.all?(&:tag_id?)
+    end
+
+    # Sample Name field
+    def pool_barcode
+      # First pool in well's barcode as samples names are already contained in bio sample name
+      pools.first.tube.barcode
+    end
+
+    # Used to indicate to the sample sheet whether it should treat a sample as barcoded
+    # Note: This doesn't actually indicate that a sample *is* barcoded, as :hidden
+    # tag sets (such as IsoSeq) lie.
+    def sample_is_barcoded
+      libraries.all? { |l| l.tag&.barcoded_for_sample_sheet? }
+    end
+
+    # Same Barcodes on Both Ends of Sequence field
+    def same_barcodes_on_both_ends_of_sequence
+      # Always true at the time of writing
+      true
+    end
+
+    def automation_parameters
+      return if pre_extension_time == 0
+      return unless pre_extension_time
+
+      "ExtensionTime=double:#{pre_extension_time}|ExtendFirst=boolean:True"
+    end
+
+    # Sample bio Name field
+    def find_sample_name
+      sample_is_barcoded ? '' : sample_names
+    end
   end
 
-  # Sample is Barcoded field
-  def all_libraries_tagged
-    libraries.all?(&:tag_id?)
-  end
+  # Provides library helper methods for sample sheet generation
+  module Library
+    # Barcode Name field
+    # Used in context of Pacbio::Library model
+    def barcode_name
+      tag&.barcode_name
+    end
 
-  # Used to indicate to the sample sheet whether it should treat a sample as barcoded
-  # Note: This doesn't actually indicate that a sample *is* barcoded, as :hidden
-  # tag sets (such as IsoSeq) lie.
-  def sample_is_barcoded
-    libraries.all? { |l| l.tag&.barcoded_for_sample_sheet? }
-  end
-
-  # Sample Name field
-  def pool_barcode
-    # First pool in well's barcode as samples names are already contained in bio sample name
-    pools.first.tube.barcode
-  end
-
-  # Same Barcodes on Both Ends of Sequence field
-  def same_barcodes_on_both_ends_of_sequence
-    # Always true at the time of writing
-    true
-  end
-
-  def automation_parameters
-    return if pre_extension_time == 0
-    return unless pre_extension_time
-
-    "ExtensionTime=double:#{pre_extension_time}|ExtendFirst=boolean:True"
+    # Sample bio Name field
+    def find_sample_name
+      request.sample_name || ''
+    end
   end
 end
