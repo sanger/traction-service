22eb88cf36ad96913d44cdfa5b06808a616f851e
Stephen <519327+stevieing@users.noreply.github.com>
2022-04-14 10:10:37 +0100

    Added todos for hooks.

--- app/controllers/v1/pacbio/libraries_controller.rb
+++ app/controllers/v1/pacbio/libraries_controller.rb
@@ -4,6 +4,7 @@ module V1
   module Pacbio
     # LibrariesController
     class LibrariesController < ApplicationController
+      # TODO: add hook to send a message after update if there is a run attached
     end
   end
 end
