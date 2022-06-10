c03ed11f8c94d253707fc82ffbb92ec9c1730b09
James Glover <james.glover@sanger.ac.uk>
2022-06-10 11:05:10 +0100

    Add uuid validator
    Validates the uuid format. Uses basic regex, so doesn't:
    1) Check if there is a matching resource
    2) Check the the format matches the uuid standard precisely.

---
+++ spec/validators/uuid_validator_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UuidValidator do
+  describe '#validate_each' do
+    let(:record_class) { Ont::Request }
+    let(:record) { record_class.new }
+    let(:attribute) { :external_study_id }
+
+    before do
+      described_class.new(attributes: [attribute]).validate_each(record, attribute, value)
+    end
+
+    context 'with a nil value' do
+      let(:value) { nil }
+
+      it 'adds an error to the record' do
+        expect(record.errors.full_messages).to include('External study is not a valid uuid')
+      end
+    end
+
+    context 'with a non-uuid' do
+      let(:value) { 'not-a-uuid' }
+
+      it 'adds an error to the record' do
+        expect(record.errors.full_messages).to include('External study is not a valid uuid')
+      end
+    end
+
+    context 'with a uuid' do
+      let(:value) { generate(:uuid) }
+
+      it 'adds an error to the record' do
+        expect(record.errors.full_messages).to be_empty
+      end
+    end
+  end
+end
