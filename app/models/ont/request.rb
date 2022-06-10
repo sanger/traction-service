06c0348009cce05837bcd5f14e02ce15b2bd1ec0
James Glover <james.glover@sanger.ac.uk>
2022-06-10 12:02:12 +0100

    Add basic outline for ONT request
    - Validate all attributes as needed
    - Add associations with library type and data_type
    - Add pipeline validation for library_type/data_type

--- app/models/ont/request.rb
+++ app/models/ont/request.rb
@@ -4,5 +4,15 @@ module Ont
   # Ont::Request
   class Request < ApplicationRecord
     include Material
+
+    belongs_to :library_type
+    belongs_to :data_type
+
+    validates :cost_code, presence: true
+    validates :number_of_flowcells, numericality: { only_integer: true, greater_than: 0 }
+    validates :external_study_id, uuid: true
+
+    validates :library_type, pipeline: :ont
+    validates :data_type, pipeline: :ont
   end
 end
