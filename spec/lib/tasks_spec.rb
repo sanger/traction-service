08986a92b369d58c55b9efcd93f1b17eccb5a802
Stephen <519327+stevieing@users.noreply.github.com>
2022-07-18 15:09:44 +0100

    Fixed dodgy error with tasks. I think it was down to over using reenable.

--- spec/lib/tasks_spec.rb
+++ spec/lib/tasks_spec.rb
@@ -6,14 +6,6 @@ Rails.application.load_tasks
 
 describe "Rake Tasks" do
 
-  # If you don't reenable the task it won't run
-  after(:each) do
-    Rake::Task["tags:create:pacbio_all"].reenable
-    Rake::Task["pacbio_data:create"].reenable
-    Rake::Task["tags:create:pacbio_sequel"].reenable
-    Rake::Task["tags:create:pacbio_isoseq"].reenable
-  end
-
   describe 'create tags' do
 
     it 'should create all of the tag sets' do
@@ -26,8 +18,11 @@ describe "Rake Tasks" do
   describe 'create pacbio runs' do
 
     it 'should create the correct number of runs' do
+      Rake::Task["tags:create:pacbio_sequel"].reenable
+      Rake::Task["tags:create:pacbio_isoseq"].reenable
       Rake::Task["pacbio_data:create"].invoke
       expect(Pacbio::Run.count).to eq(6)
     end
   end
+
 end
