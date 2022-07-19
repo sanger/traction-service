d3413b9c7347354a181d5aa322f5637c6a7a6995
James Glover <james.glover@sanger.ac.uk>
2022-07-19 12:53:02 +0100

    Extend DataType documentation

--- app/models/data_type.rb
+++ app/models/data_type.rb
@@ -2,6 +2,10 @@
 
 # A data-type communicates information downstream regarding how the customer
 # would like their data processed.
+# This information is passed to NPG in the warehouse, and adjusts the data
+# made available to the customer. For example, 'basecalls and raw data'
+# indicates that the customer would like the raw data included along with the
+# processed basecalls
 class DataType < ApplicationRecord
   include Pipelineable
 
--- app/resources/v1/data_type_resource.rb
+++ app/resources/v1/data_type_resource.rb
@@ -2,6 +2,7 @@
 
 module V1
   # Exposes valid data type options for use by the UI
+  # {include:DataType}
   class DataTypeResource < JSONAPI::Resource
     attributes :name, :pipeline, :created_at, :updated_at
 
