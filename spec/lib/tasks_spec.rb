c5d5f9eb59237c8396f70c2fa2a2db390e4d00be
James Glover <james.glover@sanger.ac.uk>
2022-07-26 16:39:39 +0100

    Add ONT data task

--- spec/lib/tasks_spec.rb
+++ spec/lib/tasks_spec.rb
@@ -22,4 +22,21 @@ RSpec.describe 'RakeTasks' do
       )
     end
   end
+
+  describe 'ont_data:create' do
+    let(:expected_plates) { 2 }
+    let(:filled_wells_per_plate) { 95 }
+    let(:expected_tubes) { 2 }
+    let(:expected_wells) { expected_plates * filled_wells_per_plate }
+    let(:expected_requests) { expected_tubes + expected_wells }
+
+    before do
+      create :library_type, :ont
+      create :data_type, :ont
+    end
+
+    it 'creates plates and tubes' do
+      expect { Rake::Task['ont_data:create'].invoke }.to change(Reception, :count).by(1).and change(Ont::Request, :count).by(expected_requests).and change(Sample, :count).by(expected_requests).and change(Plate, :count).by(expected_plates).and change(Well, :count).by(expected_wells).and change(Tube, :count).by(expected_tubes).and output("-> Created requests for #{expected_plates} plates and #{expected_tubes} tubes\n").to_stdout
+    end
+  end
 end
