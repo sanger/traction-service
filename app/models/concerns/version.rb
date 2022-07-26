ac936065bb6b719e6f83c874031bfcf2f20a0618
Stephen <519327+stevieing@users.noreply.github.com>
2022-07-26 10:14:39 +0100

    Added comment about format.

--- app/models/concerns/version.rb
+++ app/models/concerns/version.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 module Version
+  # I have gone for the simple option. We could go full semver but that would be complicated
+  # and unnecessary
+  # using a full stop would break method naming so I have gone for underscore
+  # having the format here gives us the advantage of modifying easily.
   FORMAT = /\Av\d{2}?_?\d{1,2}\z/
 end
