d973d244290188530ec9743cf430faa538ae0c62
Stephen <519327+stevieing@users.noreply.github.com>
2022-07-29 16:27:19 +0100

    Fixed issues with namespacing and loading hopefully.

--- config/initializers/types.rb
+++ config/initializers/types.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-require './app/models/smrt_link_version_type'
+require Rails.root.join('app/models/smrt_link_version_type')
 
 ActiveRecord::Type.register(:smrt_link_version, SmrtLinkVersionType)
