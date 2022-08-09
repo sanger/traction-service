e162762a30908c8f83bcd511d0fb45c3cfda52d3
Stephen <519327+stevieing@users.noreply.github.com>
2022-08-09 11:01:28 +0100

    Fixed problem in migration. Added task to move smrt link options.

--- spec/lib/tasks_spec.rb
+++ spec/lib/tasks_spec.rb
@@ -55,4 +55,25 @@ RSpec.describe 'RakeTasks' do
       expect { Rake::Task['ont_data:create'].invoke }.to change(Reception, :count).by(1).and change(Ont::Request, :count).by(expected_requests).and change(Sample, :count).by(expected_requests).and change(Plate, :count).by(expected_plates).and change(Well, :count).by(expected_wells).and change(Tube, :count).by(expected_tubes).and output("-> Created requests for #{expected_plates} plates and #{expected_tubes} tubes\n").to_stdout
     end
   end
+
+  describe 'pacbio_wells:migrate_smrt_link_options' do
+    # We need to set the smrt link version to v12 so it is not validated
+    # generate_hifi - 'In SMRT Link' => 0, 'On Instrument' => 1, 'Do Not Generate' => 2
+    let!(:well1) { create(:pacbio_well, generate_hifi_deprecated: 0, generate_hifi: nil, plate: create(:pacbio_plate, run: create(:pacbio_run, smrt_link_version: 'v12'))) }
+    let!(:well2) { create(:pacbio_well, generate_hifi_deprecated: 1,  generate_hifi: nil, plate: create(:pacbio_plate, run: create(:pacbio_run, smrt_link_version: 'v12'))) }
+    let!(:well3) { create(:pacbio_well, generate_hifi_deprecated: 2,  generate_hifi: nil, plate: create(:pacbio_plate, run: create(:pacbio_run, smrt_link_version: 'v12'))) }
+    let!(:well4) { create(:pacbio_well, ccs_analysis_output_deprecated: 'Yes', ccs_analysis_output: nil, plate: create(:pacbio_plate, run: create(:pacbio_run, smrt_link_version: 'v12'))) }
+    let!(:well5) { create(:pacbio_well, ccs_analysis_output_deprecated: 'No',  ccs_analysis_output: nil, plate: create(:pacbio_plate, run: create(:pacbio_run, smrt_link_version: 'v12'))) }
+
+    it 'modifies the data correctly' do
+      expect(Pacbio::Well.count).to eq(5)
+      Rake::Task['pacbio_wells:migrate_smrt_link_options'].invoke
+      [well1, well2, well3, well4, well5].collect(&:reload)
+      expect(well1.generate_hifi).to eq('In SMRT Link')
+      expect(well2.generate_hifi).to eq('On Instrument')
+      expect(well3.generate_hifi).to eq('Do Not Generate')
+      expect(well4.ccs_analysis_output).to eq('Yes')
+      expect(well5.ccs_analysis_output).to eq('No')
+    end
+  end
 end
