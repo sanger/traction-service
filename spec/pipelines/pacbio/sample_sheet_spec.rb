6d130a39df361efdabb7bf5de54864b004f8fd93
Stephen <519327+stevieing@users.noreply.github.com>
2022-08-09 14:44:41 +0100

    Added demultiplex barcodes to sample sheet.

--- spec/pipelines/pacbio/sample_sheet_spec.rb
+++ spec/pipelines/pacbio/sample_sheet_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe 'SampleSheet', type: :model do
+  let(:populate) { { for: [:well], with: :well }.with_indifferent_access }
+
   context 'v11' do
     let(:sample_sheet_configuration) { Pipelines.pacbio.sample_sheet.by_version('v11') }
 
@@ -11,6 +13,7 @@ RSpec.describe 'SampleSheet', type: :model do
       expect(column).to be_present
       expect(column.fetch('type')).to eq(:model)
       expect(column.fetch('value')).to eq('ccs_analysis_output_include_low_quality_reads')
+      expect(column.fetch('populate')).to eq(populate)
     end
 
     it 'must have 5mC Calls in CpG Motifs' do
@@ -18,10 +21,19 @@ RSpec.describe 'SampleSheet', type: :model do
       expect(column).to be_present
       expect(column.fetch('type')).to eq(:model)
       expect(column.fetch('value')).to eq('fivemc_calls_in_cpg_motifs')
+      expect(column.fetch('populate')).to eq(populate)
     end
 
     it 'will not have generate hi fi' do
       expect(sample_sheet_configuration.columns.children).not_to have_key('Generate HiFi Reads')
+    end
+
+    it 'will have demultiplex barcodes' do
+      column = sample_sheet_configuration.columns.children['Demultiplex Barcodes']
+      expect(column).to be_present
+      expect(column.fetch('type')).to eq(:model)
+      expect(column.fetch('value')).to eq('demultiplex_barcodes')
+      expect(column.fetch('populate')).to eq(populate)
     end
   end
 end
