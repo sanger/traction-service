617436a046d33d25e9ec3a61576d1145245c8183
Katy Taylor <kt17@sanger.ac.uk>
2022-08-18 17:13:58 +0100

    Create QcAssayType and QcResult models - haven't included the schema yet as have to sort out SQLite vs MySQL local changes

---
+++ spec/factories/qc_results.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :qc_result do
+    labware_barcode { 'AB1234' }
+    sample_external_id { 'Samp-1234' }
+    qc_assay_type { create :qc_assay_type }
+    value { '34' }
+  end
+end
