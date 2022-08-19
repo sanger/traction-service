6430cac796123167262565e6863ca912d8682f76
Katy Taylor <kt17@sanger.ac.uk>
2022-08-19 11:02:54 +0100

    Add new tables QcAssayType and QcResult to the API

---
+++ app/controllers/v1/qc_results_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module V1
+  # JsonApi::Resource controllers are primarily handled by the corresponding
+  # resources
+  # See {QcResult} and {QcResultResource} for more information
+  class QcResultsController < ApplicationController
+  end
+end
