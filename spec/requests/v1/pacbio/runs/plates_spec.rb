6adb9bfa1c12b1b6478c16df0a90ba14fcf598c4
yoldas <ay6@sanger.ac.uk>
2022-10-06 01:25:00 +0100

    Moved default version to before block.

--- spec/requests/v1/pacbio/runs/plates_spec.rb
+++ spec/requests/v1/pacbio/runs/plates_spec.rb
@@ -3,8 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe 'PlatesController', type: :request do
-  let!(:version10) { create(:pacbio_smrt_link_version, name: 'v10', default: true) }
-  let!(:version11) { create(:pacbio_smrt_link_version, name: 'v11') }
+  before do
+    # Create a default smrt link version for pacbio runs.
+    create(:pacbio_smrt_link_version, name: 'v10', default: true)
+  end
 
   describe '#get' do
     let!(:run1) { create(:pacbio_run) }
