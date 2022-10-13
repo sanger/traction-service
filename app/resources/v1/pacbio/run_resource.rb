f0e7610af31c32aac559d7671de4a4c2484e58aa
yoldas <ay6@sanger.ac.uk>
2022-10-13 21:46:07 +0100

    Added pacbio_smrt_link_version_id to run resource attributes.

--- app/resources/v1/pacbio/run_resource.rb
+++ app/resources/v1/pacbio/run_resource.rb
@@ -7,7 +7,8 @@ module V1
       model_name 'Pacbio::Run'
 
       attributes :name, :sequencing_kit_box_barcode, :dna_control_complex_box_barcode,
-                 :system_name, :created_at, :state, :comments, :all_wells_have_pools
+                 :system_name, :created_at, :state, :comments, :all_wells_have_pools,
+                 :pacbio_smrt_link_version_id
 
       has_one :plate, foreign_key_on: :related, foreign_key: 'pacbio_run_id',
                       class_name: 'Runs::Plate'
