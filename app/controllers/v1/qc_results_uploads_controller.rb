3e67998d46357d3ef6df400a9676909477088f3e
Harriet Craven <hc6@sanger.ac.uk>
2022-10-28 11:59:01 +0100

    Rubocopped

--- app/controllers/v1/qc_results_uploads_controller.rb
+++ app/controllers/v1/qc_results_uploads_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module V1
   # QcResultsUploadsController
   class QcResultsUploadsController < ApplicationController
--- spec/factories/qc_results_uploads.rb
+++ spec/factories/qc_results_uploads.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :qc_results_upload, class: 'QcResultsUpload' do
-    csv_data { "MyText" }
+    csv_data { 'MyText' }
   end
 end
