c9347cbabf24325339249ebe49839f47c704fe76
Harriet Craven <hc6@sanger.ac.uk>
2022-11-15 13:17:48 +0000

    make belongs to singular

--- app/models/qc_result.rb
+++ app/models/qc_result.rb
@@ -2,5 +2,6 @@
 
 class QcResult < ApplicationRecord
   belongs_to :qc_assay_type
+  has_many :qc_decision_results
   validates :labware_barcode, :sample_external_id, :value, presence: true
 end
--- spec/factories/qc_decisions.rb
+++ spec/factories/qc_decisions.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :qc_decision do
     status { :pass }
