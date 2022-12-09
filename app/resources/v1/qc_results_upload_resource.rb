fc180dbedcd3c14762529662dafb09ce2d286095
Stephen <519327+stevieing@users.noreply.github.com>
2022-12-09 09:17:27 +0000

    Added messaging to qc uploads. Fixed a couple of test warnings.

--- app/resources/v1/qc_results_upload_resource.rb
+++ app/resources/v1/qc_results_upload_resource.rb
@@ -7,10 +7,14 @@ module V1
 
     attributes :csv_data, :used_by
 
-    after_create :create_entities!
+    after_create :create_entities!, :publish_messages
 
     def create_entities!
       @model.create_entities!
+    end
+
+    def publish_messages
+      Messages.publish(@model.messages, Pipelines.qc_result.message)
     end
   end
 end
