80e1af93f30b71178a0f3dff66d84bb224e6640a
yoldas <ay6@sanger.ac.uk>
2022-12-15 16:01:16 +0000

    Changed ont_instrument field name from max_number to max_number_of_flowcells

--- spec/factories/ont/instruments.rb
+++ spec/factories/ont/instruments.rb
@@ -3,16 +3,16 @@
 FactoryBot.define do
   factory :ont_instrument, class: 'Ont::Instrument' do
     name { 'GridION-1' }
-    max_number { 5 }
+    max_number_of_flowcells { 5 }
 
     factory :ont_minion do
       name { 'MinION-1' }
-      max_number { 1 }
+      max_number_of_flowcells { 1 }
     end
 
     factory :ont_promethion do
       name { 'PromethION-1' }
-      max_number { 24 }
+      max_number_of_flowcells { 24 }
     end
   end
 end
