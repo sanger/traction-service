a91d4598d778178454e9bb804f06fbe0edfe742d
yoldas <ay6@sanger.ac.uk>
2023-01-06 10:09:38 +0000

    Added feature check to FlowcellsController.

--- app/controllers/v1/ont/flowcells_controller.rb
+++ app/controllers/v1/ont/flowcells_controller.rb
@@ -4,6 +4,7 @@ module V1
   module Ont
     # FlowcellsController
     class FlowcellsController < ApplicationController
+      before_action { not_found unless Flipper.enabled?(:dpl_281_ont_create_sequencing_runs) }
     end
   end
 end
