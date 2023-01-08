b1ea9aed6e088da58c3d9bbc1892874808790cc0
yoldas <ay6@sanger.ac.uk>
2023-01-08 17:43:58 +0000

    Load ONT instruments from config file

--- spec/lib/tasks_spec.rb
+++ spec/lib/tasks_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe 'RakeTasks' do
 
   describe 'ont_instruments:create' do
     it 'creates the correct instrument data' do
-      expect { Rake::Task['ont_instruments:create'].invoke }.to change(Ont::Instrument, :count).and output("-> Instrument names added\n").to_stdout
+      expect { Rake::Task['ont_instruments:create'].invoke }.to change(Ont::Instrument, :count).and output("-> ONT Instruments successfully created\n").to_stdout
     end
   end
 
