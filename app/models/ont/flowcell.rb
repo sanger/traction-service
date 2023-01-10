c8f01bab9db80f537065cb6425d1f1ff866c2a54
yoldas <ay6@sanger.ac.uk>
2023-01-10 14:15:18 +0000

    Removed extra comments from flowcell model

--- app/models/ont/flowcell.rb
+++ app/models/ont/flowcell.rb
@@ -5,20 +5,14 @@ module Ont
   class Flowcell < ApplicationRecord
     include Uuidable
 
-    # Run has multiple of these flowcells up to the maximum number for the instrument.
+    # Run has many of these flowcells up to the maximum number for the instrument.
     belongs_to :run, foreign_key: :ont_run_id, inverse_of: :flowcells
 
-    # We have changed the model to use pool rather than library.
     # We assume one-to-one relationship with pool at the moment.
-    # belongs_to :library, foreign_key: :ont_library_id, inverse_of: :flowcell
     belongs_to :pool, foreign_key: :ont_pool_id, inverse_of: :flowcell
 
-    # We have changed the delegation to expose public methods of pool rather than library.
-    # XXX: How/where are we associating requests?
-    # delegate :requests, to: :library
     delegate :requests, to: :pool
 
-    # Validate uniqueness of the position of flowcell among others of the same run.
     # Validate the position to be a positive integer.
     validates :position,
               presence: true,
