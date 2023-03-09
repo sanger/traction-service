009196a9e5689f19976c2eb7f4cbc3dcb2b89169
Eduardo Martin Rojo <emr@sanger.ac.uk>
2023-03-09 16:11:41 +0000

    Moved to its own file. Added test

--- app/models/reception/resource_factory.rb
+++ app/models/reception/resource_factory.rb
@@ -19,19 +19,7 @@ class Reception
 
     validates_nested :request_attributes, flatten_keys: false
 
-    # Validates that the model to create is not going to refer to a container
-    # which already have requests in it
-    class ContainerDoesNotHaveRequestsAlready < ActiveModel::Validator
-      def validate(record)
-        containers = record.request_attributes.map(&:container).compact.uniq
-        containers.each do |container|
-          if container.already_exists? && container.existing_records_have_requests?
-            record.errors.add :container, "The container #{container.barcode} already exists"
-          end
-        end
-      end
-    end
-    validates_with ContainerDoesNotHaveRequestsAlready
+    validates_with ResourceFactoryValidator
 
     #
     # Array describing the requests to create.
