a5dd11d354119a523e3ac2649a1d1ed6dfca44e2
Ben Topping <ben.topping@outlook.com>
2023-04-17 15:48:24 +0100

    Added v12_revio smrtlink well attributes to pacbio run wells controller spec

--- spec/requests/v1/pacbio/runs/wells_spec.rb
+++ spec/requests/v1/pacbio/runs/wells_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe 'WellsController' do
   let!(:version10) { create(:pacbio_smrt_link_version, name: 'v10', default: true) }
   let!(:version11) { create(:pacbio_smrt_link_version, name: 'v11') }
+  let!(:version12) { create(:pacbio_smrt_link_version, name: 'v12_revio') }
 
   describe '#get' do
     let!(:wells) { create_list(:pacbio_well_with_pools, 2, pool_count: 2) }
@@ -15,31 +16,45 @@ RSpec.describe 'WellsController' do
       expect(json['data'].length).to eq(2)
     end
 
-    it 'returns the correct attributes' do
-      get "#{v1_pacbio_runs_wells_path}?include=pools", headers: json_api_headers
+    describe 'has the correct attributes' do
+      before do
+        get "#{v1_pacbio_runs_wells_path}?include=pools", headers: json_api_headers
+      end
 
-      expect(response).to have_http_status(:success), response.body
-      well = wells.first
-      well_attributes = json['data'][0]['attributes']
-      expect(well_attributes['pacbio_plate_id']).to eq(well.pacbio_plate_id)
-      expect(well_attributes['row']).to eq(well.row)
-      expect(well_attributes['column']).to eq(well.column)
-      expect(well_attributes['movie_time'].to_s).to eq(well.movie_time.to_s)
-      expect(well_attributes['on_plate_loading_concentration']).to eq(well.on_plate_loading_concentration)
-      expect(well_attributes['pacbio_plate_id']).to eq(well.pacbio_plate_id)
-      expect(well_attributes['comment']).to eq(well.comment)
-      expect(well_attributes['pre_extension_time']).to eq(well.pre_extension_time)
-      expect(well_attributes['binding_kit_box_barcode']).to eq(well.binding_kit_box_barcode)
+      let!(:well) { wells.first }
+      let!(:well_attributes) { json['data'][0]['attributes'] }
 
-      # v10
-      expect(well_attributes['generate_hifi']).to eq(well.generate_hifi)
-      expect(well_attributes['ccs_analysis_output']).to eq(well.ccs_analysis_output)
+      it 'has the correct standard attributes' do
+        expect(response).to have_http_status(:success), response.body
+        expect(well_attributes['pacbio_plate_id']).to eq(well.pacbio_plate_id)
+        expect(well_attributes['row']).to eq(well.row)
+        expect(well_attributes['column']).to eq(well.column)
+        expect(well_attributes['movie_time'].to_s).to eq(well.movie_time.to_s)
+        expect(well_attributes['on_plate_loading_concentration']).to eq(well.on_plate_loading_concentration)
+        expect(well_attributes['pacbio_plate_id']).to eq(well.pacbio_plate_id)
+        expect(well_attributes['comment']).to eq(well.comment)
+        expect(well_attributes['pre_extension_time']).to eq(well.pre_extension_time)
+        expect(well_attributes['binding_kit_box_barcode']).to eq(well.binding_kit_box_barcode)
+      end
 
-      # v11
-      expect(well_attributes['ccs_analysis_output_include_low_quality_reads']).to eq(well.ccs_analysis_output_include_low_quality_reads)
-      expect(well_attributes['include_fivemc_calls_in_cpg_motifs']).to eq(well.include_fivemc_calls_in_cpg_motifs)
-      expect(well_attributes['ccs_analysis_output_include_kinetics_information']).to eq(well.ccs_analysis_output_include_kinetics_information)
-      expect(well_attributes['demultiplex_barcodes']).to eq(well.demultiplex_barcodes)
+      it 'has the correct v10 attributes' do
+        expect(well_attributes['generate_hifi']).to eq(well.generate_hifi)
+        expect(well_attributes['ccs_analysis_output']).to eq(well.ccs_analysis_output)
+      end
+
+      it 'has the correct v11 attributes' do
+        expect(well_attributes['ccs_analysis_output_include_low_quality_reads']).to eq(well.ccs_analysis_output_include_low_quality_reads)
+        expect(well_attributes['include_fivemc_calls_in_cpg_motifs']).to eq(well.include_fivemc_calls_in_cpg_motifs)
+        expect(well_attributes['ccs_analysis_output_include_kinetics_information']).to eq(well.ccs_analysis_output_include_kinetics_information)
+        expect(well_attributes['demultiplex_barcodes']).to eq(well.demultiplex_barcodes)
+      end
+
+      it 'has the correct v12_revio attributes' do
+        expect(well_attributes['movie_acquisition_time']).to eq(well.movie_acquisition_time)
+        expect(well_attributes['include_base_kinetics']).to eq(well.include_base_kinetics)
+        expect(well_attributes['library_concentration']).to eq(well.library_concentration)
+        expect(well_attributes['polymerase_kit']).to eq(well.polymerase_kit)
+      end
     end
   end
 end
