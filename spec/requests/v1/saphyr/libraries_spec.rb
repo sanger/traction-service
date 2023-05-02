d210ad9590a1bb704076b2800e0cf9e35f14d918
Stephen <519327+stevieing@users.noreply.github.com>
2023-05-02 15:26:29 +0100

    Reduced NestedGroups rubocop to max 4.

--- spec/requests/v1/saphyr/libraries_spec.rb
+++ spec/requests/v1/saphyr/libraries_spec.rb
@@ -99,131 +99,126 @@ RSpec.describe 'LibrariesController' do
         end
       end
 
-      context 'on failure' do
-        context 'when the request does not exist' do
-          let(:saphyr_enzyme) { create(:saphyr_enzyme) }
+      context 'on failure - when the request does not exist' do
+        let(:saphyr_enzyme) { create(:saphyr_enzyme) }
 
-          let(:body) do
-            {
-              data: {
-                attributes: {
-                  libraries: [
-                    { state: 'pending', saphyr_request_id: 1, saphyr_enzyme_id: saphyr_enzyme.id }
-                  ]
-                }
+        let(:body) do
+          {
+            data: {
+              attributes: {
+                libraries: [
+                  { state: 'pending', saphyr_request_id: 1, saphyr_enzyme_id: saphyr_enzyme.id }
+                ]
               }
-            }.to_json
-          end
-
-          it 'can returns unprocessable entity status' do
-            post v1_saphyr_libraries_path, params: body, headers: json_api_headers
-            expect(response).to have_http_status(:unprocessable_entity)
-          end
-
-          it 'cannot create a library' do
-            expect do
-              post v1_saphyr_libraries_path, params: body, headers: json_api_headers
-            end.not_to change(Saphyr::Library, :count)
-          end
-
-          it 'has an error message' do
-            post v1_saphyr_libraries_path, params: body, headers: json_api_headers
-            expect(response.parsed_body['data']).to include('errors' => { 'request' => ['must exist'] })
-          end
+            }
+          }.to_json
         end
 
-        context 'when the enzyme does not exist' do
-          let(:request) { create(:saphyr_request) }
+        it 'can returns unprocessable entity status' do
+          post v1_saphyr_libraries_path, params: body, headers: json_api_headers
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
 
-          let(:body) do
-            {
-              data: {
-                attributes: {
-                  libraries: [
-                    { state: 'pending', saphyr_request_id: request.id, enzyme_id: 1 }
-                  ]
-                }
+        it 'cannot create a library' do
+          expect do
+            post v1_saphyr_libraries_path, params: body, headers: json_api_headers
+          end.not_to change(Saphyr::Library, :count)
+        end
+
+        it 'has an error message' do
+          post v1_saphyr_libraries_path, params: body, headers: json_api_headers
+          expect(response.parsed_body['data']).to include('errors' => { 'request' => ['must exist'] })
+        end
+      end
+
+      context 'on failure - when the enzyme does not exist' do
+        let(:saphyr_enzyme) { create(:saphyr_enzyme) }
+        let(:request) { create(:saphyr_request) }
+
+        let(:body) do
+          {
+            data: {
+              attributes: {
+                libraries: [
+                  { state: 'pending', saphyr_request_id: request.id, enzyme_id: 1 }
+                ]
               }
-            }.to_json
-          end
+            }
+          }.to_json
+        end
 
-          it 'can returns unprocessable entity status' do
+        it 'can returns unprocessable entity status' do
+          post v1_saphyr_libraries_path, params: body, headers: json_api_headers
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it 'cannot create a library' do
+          expect do
             post v1_saphyr_libraries_path, params: body, headers: json_api_headers
-            expect(response).to have_http_status(:unprocessable_entity)
-          end
+          end.not_to change(Saphyr::Library, :count)
+        end
 
-          it 'cannot create a library' do
-            expect do
-              post v1_saphyr_libraries_path, params: body, headers: json_api_headers
-            end.not_to change(Saphyr::Library, :count)
-          end
-
-          it 'has an error message' do
-            post v1_saphyr_libraries_path, params: body, headers: json_api_headers
-            expect(response.parsed_body['data']).to include('errors' => { 'enzyme' => ['must exist'] })
-          end
+        it 'has an error message' do
+          post v1_saphyr_libraries_path, params: body, headers: json_api_headers
+          expect(response.parsed_body['data']).to include('errors' => { 'enzyme' => ['must exist'] })
         end
       end
     end
 
     context 'when creating multiple libraries' do
-      context 'on success' do
-        context 'when the request does exist' do
-          let(:request) { create(:saphyr_request) }
-          let(:saphyr_enzyme) { create(:saphyr_enzyme) }
+      context 'on success - when the request does exist' do
+        let(:request) { create(:saphyr_request) }
+        let(:saphyr_enzyme) { create(:saphyr_enzyme) }
 
-          let(:body) do
-            {
-              data: {
-                attributes: {
-                  libraries: [
-                    { state: 'pending', saphyr_request_id: request.id,
-                      saphyr_enzyme_id: saphyr_enzyme.id },
-                    { state: 'pending', saphyr_request_id: request.id,
-                      saphyr_enzyme_id: saphyr_enzyme.id },
-                    { state: 'pending', saphyr_request_id: request.id,
-                      saphyr_enzyme_id: saphyr_enzyme.id }
-                  ]
-                }
+        let(:body) do
+          {
+            data: {
+              attributes: {
+                libraries: [
+                  { state: 'pending', saphyr_request_id: request.id,
+                    saphyr_enzyme_id: saphyr_enzyme.id },
+                  { state: 'pending', saphyr_request_id: request.id,
+                    saphyr_enzyme_id: saphyr_enzyme.id },
+                  { state: 'pending', saphyr_request_id: request.id,
+                    saphyr_enzyme_id: saphyr_enzyme.id }
+                ]
               }
-            }.to_json
-          end
+            }
+          }.to_json
+        end
 
-          it 'can create libraries' do
-            post v1_saphyr_libraries_path, params: body, headers: json_api_headers
-            expect(response).to have_http_status(:created)
-          end
+        it 'can create libraries' do
+          post v1_saphyr_libraries_path, params: body, headers: json_api_headers
+          expect(response).to have_http_status(:created)
         end
       end
 
-      context 'on failure' do
-        context 'when the request does not exist' do
-          let(:saphyr_enzyme) { create(:saphyr_enzyme) }
+      context 'on failure - when the request does not exist' do
+        let(:saphyr_enzyme) { create(:saphyr_enzyme) }
 
-          let(:body) do
-            {
-              data: {
-                attributes: {
-                  libraries: [
-                    { state: 'pending', saphyr_request_id: 1, saphyr_enzyme_id: saphyr_enzyme.id },
-                    { state: 'pending', saphyr_request_id: 1, saphyr_enzyme_id: saphyr_enzyme.id }
-                  ]
-                }
+        let(:body) do
+          {
+            data: {
+              attributes: {
+                libraries: [
+                  { state: 'pending', saphyr_request_id: 1, saphyr_enzyme_id: saphyr_enzyme.id },
+                  { state: 'pending', saphyr_request_id: 1, saphyr_enzyme_id: saphyr_enzyme.id }
+                ]
               }
-            }.to_json
-          end
+            }
+          }.to_json
+        end
 
-          it 'cannot create libraries' do
-            post v1_saphyr_libraries_path, params: body, headers: json_api_headers
-            expect(response).to have_http_status(:unprocessable_entity)
-          end
+        it 'cannot create libraries' do
+          post v1_saphyr_libraries_path, params: body, headers: json_api_headers
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
 
-          it 'has an error message' do
-            post v1_saphyr_libraries_path, params: body, headers: json_api_headers
-            expect(response.parsed_body['data']).to include('errors' => { 'request' => [
-              'must exist', 'must exist'
-            ] })
-          end
+        it 'has an error message' do
+          post v1_saphyr_libraries_path, params: body, headers: json_api_headers
+          expect(response.parsed_body['data']).to include('errors' => { 'request' => [
+            'must exist', 'must exist'
+          ] })
         end
       end
     end
