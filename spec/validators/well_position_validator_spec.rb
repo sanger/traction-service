8a1b884202ac07eae5154bdfdb97875eb0e5896b
Stephen <519327+stevieing@users.noreply.github.com>
2023-05-02 14:11:35 +0100

    Added pending tests for validator.

--- spec/validators/well_position_validator_spec.rb
+++ spec/validators/well_position_validator_spec.rb
@@ -2,8 +2,57 @@
 
 require 'rails_helper'
 
-RSpec.describe WellPositionValidator do
-  it 'needs a passing test' do
-    expect(true).to be_truthy
+RSpec.describe.skip WellPositionValidator do
+
+  describe 'well locations' do
+
+    it 'when the wells are in the correct position' do
+      record = build(:pacbio_run, wells: [build(:pacbio_well, position: 'A1')])
+      expect(described_class.new.validate(record)).to be_valid
+
+      record = build(:pacbio_run, wells: [build(:pacbio_well, position: 'A1'), build(:pacbio_well, position: 'B1')])
+      expect(described_class.new.validate(record)).to be_valid
+
+      record = build(:pacbio_run, wells: [build(:pacbio_well, position: 'A1'), build(:pacbio_well, position: 'B1'), build(:pacbio_well, position: 'C1')])
+      expect(described_class.new.validate(record)).to be_valid
+
+      record = build(:pacbio_run, wells: [build(:pacbio_well, position: 'A1'), build(:pacbio_well, position: 'B1'), build(:pacbio_well, position: 'C1'), build(:pacbio_well, position: 'D1')])
+      expect(described_class.new.validate(record)).to be_valid
+
+    end
+
+    it 'when the wells are not in the correct position' do
+      record = build(:pacbio_run, wells: [build(:pacbio_well, position: 'G1')])
+      expect(described_class.new.validate(record)).to_not be_valid
+
+      record = build(:pacbio_run, wells: [build(:pacbio_well, position: 'A1'), build(:pacbio_well, position: 'G12')])
+      expect(described_class.new.validate(record)).to be_valid
+
+      record = build(:pacbio_run, wells: [build(:pacbio_well, position: 'A1'), build(:pacbio_well, position: 'B1'), build(:pacbio_well, position: 'H4')])
+      expect(described_class.new.validate(record)).to be_valid
+
+      record = build(:pacbio_run, wells: [build(:pacbio_well, position: 'A1'), build(:pacbio_well, position: 'B1'), build(:pacbio_well, position: 'D4'), build(:pacbio_well, position: 'H12')])
+      expect(described_class.new.validate(record)).to be_valid
+    end
+
   end
+
+  describe 'contiguousness' do
+
+    it 'when the wells are not in the correct sequence' do
+      record = build(:pacbio_run, wells: [build(:pacbio_well, position: 'B1')])
+      expect(described_class.new.validate(record)).to_not be_valid
+
+      record = build(:pacbio_run, wells: [build(:pacbio_well, position: 'A1'), build(:pacbio_well, position: 'C1')])
+      expect(described_class.new.validate(record)).to_not be_valid
+
+      record = build(:pacbio_run, wells: [build(:pacbio_well, position: 'A1'), build(:pacbio_well, position: 'D1')])
+      expect(described_class.new.validate(record)).to_not be_valid
+
+      record = build(:pacbio_run, wells: [build(:pacbio_well, position: 'B1'), build(:pacbio_well, position: 'C1'), build(:pacbio_well, position: 'D1')])
+      expect(described_class.new.validate(record)).to_not be_valid
+    end
+
+  end
+  
 end
