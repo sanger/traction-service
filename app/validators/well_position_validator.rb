961dc4f25240b5ccd78ec912dea7c1ff05e5fc14
Stephen <519327+stevieing@users.noreply.github.com>
2023-05-11 09:12:40 +0100

    Modified method for validating contiguousness using the new rules.

--- app/validators/well_position_validator.rb
+++ app/validators/well_position_validator.rb
@@ -6,14 +6,6 @@ class WellPositionValidator < ActiveModel::Validator
 
   VALID_WELLS = %w[A1 B1 C1 D1].freeze
 
-  INVALID_COMBINATIONS = [
-    %w[A1 D1],
-    %w[A1 C1],
-    %w[B1 D1],
-    %w[A1 C1 D1],
-    %w[A1 B1 D1]
-  ].freeze
-
   def validate(record)
     return unless record.wells
 
@@ -35,20 +27,37 @@ class WellPositionValidator < ActiveModel::Validator
     nil
   end
 
-  # This validation checks that the wells only appear in certain combinations
-  # no empty wells between libraries. It may be better to come up with an algorithm
-  # i.e. check number of wells if it is more than 1 check no empty wells between libraries?
+  # This validation checks that the there are no empty wells between libraries
+  # e.g. A1, D1 is invalid whereas B1, C1 is valid
   def validate_contiguousness(record)
     # if we don't do this we get a 500 when there are no wells
     # there is already validation to check if there are wells
-    return if record.wells.blank?
+    return if record.wells.blank? || record.wells.count == 1
 
+    # get the position for each well in an array
     well_positions = record.wells.collect(&:position)
 
-    INVALID_COMBINATIONS.each do |combination|
-      next unless well_positions == combination
+    # find the correct index of each well position vs the valid well and reverse it
+    reversed_indexes = well_positions.collect { |position| VALID_WELLS.index(position) }.reverse
 
-      record.errors.add(:wells, "must be in the valid order #{VALID_WELLS}")
+    # get the differences in the indexes for each well
+    index_differences = find_index_differences(reversed_indexes)
+
+    # if all of the wells are next to each other and they are valid i.e. difference is 1
+    return unless index_differences.any? { |index| index > 1 }
+
+    # if the wells are not next to each other then it is not valid
+    record.errors.add(:wells, "must be in the valid order #{VALID_WELLS}")
+  end
+
+  # find the difference in position of each well
+  # e.g. if wells are B1 and C1 then the difference is 1
+  # whereas if the wells are B1 and D1 the difference is 2
+  def find_index_differences(indexes)
+    [].tap do |index_differences|
+      indexes.each_with_index do |value, index|
+        index_differences << (value - indexes[index + 1]) unless value == indexes.last
+      end
     end
   end
 end
--- spec/validators/well_position_validator_spec.rb
+++ spec/validators/well_position_validator_spec.rb
@@ -75,7 +75,7 @@ RSpec.describe WellPositionValidator do
       end
 
       it 'B1, C1 filled' do
-        plate.wells = [build(:pacbio_well, row: 'B', column: '1'), build(:pacbio_well, row: 'C', column: '1'), build(:pacbio_well, row: 'D', column: '1')]
+        plate.wells = [build(:pacbio_well, row: 'B', column: '1'), build(:pacbio_well, row: 'C', column: '1')]
         described_class.new.validate(plate.run)
         expect(plate.run.errors).to be_empty
       end
@@ -87,50 +87,46 @@ RSpec.describe WellPositionValidator do
       end
 
       it 'C1, D1 filled' do
-        plate.wells = [build(:pacbio_well, row: 'C', column: '1'), build(:pacbio_well, row: 'D', column: '1'), build(:pacbio_well, row: 'D', column: '1')]
+        plate.wells = [build(:pacbio_well, row: 'C', column: '1'), build(:pacbio_well, row: 'D', column: '1')]
         described_class.new.validate(plate.run)
         expect(plate.run.errors).to be_empty
       end
 
-      WellPositionValidator::INVALID_COMBINATIONS.each do |combination|
-        let(:block_list) { combination.collect(&:chars) }
-
-        it combination.to_s do
-          block_list.each { |item| plate.wells << build(:pacbio_well, row: item[0], column: item[1]) }
-          described_class.new.validate(plate.run)
-          expect(plate.run.errors.full_messages.length).to eq(1)
-        end
+      it 'D1 filled' do
+        plate.wells = [build(:pacbio_well, row: 'D', column: '1')]
+        described_class.new.validate(plate.run)
+        expect(plate.run.errors).to be_empty
       end
 
-      # it 'A1 - empty, B1 - empty, C1 - filled, D1 - filled' do
-      #   plate.wells = [build(:pacbio_well, row: 'C', column: '1'), build(:pacbio_well, row: 'D', column: '1')]
-      #   described_class.new.validate(plate.run)
-      #   expect(plate.run.errors).to be_empty
-      # end
+      it 'A1, D1 filled' do
+        plate.wells = [build(:pacbio_well, row: 'A', column: '1'), build(:pacbio_well, row: 'D', column: '1')]
+        described_class.new.validate(plate.run)
+        expect(plate.run.errors.full_messages.length).to eq(1)
+      end
 
-      # it 'A1 - empty, B1 - filled, C1 - empty, D1 - empty' do
-      #   plate.wells = [build(:pacbio_well, row: 'B', column: '1')]
-      #   described_class.new.validate(plate.run)
-      #   expect(plate.run.errors.full_messages.length).to eq(1)
-      # end
+      it 'A1, C1 filled' do
+        plate.wells = [build(:pacbio_well, row: 'A', column: '1'), build(:pacbio_well, row: 'C', column: '1')]
+        described_class.new.validate(plate.run)
+        expect(plate.run.errors.full_messages.length).to eq(1)
+      end
 
-      # it 'A1 - filled, B1 - empty, C1 - filled, D1 - empty' do
-      #   plate.wells = [build(:pacbio_well, row: 'A', column: '1'), build(:pacbio_well, row: 'C', column: '1')]
-      #   described_class.new.validate(plate.run)
-      #   expect(plate.run.errors.full_messages.length).to eq(1)
-      # end
+      it 'B1, D1 filled' do
+        plate.wells = [build(:pacbio_well, row: 'B', column: '1'), build(:pacbio_well, row: 'D', column: '1')]
+        described_class.new.validate(plate.run)
+        expect(plate.run.errors.full_messages.length).to eq(1)
+      end
 
-      # it 'A1 - filled, B1 - empty, C1 - empty, D1 - filled' do
-      #   plate.wells = [build(:pacbio_well, row: 'A', column: '1'), build(:pacbio_well, row: 'D', column: '1')]
-      #   described_class.new.validate(plate.run)
-      #   expect(plate.run.errors.full_messages.length).to eq(1)
-      # end
+      it 'A1, C1, D1 filled' do
+        plate.wells = [build(:pacbio_well, row: 'A', column: '1'), build(:pacbio_well, row: 'C', column: '1'), build(:pacbio_well, row: 'D', column: '1')]
+        described_class.new.validate(plate.run)
+        expect(plate.run.errors.full_messages.length).to eq(1)
+      end
 
-      # it 'A1 - empty, B1 - filled, C1 - filled, D1 - filled' do
-      #   plate.wells = [build(:pacbio_well, row: 'B', column: '1'), build(:pacbio_well, row: 'C', column: '1'), build(:pacbio_well, row: 'D', column: '1')]
-      #   described_class.new.validate(plate.run)
-      #   expect(plate.run.errors.full_messages.length).to eq(1)
-      # end
+      it 'A1, B1, D1 filled' do
+        plate.wells = [build(:pacbio_well, row: 'A', column: '1'), build(:pacbio_well, row: 'B', column: '1'), build(:pacbio_well, row: 'D', column: '1')]
+        described_class.new.validate(plate.run)
+        expect(plate.run.errors.full_messages.length).to eq(1)
+      end
     end
   end
 end
