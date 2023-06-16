632ffce7725be2ec7f1e3b3d7a4f54f92e2c8b54
Stephen <519327+stevieing@users.noreply.github.com>
2023-06-16 15:28:14 +0100

    Added first test for instrument type validator.

--- app/validators/instrument_type_validator.rb
+++ app/validators/instrument_type_validator.rb
@@ -4,5 +4,23 @@
 class InstrumentTypeValidator < ActiveModel::Validator
   include ActiveModel::Validations
 
-  def validate(record); end
+  attr_reader :instrument_types, :instrument_type
+
+  def initialize(options)
+    super
+    @instrument_types = options[:instrument_types]
+  end
+
+  def validate(record)
+    @instrument_type = instrument_types.select do |_key, value|
+      value['name'] == record.system_name
+    end.values.first
+    validate_required_attributes(record, 'run')
+  end
+
+  def validate_required_attributes(record, model)
+    instrument_type[model]['required_attributes'].each do |attribute|
+      record.errors.add(attribute, "can't be blank") if record.send(attribute).blank?
+    end
+  end
 end
--- spec/validators/instrument_type_validator_spec.rb
+++ spec/validators/instrument_type_validator_spec.rb
@@ -2,16 +2,23 @@
 
 require 'rails_helper'
 
-# rubocop:disable RSpec/RepeatedExampleGroupBody, RSpec/ScatteredSetup
 RSpec.describe InstrumentTypeValidator do
-  describe.skip 'when the instrument type is Sequel IIe' do
+  let!(:instrument_types) { YAML.load_file(Rails.root.join('config/pacbio_instrument_types.yml'), aliases: true)[Rails.env]['instrument_types'] }
+
+  describe 'when the instrument type is Sequel IIe' do
     before do
       create(:pacbio_smrt_link_version, name: 'v11', default: true)
     end
 
     context 'run' do
       it 'required attributes' do
-        expect(true).to be_truthy
+        instrument_types['sequel_iie']['run']['required_attributes'].each do |attribute|
+          run = build(:pacbio_run, system_name: 'Sequel IIe')
+          run.send("#{attribute}=", nil)
+          instrument_type_validator = described_class.new(instrument_types:)
+          instrument_type_validator.validate(run)
+          expect(run.errors.messages[attribute]).to include("can't be blank")
+        end
       end
     end
 
@@ -46,4 +53,3 @@ RSpec.describe InstrumentTypeValidator do
     end
   end
 end
-# rubocop:enable RSpec/RepeatedExampleGroupBody, RSpec/ScatteredSetup
