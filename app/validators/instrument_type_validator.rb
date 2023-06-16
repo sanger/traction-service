b1b9ecfb19b2599a77fe65d83b3e21bd4d7ae0ed
Stephen <519327+stevieing@users.noreply.github.com>
2023-06-16 12:25:45 +0100

    Initial setup for pacbio instrument type validator. Added configuration and test skeleton.

---
+++ app/validators/instrument_type_validator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Validator for InstrumentType
+class InstrumentTypeValidator < ActiveModel::Validator
+  include ActiveModel::Validations
+
+  def validate(record); end
+end
