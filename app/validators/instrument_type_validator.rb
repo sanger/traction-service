c06e91e64764b9df5db530e9d44c023d8bf16524
Stephen <519327+stevieing@users.noreply.github.com>
2023-06-21 10:45:15 +0100

    Added comments and refactored instrument type validator.

--- app/validators/instrument_type_validator.rb
+++ app/validators/instrument_type_validator.rb
@@ -1,16 +1,26 @@
 # frozen_string_literal: true
 
 # Validator for InstrumentType
+# Validates the InstrumentType and its associated Plates and Wells
+# Validates the presence of required attributes
+# Validates the number of Plates and Wells
+# Validates the positions of Wells
+# Validates the combinations of Wells
+# TODO: Move the error messages to locale file
+# TODO: It functions but it is not pretty
 class InstrumentTypeValidator < ActiveModel::Validator
   include ActiveModel::Validations
 
   attr_reader :instrument_types, :instrument_type
 
+  # @param [Hash] options
+  # @option options [Hash] :instrument_types
   def initialize(options)
     super
     @instrument_types = options[:instrument_types].with_indifferent_access
   end
 
+  # @param [Run] record
   def validate(record)
     self.instrument_type = record
 
@@ -19,6 +29,9 @@ class InstrumentTypeValidator < ActiveModel::Validator
     validate_plates(record)
   end
 
+  # @param [Run] record
+  # validates the presence of required attributes
+  # validates the number of Wells
   def validate_plates(record)
     record.plates.each do |plate|
       validate_required_attributes(plate, instrument_type['plates'])
@@ -27,15 +40,24 @@ class InstrumentTypeValidator < ActiveModel::Validator
     end
   end
 
+  # @param [Plate] record
+  # @param [Hash] configuration
+  # validates the positions of Wells
+  # validates the combinations of Wells
   def validate_wells(record, configuration)
     well_positions = record.wells.filter do |well|
       !well.marked_for_destruction?
     end.collect(&:position).sort
-    
-    validate_well_positions(record, well_positions,configuration['positions'])
+
+    validate_well_positions(record, well_positions, configuration['positions'])
     validate_well_combinations(record, well_positions, configuration['combinations'])
   end
 
+  private
+
+  # @param [Run | Plate] record
+  # @param [Hash] configuration
+  # validates the presence of required attributes
   def validate_required_attributes(record, configuration)
     return if configuration['required_attributes'].blank?
 
@@ -44,6 +66,10 @@ class InstrumentTypeValidator < ActiveModel::Validator
     end
   end
 
+  # @param [Run | Plate] record
+  # @param [Symbol] labware_type
+  # @param [Hash] limits
+  # validates the number of Plates or Wells
   def validate_limits(record, labware_type, limits)
     if record.send(labware_type).length < limits[:minimum]
       record.errors.add(labware_type,
@@ -57,6 +83,9 @@ class InstrumentTypeValidator < ActiveModel::Validator
                                                                          labware_type)}")
   end
 
+  # @param [Plate] record
+  # @param [Hash] configuration
+  # validates the positions of Wells
   def validate_well_positions(record, well_positions, valid_positions)
     return if valid_positions.blank?
 
@@ -65,6 +94,9 @@ class InstrumentTypeValidator < ActiveModel::Validator
     record.errors.add(:wells, "must be in positions #{valid_positions}")
   end
 
+  # @param [Plate] record
+  # @param [Hash] configuration
+  # validates the combinations of Wells
   def validate_well_combinations(record, well_positions, valid_combinations)
     return if valid_combinations.blank?
 
@@ -73,6 +105,9 @@ class InstrumentTypeValidator < ActiveModel::Validator
     record.errors.add(:wells, 'must be in a valid order')
   end
 
+  # @param [Run] record
+  # sets the instrument_type
+  # @return [Hash]
   def instrument_type=(record)
     @instrument_type = instrument_types.select do |_key, value|
       value['name'] == record.system_name
@@ -80,6 +115,10 @@ class InstrumentTypeValidator < ActiveModel::Validator
   end
 
   # This needs to be moved to locale file
+  # @param [Integer] limit
+  # @param [String] text
+  # @return [String]
+  # returns the plural or singular form of a word
   def pluralize(limit, text)
     return text.to_s.pluralize if limit > 1
 
