8e6e2c76bb3760331a36fe5197de8fd324e05d35
Stephen <519327+stevieing@users.noreply.github.com>
2023-06-26 10:11:39 +0100

    Rubocopped.

--- app/validators/well_positions_validator.rb
+++ app/validators/well_positions_validator.rb
@@ -28,6 +28,7 @@ class WellPositionsValidator < ActiveModel::Validator
 
   def filtered(record)
     return record.wells unless exclude_marked_for_destruction
+
     record.wells.filter { |well| !well.marked_for_destruction? }
   end
 end
