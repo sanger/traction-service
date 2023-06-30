9f1660192d88b144ce30d153356b939b283f93c0
Ben Topping <ben.topping@outlook.com>
2023-06-30 15:12:21 +0100

    Added pacbio revio run plate validator to handle sequencing kit box barcode constraints

---
+++ app/validators/pacbio_revio_plate_validator.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# Failed validations return unprocessable_entity
+class PacbioRevioPlateValidator < ActiveModel::Validator
+    def validate(record)
+        # return unless the run is a Revio run
+        return unless record.run.present? && record.run.system_name == 'Revio'
+
+        existing_plates = Pacbio::Plate.where(sequencing_kit_box_barcode: record.sequencing_kit_box_barcode).where.not(id: record.id)
+
+        # return unless there are existing sequencing kit barcodes
+        return unless existing_plates.present?
+
+        # return if the sequencing kit barcode is already used on more than 1 plate
+        if existing_plates.count > 1
+            record.errors.add(:sequencing_kit_box_barcode, "has already been used on 2 plates")
+            return
+        end
+
+
+        # return if the positions have already been used
+        common_positions = existing_plates.map(&:wells).flatten.map(&:position) & record.wells.map(&:position)
+        if common_positions.any?
+            positions = common_positions.join(',')
+            record.errors.add(:wells, "#{positions} have already been used for sequencing kit box barcode #{record.sequencing_kit_box_barcode}")
+        end
+    end
+end
