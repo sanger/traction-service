fe9333ba09e9c872650b40a27b6a13d9feee3da1
Stephen Hulme <sh51@sanger.ac.uk>
2023-07-25 15:45:35 +0100

    fix creat tags

--- spec/lib/tasks/create_tags.rake_spec.rb
+++ spec/lib/tasks/create_tags.rake_spec.rb
@@ -5,11 +5,6 @@ require 'rails_helper'
 Rails.application.load_tasks
 
 RSpec.describe 'RakeTasks' do
-  before do
-    Pacbio::SmrtLinkVersion.find_by(name: 'v10') || create(:pacbio_smrt_link_version, name: 'v10', default: true)
-    Pacbio::SmrtLinkVersion.find_by(name: 'v11') || create(:pacbio_smrt_link_version, name: 'v11')
-    Pacbio::SmrtLinkVersion.find_by(name: 'v12_revio') || create(:pacbio_smrt_link_version, name: 'v12_revio')
-  end
 
   describe 'create tags' do
     it 'creates all of the pacbio tag sets' do
