b1fe10d4c3a4cdad3b94cbaee06692c303bfc5e9
Stephen Hulme <sh51@sanger.ac.uk>
2023-07-25 15:44:52 +0100

    fix ont instruments

--- spec/lib/tasks/create_ont_instruments.rake_spec.rb
+++ spec/lib/tasks/create_ont_instruments.rake_spec.rb
@@ -5,11 +5,6 @@ require 'rails_helper'
 Rails.application.load_tasks
 
 RSpec.describe 'RakeTasks' do
-  before do
-    Pacbio::SmrtLinkVersion.find_by(name: 'v10') || create(:pacbio_smrt_link_version, name: 'v10', default: true)
-    Pacbio::SmrtLinkVersion.find_by(name: 'v11') || create(:pacbio_smrt_link_version, name: 'v11')
-    Pacbio::SmrtLinkVersion.find_by(name: 'v12_revio') || create(:pacbio_smrt_link_version, name: 'v12_revio')
-  end
 
   describe 'ont_instruments:create' do
     it 'creates the correct instrument data' do
