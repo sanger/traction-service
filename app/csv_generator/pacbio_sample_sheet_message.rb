ecdcf9bcff9d061cc77bb825e3e63aa83c455603
Stephen Hulme <sh51@sanger.ac.uk>
2023-07-27 14:11:06 +0100

    wip: refactor CSV generation to use recursion

--- app/csv_generator/pacbio_sample_sheet_message.rb
+++ app/csv_generator/pacbio_sample_sheet_message.rb
@@ -8,25 +8,39 @@ class PacbioSampleSheetMessage < DataStructureBuilder
     configuration.column_order
   end
 
-  # Data Structure as csv
+  # Returns true if the object can be iterated using `each`,
+  # false otherwise
+  def eachable?(object)
+    object.respond_to?(:each)
+  end
+
+  # Flattens a nested hash of arrays into a 1D array.
+  # Includes non-iterable key-value pairs in the result.
+  def recursive_array_extractor(array_of_hashes)
+    rows = []
+    array_of_hashes.each do |hash_element|
+      rows << hash_element
+      hash_element.values.each do |values|
+        if eachable? values
+          child_rows = recursive_array_extractor(values)
+          rows.push(*child_rows)
+        end
+      end
+    end
+    rows
+  end
+
+  # Parse the JSON data structure to derive the CSV. If a
+  # value contains an array, the key name will be ignored
+  # and the individual array elements parsed.
   def payload
     CSV.generate do |csv|
       csv << csv_headers
-      # data_structure = {"sorted_wells"=>[{"Library Type"=>"Standard",
-      data_structure.values.each do |children|
-        # children = [{"Library Type"=>"Standard",
-        children.each do |child|
-          csv << child.values_at(*csv_headers)
-          # child = {"Library Type"=>"Standard",...,"samples"=>[{"Reagent Plate"=>1,
 
-          child.values.select { |v| v.respond_to?(:each) }.each do |grandchildren|
-            # grandchildren = [{"Reagent Plate"=>1,
-            grandchildren.each do |grandchild|
-              # grandchild = {"Reagent Plate"=>1,
-              csv << grandchild.values_at(*csv_headers)
-            end
-          end
-        end
+      data_rows = recursive_array_extractor([data_structure])
+      data_rows.each do |row|
+        row_values = row.values_at(*csv_headers)
+        csv << row_values unless row_values.all?(nil)
       end
     end
   end
