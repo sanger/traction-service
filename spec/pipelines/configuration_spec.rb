81bc457c3ec98faf3cf8b2acb5f2245d011eea8b
Stephen Hulme <sh51@sanger.ac.uk>
2023-08-09 09:13:01 +0100

    tests (configuration): confirm respect of version fields after loading

--- spec/pipelines/configuration_spec.rb
+++ spec/pipelines/configuration_spec.rb
@@ -143,5 +143,19 @@ RSpec.describe Pipelines::Configuration, type: :model do
         configuration.pipeline_c.sample_sheet.by_version('v10').field_20
       end
     end
+
+    it 'will not be able to access fields from another version without by_version' do
+      expect(configuration.pipeline_c.sample_sheet.v20.field_20).to eq('c')
+      assert_raises NoMethodError do
+        configuration.pipeline_c.sample_sheet.v10.field_20
+      end
+    end
+
+    it 'will not be able to access fields from another version once another version has been loaded' do
+      expect(configuration.pipeline_c.sample_sheet.by_version('v20').field_20).to eq('c')
+      assert_raises NoMethodError do
+        configuration.pipeline_c.sample_sheet.by_version('v10').field_20
+      end
+    end
   end
 end
