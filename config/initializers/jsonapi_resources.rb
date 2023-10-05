462af2a5da3f23209e64b190d1b15d6fcdbaad44
Ben Topping <ben.topping@outlook.com>
2023-10-05 14:16:25 +0100

    feat(api): json api resources return page count in response meta

--- config/initializers/jsonapi_resources.rb
+++ config/initializers/jsonapi_resources.rb
@@ -8,6 +8,8 @@ JSONAPI.configure do |config|
   # If we apply a filter that returns more than 1000 results some will be hidden
   # from the user. May need to address in future
   config.maximum_page_size = 1000
+  # Enables the requestor to know the total number of pages
+  config.top_level_meta_include_page_count = true
 
   # TODO: we have to set this to false for tests. Why?
   config.warn_on_missing_routes = false
