435686a0aa052622a5005a78dda173a7432fc7c3
Seena Nair <55585488+seenanair@users.noreply.github.com>
2023-10-27 18:03:30 +0100

    Set dna_control_complex_box_barcode as a required field for Sequel IIe v12

--- app/validators/required_attributes_validator.rb
+++ app/validators/required_attributes_validator.rb
@@ -3,12 +3,14 @@
 # Validator for required attributes
 # Validates the presence of required attributes
 class RequiredAttributesValidator < ActiveModel::Validator
-  attr_reader :required_attributes
+  attr_reader :required_attributes, :versions
 
   # @param [Hash] options
   # @option options [Array] :required_attributes
   def initialize(options)
     super
+
+    @versions = options[:versions]
     @required_attributes = options[:required_attributes]
   end
 
@@ -24,8 +26,12 @@ class RequiredAttributesValidator < ActiveModel::Validator
   # @param [ActiveRecord::Base] record
   # @param [Symbol] required_attribute
   def validate_required_attribute(record, required_attribute)
-    return if record.send(required_attribute).present?
+    if versions.present? && versions.exclude?(record.smrt_link_version.name)
+      nil
+    else
+      return if record.send(required_attribute).present?
 
-    record.errors.add(required_attribute, "can't be blank")
+      record.errors.add(required_attribute, "can't be blank")
+    end
   end
 end
