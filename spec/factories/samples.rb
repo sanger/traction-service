6f99f7b7e0e42d0b47d76b94a23748ade16ba6fc
Stuart McHattie <sm49@sanger.ac.uk>
2024-02-26 18:16:49 +0000

    Fix missing space in block

--- spec/factories/samples.rb
+++ spec/factories/samples.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     external_id
     species { 'human' }
     public_name { 'PublicName' }
-    genome_size { '987,654,321 bp'}
+    genome_size { '987,654,321 bp' }
     priority_level { 'Medium' }
     country_of_origin { 'United Kingdom' }
   end
