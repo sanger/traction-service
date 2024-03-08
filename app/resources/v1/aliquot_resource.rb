7766d8e6c7009f81b983086efe582db86bb027fc
Stephen <519327+stevieing@users.noreply.github.com>
2024-03-08 09:56:07 +0000

    Refactor AliquotResource to support polymorphic relationships in Pacbio pipeline

--- app/resources/v1/aliquot_resource.rb
+++ app/resources/v1/aliquot_resource.rb
@@ -9,18 +9,6 @@ module V1
     attributes :aliquot_type, :source_id, :source_type, :used_by_id, :used_by_type, :state,
                :volume, :concentration, :insert_size, :template_prep_kit_box_barcode, :tag_id
 
-    has_one :source, polymorphic: true,
-                     polymorphic_types: %w[pacbio/requests pacbio/pools pacbio/libraries]
-    has_one :used_by, polymorphic: true,
-                      polymorphic_types: %w[pacbio/requests pacbio/pools pacbio/libraries]
-
     has_one :tag, always_include_optional_linkage_data: true
-
-    # Aliquot polymorphism support
-    # This fixes the polymorphic relationships in json-api resources
-    # as json-api resources underscores and pluralizes the type
-    def self.resource_klass_for(type)
-      super(type.underscore.pluralize.gsub('pacbio/', ''))
-    end
   end
 end
--- app/resources/v1/pacbio/aliquot_resource.rb
+++ app/resources/v1/pacbio/aliquot_resource.rb
@@ -4,6 +4,20 @@ module V1
   module Pacbio
     # AliquotResource
     class AliquotResource < V1::AliquotResource
+      # We could move this to a concern and dynamically build the polymorphic types
+      # but no need to do this unless we use this for any other pipelines
+      # for now this is best in here as it is specific to pacbio
+      POLYMORPHIC_TYPES = %w[pacbio/requests pacbio/pools pacbio/libraries].freeze
+
+      has_one :source, polymorphic: true, polymorphic_types: POLYMORPHIC_TYPES
+      has_one :used_by, polymorphic: true, polymorphic_types: POLYMORPHIC_TYPES
+
+      #  # Aliquot polymorphism support
+      # # This fixes the polymorphic relationships in json-api resources
+      # # as json-api resources underscores and pluralizes the type
+      def self.resource_klass_for(type)
+        super(type.underscore.pluralize.gsub('pacbio/', ''))
+      end
     end
   end
 end
