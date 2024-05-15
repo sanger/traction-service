3fcee8d8a2950c91083988e1671a1a6ff89b0c68
Stephen Hulme <sh51@sanger.ac.uk>
2024-05-15 13:48:40 +0100

    fix: remove feature flag in code

--- app/models/pacbio/run.rb
+++ app/models/pacbio/run.rb
@@ -53,8 +53,7 @@ module Pacbio
     # using pipelines.yml configuration to generate data
     def generate_sample_sheet
       configuration = pacbio_run_sample_sheet_config
-      sample_sheet_class = if configuration.respond_to?(:sample_sheet_class) &&
-                              Flipper.enabled?(:new_format_sample_sheet)
+      sample_sheet_class = if configuration.respond_to?(:sample_sheet_class)
                              "RunCsv::#{configuration.sample_sheet_class}".constantize
                            else
                              RunCsv::PacbioSampleSheet
