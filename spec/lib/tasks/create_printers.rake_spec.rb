11517030febe6998afef9b2cb813b231a25a96d1
Stephen <519327+stevieing@users.noreply.github.com>
2024-05-21 10:09:09 +0100

    Refactor printers rake task and add destroy functionality

--- spec/lib/tasks/create_printers.rake_spec.rb
+++ spec/lib/tasks/create_printers.rake_spec.rb
@@ -7,11 +7,20 @@ require 'rake'
 Rails.application.load_tasks if Rake::Task.tasks.empty?
 
 RSpec.describe 'RakeTasks' do
-  describe 'printers:create' do
+  describe 'printers' do
     it 'creates the printers' do
       expect { Rake::Task['printers:create'].invoke }.to change(Printer, :count).by(6).and output(
         <<~HEREDOC
           -> Printers succesfully updated
+        HEREDOC
+      ).to_stdout
+    end
+
+    it 'destroys the printers' do
+      create_list(:printer, 6)
+      expect { Rake::Task['printers:destroy'].invoke }.to change(Printer, :count).by(-6).and output(
+        <<~HEREDOC
+          -> Printers succesfully deleted
         HEREDOC
       ).to_stdout
     end
