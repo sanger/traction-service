ced62da11c43a29f0dfa2eccb0ac0783cfdce6f5
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-07-16 14:23:16 +0100

    refactor: Improve Emq::PublishingJob to use message builder configuration

--- app/messages/emq/publishing_job.rb
+++ app/messages/emq/publishing_job.rb
@@ -8,6 +8,10 @@ module Emq
   class PublishingJob
     attr_reader :bunny_config
 
+    # The prefix for the key which contains the version of the Avro schema to use
+    # by the message builder
+    AVRO_SCHEMA_VERSION_KEY = 'avro_schema_version_'
+
     # Initialize the publishing job with the bunny configuration
     def initialize
       # Load the bunny configuration from the Rails configuration and convert it to an OpenStruct
@@ -16,11 +20,17 @@ module Emq
 
     # Publish a message to the EMQ
     # @param [Object] objects the object or objects to publish
-    # @param [Object] create_messsage_configuration the configuration to construct
-    #                                               the message to publish from the given object(s)
+    # @param [Object] the pipeline configuration to construct
+    #                 the message to publish from the given object(s)
     # @param [String] schema_key the key of the schema to validate the message against
-
-    def publish(objects, create_messsage_configuration, schema_key) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+    # Note:-
+    # The schema_key must exist within the subjects hash of the bunny configuration and
+    # must also have a matching configuration within the pipeline settings.
+    # (See the 'volume_tracking' section in config/pipelines/pacbio.yml for reference.)
+    # Any messages published using publishing_job require a corresponding entry in the
+    # pipeline configuration, identified by the schema key.
+    #
+    def publish(objects, message_config, schema_key) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
       # Check if the schema_key exists in the subjects hash and return early if it does not
       schema = bunny_config.amqp.schemas.subjects[schema_key]
       return if schema.nil?
@@ -30,22 +40,32 @@ module Emq
       version = bunny_config.amqp.schemas.subjects[schema_key].version
       return if subject.nil? || version.nil?
 
+      # Get the message builder configuration for the schema key and version
+      # and create a message builder class from the configuration
+      message_builder_config_obj = message_builder_config(message_config, schema_key, version)
+      if message_builder_config_obj.nil?
+        Rails.logger.error("Message builder configuration not found for schema key: #{schema_key} and version: #{version}") # rubocop:disable Layout/LineLength
+        return
+      end
+      message_builder_class = message_builder_config_obj.message_class.to_s.constantize
+
       # Create a validator and sender for the subject and version
-      validator = Emq::Validator.new(subject, version, bunny_config.amqp.schemas.registry_url)
+      encoder = Emq::Encoder.new(subject, version, bunny_config.amqp.schemas.registry_url)
       sender = Emq::Sender.new(bunny_config.amqp.isg, subject, version)
 
       # Publish each object to the EMQ
       Array(objects).each do |object|
         # Construct the message to publish from the object using the given configuration
-        message_object = Message::Message.new(object:,
-                                              configuration: create_messsage_configuration).content
+        message_object = message_builder_class.new(object:,
+                                                   configuration: message_builder_config_obj)
+                                              .content
 
         # check if the schema_key is present in the payload
         next if message_object[schema_key].nil?
 
         # Validate the message against the schema and send it to the EMQ
         publish_message = message_object[schema_key]
-        message = validator.validate_message(publish_message)
+        message = encoder.encode_message(publish_message)
         sender.send_message(message)
       end
     end
@@ -56,6 +76,26 @@ module Emq
       return obj unless obj.is_a?(Hash)
 
       OpenStruct.new(obj.transform_values { |val| deep_open_struct(val) }) # rubocop:disable Style/OpenStructUse
+    end
+
+    private
+
+    # Get the message builder configuration for the schema key and version
+    # @param [Object] message_config the pipeline configuration to get the message builder
+    #                                configuration from
+    # @param [String] schema_key the key of the schema to get the message builder configuration for
+    # @param [Integer] version the version of the schema to get the message builder configuration
+    # @return [OpenStruct | nil] the message builder configuration for the schema key and version
+    # the builder configuratin should be in the format:
+
+    def message_builder_config(message_config, schema_key, version)
+      children = message_config.public_send(schema_key)&.instance_variable_get(:@children)
+      return unless children
+
+      builder_config = children["#{AVRO_SCHEMA_VERSION_KEY}#{version}"]
+      return unless builder_config
+
+      OpenStruct.new(builder_config) # rubocop:disable Style/OpenStructUse
     end
   end
 end
