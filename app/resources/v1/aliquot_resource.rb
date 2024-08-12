9d239c2e1db5ffbf74c95853f6a0b911feb01200
Dasun Pubudumal <dp27@sanger.ac.uk>
2024-08-12 11:32:38 +0100

    Adding docs (1)

--- app/resources/v1/aliquot_resource.rb
+++ app/resources/v1/aliquot_resource.rb
@@ -16,6 +16,29 @@ module V1
   # of the JSON:API standard.
   class AliquotResource < JSONAPI::Resource
     model_name 'Aliquot'
+
+    # @!attribute [rw] aliquot_type
+    #   @return [String] the type of the aliquot
+    # @!attribute [rw] source_id
+    #   @return [Integer] the ID of the source
+    # @!attribute [rw] source_type
+    #   @return [String] the type of the source
+    # @!attribute [rw] used_by_id
+    #   @return [Integer] the ID of the entity that used the aliquot
+    # @!attribute [rw] used_by_type
+    #   @return [String] the type of the entity that used the aliquot
+    # @!attribute [rw] state
+    #   @return [String] the state of the aliquot
+    # @!attribute [rw] volume
+    #   @return [Float] the volume of the aliquot
+    # @!attribute [rw] concentration
+    #   @return [Float] the concentration of the aliquot
+    # @!attribute [rw] insert_size
+    #   @return [Integer] the insert size of the aliquot
+    # @!attribute [rw] template_prep_kit_box_barcode
+    #   @return [String] the barcode of the template prep kit box
+    # @!attribute [rw] tag_id
+    #   @return [Integer] the ID of the tag associated with the aliquot
     attributes :aliquot_type, :source_id, :source_type, :used_by_id, :used_by_type, :state,
                :volume, :concentration, :insert_size, :template_prep_kit_box_barcode, :tag_id
 
