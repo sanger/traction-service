cf1952ae371604f889457c4bf1b50713709af76e
Dasun Pubudumal <dp27@sanger.ac.uk>
2024-08-12 11:27:20 +0100

    Adding yardopts

--- app/resources/v1/aliquot_resource.rb
+++ app/resources/v1/aliquot_resource.rb
@@ -3,7 +3,17 @@
 # JSON-API resources wont resolve across namespaces spaces
 # so we need the Aliquot resource in the PacBio namespace
 module V1
-  # AliquotResource
+  # @todo This documentation does not yet include a detailed description of what this resource represents.
+  # @todo This documentation does not yet include detailed descriptions for relationships, attributes and filters.
+  # @todo This documentation does not yet include any example usage of the API via cURL or similar.
+  #
+  # @note Access this resource via the `/api/v1/aliquots/` endpoint.
+  #
+  # Provides a JSON:API representation of {Aliquot}.
+  #
+  # For more information about JSON:API see the [JSON:API Specifications](https://jsonapi.org/format/)
+  # or look at the [JSONAPI::Resources](http://jsonapi-resources.com/) package for the service implementation
+  # of the JSON:API standard.
   class AliquotResource < JSONAPI::Resource
     model_name 'Aliquot'
     attributes :aliquot_type, :source_id, :source_type, :used_by_id, :used_by_type, :state,
