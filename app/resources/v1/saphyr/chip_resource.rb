c86b06b986388e798b99b876f74ee42495280397
Dasun Pubudumal <dp27@sanger.ac.uk>
2024-08-13 14:36:45 +0100

    Documentation for Saphyr (old ones)

--- app/resources/v1/saphyr/chip_resource.rb
+++ app/resources/v1/saphyr/chip_resource.rb
@@ -2,10 +2,22 @@
 
 module V1
   module Saphyr
-    # ChipResource
+    # @todo This documentation does not yet include a detailed description of what this resource represents.
+    # @todo This documentation does not yet include detailed descriptions for relationships, attributes and filters.
+    # @todo This documentation does not yet include any example usage of the API via cURL or similar.
+    #
+    # @note Access this resource via the `/api/v1/saphyr/chip/` endpoint.
+    #
+    # Provides a JSON:API representation of {Saphyr::Chip}.
+    #
+    # For more information about JSON:API see the [JSON:API Specifications](https://jsonapi.org/format/)
+    # or look at the [JSONAPI::Resources](http://jsonapi-resources.com/) package
+    # for the service implementation of the JSON:API standard.
     class ChipResource < JSONAPI::Resource
       model_name 'Saphyr::Chip'
 
+      # @!attribute [rw] barcode
+      #   @return [String] the barcode of the chip
       attributes :barcode
 
       has_many :flowcells, foreign_key_on: :related
