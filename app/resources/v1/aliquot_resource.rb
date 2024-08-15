2458b65123d199a9d05e60a7a1dccf182a2f4959
Dasun Pubudumal <dp27@sanger.ac.uk>
2024-08-15 15:25:44 +0100

    [skip ci] chore: Update resource endpoint in aliquot_resource.rb

--- app/resources/v1/aliquot_resource.rb
+++ app/resources/v1/aliquot_resource.rb
@@ -7,7 +7,7 @@ module V1
   # @todo This documentation does not yet include detailed descriptions for relationships, attributes and filters.
   # @todo This documentation does not yet include any example usage of the API via cURL or similar.
   #
-  # @note Access this resource via the `/v1/aliquots/` endpoint.
+  # @note Access this resource via the `/v1/aliquot/` endpoint.
   #
   # Provides a JSON:API representation of {Aliquot}.
   #
