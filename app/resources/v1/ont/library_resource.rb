dcf4fef5260ff41c4581be34431683f8979e5f42
Dasun Pubudumal <dp27@sanger.ac.uk>
2024-08-15 15:41:10 +0100

    [skip ci] chore: Update resource endpoint in library_resource.rb

--- app/resources/v1/ont/library_resource.rb
+++ app/resources/v1/ont/library_resource.rb
@@ -6,7 +6,7 @@ module V1
     # @todo This documentation does not yet include detailed descriptions for relationships, attributes and filters.
     # @todo This documentation does not yet include any example usage of the API via cURL or similar.
     #
-    # @note Access this resource via the `/v1/ont/librarys/` endpoint.
+    # @note Access this resource via the `/v1/ont/libraries/` endpoint.
     #
     # Provides a JSON:API representation of {Ont::Library}.
     #
