ab71597f45a248ac67d06e5206acc622d09bd739
Dasun Pubudumal <dp27@sanger.ac.uk>
2024-08-19 14:16:46 +0100

    [skip ci] Update volume tracking documentation build script and directory structure

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-# Loop through each item in the 'documentation' directory
-for dir in documentation/*; do
-    # Check if the item is a directory
-    if [ -d "$dir" ]; then
-        echo "Building documentation in $dir"
-        
-        # Build the documentation using mkdocs with the configuration file in the current directory
-        mkdocs build -f $dir/mkdocs.yml
-        
-        # Get the base name of the directory (e.g., 'volume-tracking' from 'documentation/volume-tracking')
-        target_doc=$(basename "$dir")
-        
-        # Define the target directory where the built documentation will be copied
-        target_directory="doc/$target_doc"
-        
-        # Create the target directory if it doesn't exist
-        mkdir -p $target_directory
-        
-        # Copy the built site to the target directory
-        cp -r $dir/site/* $target_directory/
-    fi
-done
+# Check if the 'documentation' directory exists
+if [ -d "documentation" ]; then
+    # Loop through each item in the 'documentation' directory
+    for dir in documentation/*; do
+        # Check if the item is a directory
+        if [ -d "$dir" ]; then
+            echo "Building documentation in $dir"
+            
+            # Build the documentation using mkdocs with the configuration file in the current directory
+            mkdocs build -f $dir/mkdocs.yml
+            
+            # Get the base name of the directory (e.g., 'volume-tracking' from 'documentation/volume-tracking')
+            target_doc=$(basename "$dir")
+            
+            # Define the target directory where the built documentation will be copied
+            target_directory="doc/$target_doc"
+            
+            # Create the target directory if it doesn't exist
+            mkdir -p $target_directory
+            
+            # Copy the built site to the target directory
+            cp -r $dir/site/* $target_directory/
+        fi
+    done
+else
+    echo "The 'documentation' directory does not exist. Proceeding without building mkdocs documentation."
+    exit 0
+fi
