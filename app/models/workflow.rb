82d8c32e0acfb3d2ce057253913b4acbe727e5cf
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-09-10 16:46:49 +0100

    refactor: updates to Workflow model

--- app/models/workflow.rb
+++ app/models/workflow.rb
@@ -2,9 +2,9 @@
 
 # Workflow
 class Workflow < ApplicationRecord
-  include Pipelineable
+  include WorkflowPipelineable
   has_many :workflow_steps, dependent: :destroy
   accepts_nested_attributes_for :workflow_steps, allow_destroy: true
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
