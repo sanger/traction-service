99fac3fd8ce1d0b95cf6ee9dcd078a350288e0fc
Ben Topping <ben.topping@outlook.com>
2024-11-07 10:48:14 +0000

    feat(pacbio-library-batch): adds ability to create libraries through library batch

--- app/models/pacbio/library_batch.rb
+++ app/models/pacbio/library_batch.rb
@@ -7,5 +7,8 @@ module Pacbio
 
     has_many :libraries, class_name: 'Pacbio::Library', dependent: :nullify,
                          foreign_key: :pacbio_library_batch_id, inverse_of: :library_batch
+
+    # This allows the creation of libraries when creating a library batch
+    accepts_nested_attributes_for :libraries
   end
 end
--- spec/models/pacbio/library_batch_spec.rb
+++ spec/models/pacbio/library_batch_spec.rb
@@ -20,4 +20,32 @@ RSpec.describe Pacbio::LibraryBatch, :pacbio do
     library_batch.destroy
     expect(library.reload.library_batch).to be_nil
   end
+
+  describe 'creating nested libraries' do
+    define_negated_matcher :not_change, :change
+
+    let(:pacbio_requests_enum) { create_list(:pacbio_request, 8).cycle }
+    let(:pacbio_libraries) { build_list(:pacbio_library, 8, pacbio_request_id: pacbio_requests_enum.next.id) }
+    # Creates valid attributes for the libraries - including the primary aliquot attributes and a pacbio_request_id
+    let(:libraries_attributes) do
+      pacbio_libraries.map do |lib|
+        { **lib.attributes, primary_aliquot_attributes: lib.primary_aliquot.attributes, pacbio_request_id: lib.request.id }
+      end
+    end
+
+    it 'accepts nested attributes for libraries' do
+      library_batch = described_class.new(libraries_attributes: libraries_attributes)
+      expect { library_batch.save! }.to change(Pacbio::Library, :count).by(8)
+      expect(library_batch.libraries.count).to eq(8)
+    end
+
+    it 'does not save the library batch if a library is invalid' do
+      # Make the last library invalid
+      libraries_attributes.last[:pacbio_request_id] = nil
+
+      library_batch = described_class.new(libraries_attributes: libraries_attributes)
+      expect { library_batch.save! }.to raise_error(ActiveRecord::RecordInvalid).and not_change(Pacbio::Library, :count)
+      expect(library_batch.errors['libraries.request']).to include('must exist')
+    end
+  end
 end
