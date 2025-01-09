b411d5a5af68fe07c2a7ed85a2ca25cfd1e1aff2
Ben Topping <ben.topping@outlook.com>
2025-01-09 11:57:02 +0000

    feat(smrtlink): adds v25 SMRTLink config

--- app/exchanges/run_csv/pacbio_sample_sheet.rb
+++ app/exchanges/run_csv/pacbio_sample_sheet.rb
@@ -49,9 +49,10 @@ module RunCsv
         'Indexes'	=> well.barcode_set, # 244d96c6-f3b2-4997-5ae3-23ed33ab925f
         'Sample is indexed'	=> well.tagged?, # Set to True to Multiplex
         'Bio Sample Name' => well.tagged? ? nil : well.bio_sample_name,
-        'Use Adaptive Loading'	=> false, # this will likely be a well default in future
+        'Use Adaptive Loading'	=> well.use_adaptive_loading,
         'Consensus Mode'	=> 'molecule', # (default to molecule do we need a custom field)
-        'Same Barcodes on Both Ends of Sequence'	=> well.same_barcodes_on_both_ends_of_sequence
+        'Same Barcodes on Both Ends of Sequence'	=> well.same_barcodes_on_both_ends_of_sequence,
+        'Full Resolution Base Qual' => well.full_resolution_base_qual
       }
     end
 
