71ee72b7c3646574cef1acd467e574f5de854c65
Seena Nair <55585488+seenanair@users.noreply.github.com>
2025-02-06 17:16:52 +0000

    update(pacbio_sample_sheet): Add bio_sample_name method to utilize formatted_bio_sample_name for sample handling

--- app/exchanges/run_csv/pacbio_sample_sheet_v25.rb
+++ app/exchanges/run_csv/pacbio_sample_sheet_v25.rb
@@ -9,6 +9,11 @@ module RunCsv
     # Generate a hash of settings for a single cell
     # Overrides the method in the parent class
     # Only difference is removal of 'Polymerase Kit' key
+    #
+    def bio_sample_name(sample)
+      sample.formatted_bio_sample_name
+    end
+
     def generate_smrt_cell_settings(well) # rubocop:disable Metrics/MethodLength
       {
         'Well Name'	=> well.used_aliquots.first.source.tube.barcode, # TRAC-2-7242
