1604d26a12ecfe7182a572037833d48cf063394d
yoldas <ay6@sanger.ac.uk>
2025-11-09 10:03:13 +0000

    docs: update app/resources/v1/ont/tag_set_resource.rb

--- app/resources/v1/ont/tag_set_resource.rb
+++ app/resources/v1/ont/tag_set_resource.rb
@@ -2,12 +2,52 @@
 
 module V1
   module Ont
-    # @todo This documentation does not yet include a detailed description of what this resource represents.
-    # @todo This documentation does not yet include detailed descriptions for relationships, attributes and filters.
-    # @todo This documentation does not yet include any example usage of the API via cURL or similar.
+    # Provides a JSON:API representation of {Ont::TagSet} model.
+    #
+    # A tag set is a collection of unique oligo sequences known as tags or
+    # barcodes that are ligated to DNA or RNA samples during libray preparation
+    # to enable multiplexing, allowing multiple samples to be sequenced
+    # simultaneously on a single flowcell.
+    #
+    # Primary relationships:
+    # * tags {V1::Ont::TagResource} - The tags contained in the tag set.
     #
     # @note Access this resource via the `/v1/ont/tag_sets/` endpoint.
     #
+    # @example GET request to list all ONT tag sets
+    #   curl -X GET http://localhost:3100/v1/ont/tag_sets
+    #
+    # @example GET request to retrieve a specific ONT tag set by ID
+    #  curl -X GET "http://localhost:3100/v1/ont/tag_sets/1"
+    #
+    # @example POST request to create a new ONT tag set
+    #   curl -X POST http://localhost:3100/v1/ont/tag_sets \
+    #     -H "Content-Type: application/vnd.api+json" \
+    #     -H "Accept: application/vnd.api+json" \
+    #       -d '{
+    #         "data": {
+    #           "type": "tag_sets",
+    #           "attributes": {
+    #             "name": "New Tag Set"
+    #            }
+    #          }
+    #        }'
+    #
+    # @example PATCH request to update an existing ONT tag set with name change
+    #   curl -X PATCH http://localhost:3100/v1/ont/tag_sets/16 \
+    #     -H "Content-Type: application/vnd.api+json" \
+    #     -H "Accept: application/vnd.api+json" \
+    #     -d '{
+    #       "data": {
+    #         "type": "tag_sets",
+    #         "id": "16",
+    #         "attributes": {
+    #           "name": "Updated Tag Set Name"
+    #         }
+    #       }
+    #     }'
+    # @example DELETE request to remove an existing ONT tag set
+    #   curl -X DELETE http://localhost:3100/v1/ont/tag_sets/16
     #
     # For more information about JSON:API see the [JSON:API Specifications](https://jsonapi.org/format/)
     # or look at the [JSONAPI::Resources](http://jsonapi-resources.com/) package
