706d0132cace53442737d11d6fe90ac99ade5d3a
yoldas <ay6@sanger.ac.uk>
2025-11-09 10:03:10 +0000

    docs: update app/resources/v1/ont/instrument_resource.rb

--- app/resources/v1/ont/instrument_resource.rb
+++ app/resources/v1/ont/instrument_resource.rb
@@ -2,25 +2,35 @@
 
 module V1
   module Ont
-    # @todo This documentation does not yet include a detailed description of what this resource represents.
-    # @todo This documentation does not yet include detailed descriptions for relationships, attributes and filters.
-    # @todo This documentation does not yet include any example usage of the API via cURL or similar.
+    # rubocop:disable Layout/LineLength
+    # Provides a JSON:API representation of {Ont::Instrument}.
     #
+    # ONT Instruments are devices used for DNA or RNA sequencing by Oxford
+    # Nanopore Technologies (ONT). They use the same nanopore sequencing
+    # technology but differ in scale and throughput. The main difference is
+    # the number of flowcells they can run.
+    #
+    # @note This resource is immutable: its endpoint will not accept `POST`, `PATCH`, or `DELETE` requests.
     # @note Access this resource via the `/v1/ont/instruments/` endpoint.
     #
-    # Provides a JSON:API representation of {Ont::Instrument}.
+    # @example GET request to list all ONT instruments
+    #   curl -X GET http://localhost:3100/v1/ont/instruments
+    #
+    # @example GET request to retrieve a specific ONT instrument by ID
+    #  curl -X GET "http://localhost:3100/v1/ont/instruments/1"
     #
     # For more information about JSON:API see the [JSON:API Specifications](https://jsonapi.org/format/)
     # or look at the [JSONAPI::Resources](http://jsonapi-resources.com/) package
     # for the service implementation of the JSON:API standard.
+    # rubocop:enable Layout/LineLength
     class InstrumentResource < JSONAPI::Resource
       model_name 'Ont::Instrument'
 
-      # @!attribute [rw] name
+      # @!attribute [r] name
       #   @return [String] the name of the instrument
-      # @!attribute [rw] instrument_type
+      # @!attribute [r] instrument_type
       #   @return [String] the type of the instrument
-      # @!attribute [rw] max_number_of_flowcells
+      # @!attribute [r] max_number_of_flowcells
       #   @return [Integer] the maximum number of flowcells the instrument can handle
       attributes :name, :instrument_type, :max_number_of_flowcells
     end
