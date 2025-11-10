6856cd1115d4eeefed953f33f0e84df71025c966
Stephen <519327+stevieing@users.noreply.github.com>
2025-11-10 17:04:44 +0000

    docs: remove TODO comments and enhance documentation for PrinterResource and SampleResource
    Removed sample from routes

--- app/resources/v1/printer_resource.rb
+++ app/resources/v1/printer_resource.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 
 module V1
-  # @todo This documentation does not yet include a detailed description of what this resource represents.
-  # @todo This documentation does not yet include detailed descriptions for relationships, attributes and filters.
-  # @todo This documentation does not yet include any example usage of the API via cURL or similar.
   #
   # @note Access this resource via the `/v1/printer_types` endpoint.
   #
-  # Provides a JSON:API representation of {Printer} and exposes valid library type options
-  # for use by the UI.
+  # Provides a JSON:API representation of {Printer}
   #
   # For more information about JSON:API see the [JSON:API Specifications](https://jsonapi.org/format/)
   # or look at the [JSONAPI::Resources](http://jsonapi-resources.com/) package for the service
