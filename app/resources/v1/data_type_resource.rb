1f08e9e933300fdc7edbd5d93f56d80c83d4ca37
Stephen <519327+stevieing@users.noreply.github.com>
2025-11-10 16:05:18 +0000

    docs: remove TODO comments from AliquotResource and TagResource documentation

--- app/resources/v1/data_type_resource.rb
+++ app/resources/v1/data_type_resource.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 module V1
-  # @todo This documentation does not yet include a detailed description of what this resource represents.
-  # @todo This documentation does not yet include detailed descriptions for relationships, attributes and filters.
-  # @todo This documentation does not yet include any example usage of the API via cURL or similar.
   #
   # @note Access this resource via the `/v1/data_types` endpoint.
   #
