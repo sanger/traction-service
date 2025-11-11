8e62e9197f68ca7833967d9a0e07bc0078e4c122
yoldas <ay6@sanger.ac.uk>
2025-11-11 22:59:47 +0000

    docs: update app/resources/v1/ont/flowcell_resource.rb

--- app/resources/v1/ont/flowcell_resource.rb
+++ app/resources/v1/ont/flowcell_resource.rb
@@ -2,29 +2,21 @@
 
 module V1
   module Ont
-    # rubocop:disable Layout/LineLength
-    # Provides a JSON:API representation of {Ont::Flowcell} model.
+    # Provides a JSON:API representation of {Ont::Flowcell}.
     #
     # Flowcell is a cartridge containing a chip with nanopores used for DNA or
     # RNA sequencing by Oxford Nanopore Technologies (ONT). A flowcell is
     # associated with a sequencing run and a pool of samples to be sequenced.
     #
-    # Primary relationships:
+    ## Primary relationships:
     # * pool {V1::PoolResource} - The pool loaded onto the flowcell.
     #
-    # @note This resource is immutable: its endpoint will not accept `POST`, `PATCH`, or `DELETE` requests.
     # @note Access this resource via the `/v1/ont/flowcells/` endpoint.
     #
-    # @example GET request to list all ONT flowcells
+    # @example
     #   curl -X GET http://localhost:3100/v1/ont/flowcells
+    #   curl -X GET "http://localhost:3100/v1/ont/flowcells/1"
     #
-    # @example GET request to retrieve a specific ONT flowcell by ID
-    #  curl -X GET "http://localhost:3100/v1/ont/flowcells/1"
-    #
-    # For more information about JSON:API see the [JSON:API Specifications](https://jsonapi.org/format/)
-    # or look at the [JSONAPI::Resources](http://jsonapi-resources.com/) package
-    # for the service implementation of the JSON:API standard.
-    # rubocop:enable Layout/LineLength
     class FlowcellResource < JSONAPI::Resource
       model_name 'Ont::Flowcell'
 
