9e94ec46818f185a615587749043a5d549aec3e1
yoldas <ay6@sanger.ac.uk>
2025-11-11 22:59:52 +0000

    docs: update app/resources/v1/ont/tag_resource.rb

--- app/resources/v1/ont/tag_resource.rb
+++ app/resources/v1/ont/tag_resource.rb
@@ -2,24 +2,17 @@
 
 module V1
   module Ont
-    # Provides a JSON:API representation of {Ont::Tag} model.
+    # Provides a JSON:API representation of {Ont::Tag}.
     #
     # A tag is a short unique DNA sequence (oligo) in a tag set (barcode set).
     # A tag set can have a single group of tags or multiple groups of tags
     # (indexes). A tag can only belong to one group within a tag set.
     #
-    # @note Access to this resource is disabled. See
-    # @note Access this resource via the `/v1/ont/tags/` endpoint.
-    # @todo This documentation does not yet include any example usage of the API via cURL or similar.
+    ## Primary relationships:
+    # * tagset {V1::Ont::TagSetResource} - The tag set to which the tag belongs.
     #
     # @note Access this resource via the `/v1/ont/tags/` endpoint.
     #
-    # Provides a JSON:API representation of {Tag}. This is a resource to return all of the ONT
-    #  tags.
-    #
-    # For more information about JSON:API see the [JSON:API Specifications](https://jsonapi.org/format/)
-    # or look at the [JSONAPI::Resources](http://jsonapi-resources.com/) package
-    # for the service implementation of the JSON:API standard.
     class TagResource < JSONAPI::Resource
       model_name 'Tag'
 
