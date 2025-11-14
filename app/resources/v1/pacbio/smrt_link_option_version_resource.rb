551031be23c30d823dd767a45fde3c64913c94dc
yoldas <ay6@sanger.ac.uk>
2025-11-14 13:53:22 +0000

    docs: update app/resources/v1/pacbio/smrt_link_option_version_resource.rb

--- app/resources/v1/pacbio/smrt_link_option_version_resource.rb
+++ app/resources/v1/pacbio/smrt_link_option_version_resource.rb
@@ -2,21 +2,20 @@
 
 module V1
   module Pacbio
-    # @todo This documentation does not yet include a detailed description of what this resource represents.
-    # @todo This documentation does not yet include detailed descriptions for relationships, attributes and filters.
-    # @todo This documentation does not yet include any example usage of the API via cURL or similar.
+    # Provides a JSON:API representation of {Pacbio::SmrtLinkOptionVersion}.
+    #
+    # Each SMRT link version can have multiple options.
+    # Each SMRT Link Option can belong to multiple versions.
+    #
+    ## Primary relationships:
+    # * smrt_link_option {V1::Pacbio::SmrtLinkOptionResource} - The SMRT Link
+    #   option associated with this version.
     #
     # @note Access this resource via the `/v1/pacbio/smrt_link_option_versions/` endpoint.
     #
-    # Provides a JSON:API representation of {Pacbio::SmrtLinkOptionVersion}.
-    # Each SMRT link version can have multiple options.
-    #  Each SMRT Link Option can belong to multiple versions.
+    # @example
+    #   curl -X GET http://localhost:3000/v1/pacbio/smrt_link_versions/1?include=smrt_link_option_versions
     #
-    # For more information about JSON:API see the [JSON:API Specifications](https://jsonapi.org/format/)
-    # or look at the [JSONAPI::Resources](http://jsonapi-resources.com/) package
-    # for the service implementation of the JSON:API standard.
-    # SmrtLinkOptionVersionResource - each SMRT link version can have multiple options
-    # Each SMRT Linke Option can belong to multiple versions
     class SmrtLinkOptionVersionResource < JSONAPI::Resource
       model_name 'Pacbio::SmrtLinkOptionVersion'
 
