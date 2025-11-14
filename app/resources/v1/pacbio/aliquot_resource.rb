a64480d369c0990b1e6832290a02c5e2db1af6f1
yoldas <ay6@sanger.ac.uk>
2025-11-14 14:43:27 +0000

    docs: set port app/resources/v1/pacbio/aliquot_resource.rb

--- app/resources/v1/pacbio/aliquot_resource.rb
+++ app/resources/v1/pacbio/aliquot_resource.rb
@@ -4,17 +4,8 @@ module V1
   module Pacbio
     # Provides a JSON:API representation of {Pacbio::Aliquot}.
     #
-    # @todo This documentation does not yet include a detailed description of what this resource represents.
-    # @todo This documentation does not yet include detailed descriptions for relationships, attributes and filters.
-    # @todo This documentation does not yet include any example usage of the API via cURL or similar.
-    #
     # @note Access this resource via the `/v1/pacbio/aliquots/` endpoint.
     #
-    # Provides a JSON:API representation of {Aliquot}.
-    #
-    # For more information about JSON:API see the [JSON:API Specifications](https://jsonapi.org/format/)
-    # or look at the [JSONAPI::Resources](http://jsonapi-resources.com/) package
-    # for the service implementation of the JSON:API standard.
     class AliquotResource < V1::AliquotResource
       include Shared::RunSuitability
 
