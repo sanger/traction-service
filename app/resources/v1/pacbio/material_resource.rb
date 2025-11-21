92fc1c82c9aba76c4af71c83c4b500bb2fa20a39
Stephen <519327+stevieing@users.noreply.github.com>
2025-11-21 10:27:47 +0000

    docs: update MaterialResource documentation to clarify access and usage

--- app/resources/v1/pacbio/material_resource.rb
+++ app/resources/v1/pacbio/material_resource.rb
@@ -2,16 +2,9 @@
 
 module V1
   module Pacbio
-    # @todo This documentation does not yet include a detailed description of what this resource represents.
-    # @todo This documentation does not yet include detailed descriptions for relationships, attributes and filters.
-    # @todo This documentation does not yet include any example usage of the API via cURL or similar.
-    #
-    # @note Access this resource via the `/v1/pacbio/material/` endpoint.
-    #
-    #
-    # For more information about JSON:API see the [JSON:API Specifications](https://jsonapi.org/format/)
-    # or look at the [JSONAPI::Resources](http://jsonapi-resources.com/) package
-    # for the service implementation of the JSON:API standard.
+    # @note There is no access to the material resource via the Pacbio API at this time.
+    # It is used via the tube resource.
+    # @see {V1::Pacbio::TubeResource}
     class MaterialResource < JSONAPI::Resource
     end
   end
