c27a2f521c5c63eff82750def93775f5f870d29c
yoldas <ay6@sanger.ac.uk>
2025-12-04 04:09:10 +0000

    docs; no direct access for app/resources/v1/pacbio/well_resource.rb

--- app/resources/v1/pacbio/well_resource.rb
+++ app/resources/v1/pacbio/well_resource.rb
@@ -22,15 +22,8 @@ module V1
     # * requests {V1::Pacbio::RequestResource}
     # * plate {V1::Pacbio::PlateResource}
     #
-    # @note Access this resource via the `/v1/pacbio/wells/` endpoint.
-    #
-    # @example
-    #   curl -X GET http://localhost:3100/v1/pacbio/wells/1
-    #   curl -X GET http://localhost:3100/v1/pacbio/wells
-    #   curl -X GET http://localhost:3100/v1/pacbio/runs/1/wells
-    #   curl -X GET http://localhost:3100/v1/pacbio/runs/1/wells/1
-    #
-    #   https://localhost:3100/v1/pacbio/v1/wells/1?include=plate,materials
+    # @note This resource cannot be accessed via the `/v1/pacbio/wells/` endpoint.
+    # It is accessible via the nested route under {V1::Pacbio::Plate} using includes.
     #
     class WellResource < JSONAPI::Resource
       model_name '::Well'
