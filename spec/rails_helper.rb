521ce5d3d71c8d57f0fb8a98f5e9526e2343bc52
Ben Topping <ben.topping@outlook.com>
2026-01-22 09:03:11 +0000

    style: fix rspec/output errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,8 +32,7 @@ require 'factory_bot_rails'
 # If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
+rescue ActiveRecord::PendingMigrationError
   exit 1
 end
 RSpec.configure do |config|
