537ed032bb2be6938333a803c6f32174001dee25
yoldas <ay6@sanger.ac.uk>
2026-04-27 00:58:52 +0100

    test(auth): rename feature variables for clarity

--- spec/controllers/concerns/authenticator_spec.rb
+++ spec/controllers/concerns/authenticator_spec.rb
@@ -3,8 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Authenticator Flipper feature flagging', type: :controller do
-  let(:feature) { :y25_662_enable_request_authentication }
+  let(:feature_flag) { :y25_662_enable_request_authentication }
   let(:feature_reject_unauthenticated) { :y25_662_reject_unauthenticated_requests }
+  let(:feature_okta) { :y25_661_enable_okta_authentication }
   let(:api_application) { create(:api_application) }
 
   # Dummy controller for testing
@@ -32,14 +33,14 @@ RSpec.describe 'Authenticator Flipper feature flagging', type: :controller do
   end
 
   it 'skips authentication when Flipper flag is disabled' do
-    allow(Flipper).to receive(:enabled?).with(feature).and_return(false)
+    allow(Flipper).to receive(:enabled?).with(feature_flag).and_return(false)
     get :index
     expect(response.body).to eq('ok')
     expect(response).to have_http_status(:ok)
   end
 
   it 'calls authentication when Flipper flag is enabled' do
-    allow(Flipper).to receive(:enabled?).with(feature).and_return(true)
+    allow(Flipper).to receive(:enabled?).with(feature_flag).and_return(true)
     # No auth headers, so should be unauthorized
     get :index
     expect(response).to have_http_status(:unauthorized)
@@ -47,7 +48,7 @@ RSpec.describe 'Authenticator Flipper feature flagging', type: :controller do
 
   context 'API key authentication' do
     it 'authenticates a valid API key for GET requests' do
-      allow(Flipper).to receive(:enabled?).with(feature).and_return(true)
+      allow(Flipper).to receive(:enabled?).with(feature_flag).and_return(true)
       issued = api_application.rotate_api_key!
 
       request.headers['X-Traction-Client-Id'] = issued[:plaintext_key]
@@ -61,7 +62,7 @@ RSpec.describe 'Authenticator Flipper feature flagging', type: :controller do
     end
 
     it 'rejects malformed API keys' do
-      allow(Flipper).to receive(:enabled?).with(feature).and_return(true)
+      allow(Flipper).to receive(:enabled?).with(feature_flag).and_return(true)
 
       request.headers['X-Traction-Client-Id'] = 'bad-format-key'
       get :index
@@ -70,7 +71,7 @@ RSpec.describe 'Authenticator Flipper feature flagging', type: :controller do
     end
 
     it 'permits non-GET requests authenticated by API key' do
-      allow(Flipper).to receive(:enabled?).with(feature).and_return(true)
+      allow(Flipper).to receive(:enabled?).with(feature_flag).and_return(true)
       issued = api_application.rotate_api_key!
 
       request.headers['X-Traction-Client-Id'] = issued[:plaintext_key]
@@ -80,7 +81,7 @@ RSpec.describe 'Authenticator Flipper feature flagging', type: :controller do
     end
 
     it 'authenticates a grace-period API key and sets a deprecation warning header' do
-      allow(Flipper).to receive(:enabled?).with(feature).and_return(true)
+      allow(Flipper).to receive(:enabled?).with(feature_flag).and_return(true)
       # Issue a key then rotate so the first key moves to grace_period
       issued = api_application.rotate_api_key!
       api_application.rotate_api_key!
@@ -95,7 +96,7 @@ RSpec.describe 'Authenticator Flipper feature flagging', type: :controller do
     end
 
     it 'rejects an expired API key' do
-      allow(Flipper).to receive(:enabled?).with(feature).and_return(true)
+      allow(Flipper).to receive(:enabled?).with(feature_flag).and_return(true)
       # Two rotations push the original key to expired
       issued = api_application.rotate_api_key!
       api_application.rotate_api_key!
@@ -108,7 +109,7 @@ RSpec.describe 'Authenticator Flipper feature flagging', type: :controller do
     end
 
     it 'rejects an active API key when expires_at has passed' do
-      allow(Flipper).to receive(:enabled?).with(feature).and_return(true)
+      allow(Flipper).to receive(:enabled?).with(feature_flag).and_return(true)
       issued = api_application.rotate_api_key!
       issued[:api_key].update!(expires_at: 1.minute.ago)
 
@@ -119,7 +120,7 @@ RSpec.describe 'Authenticator Flipper feature flagging', type: :controller do
     end
 
     it 'permits non-expiring API keys' do
-      allow(Flipper).to receive(:enabled?).with(feature).and_return(true)
+      allow(Flipper).to receive(:enabled?).with(feature_flag).and_return(true)
       issued = api_application.rotate_api_key!(expires_at: nil)
 
       request.headers['X-Traction-Client-Id'] = issued[:plaintext_key]
@@ -131,7 +132,7 @@ RSpec.describe 'Authenticator Flipper feature flagging', type: :controller do
 
   context 'unauthenticated requests' do
     it 'logs unauthenticated requests' do
-      allow(Flipper).to receive(:enabled?).with(feature).and_return(true)
+      allow(Flipper).to receive(:enabled?).with(feature_flag).and_return(true)
       allow(Flipper).to receive(:enabled?).with(feature_reject_unauthenticated).and_return(false)
 
       expect(Rails.logger).to receive(:warn).with(match(/\[AUTH\] Unauthenticated request/))
@@ -139,7 +140,7 @@ RSpec.describe 'Authenticator Flipper feature flagging', type: :controller do
     end
 
     it 'allows unauthenticated requests when reject flag is OFF' do
-      allow(Flipper).to receive(:enabled?).with(feature).and_return(true)
+      allow(Flipper).to receive(:enabled?).with(feature_flag).and_return(true)
       allow(Flipper).to receive(:enabled?).with(feature_reject_unauthenticated).and_return(false)
 
       get :index
@@ -148,7 +149,7 @@ RSpec.describe 'Authenticator Flipper feature flagging', type: :controller do
     end
 
     it 'rejects unauthenticated requests when reject flag is ON' do
-      allow(Flipper).to receive(:enabled?).with(feature).and_return(true)
+      allow(Flipper).to receive(:enabled?).with(feature_flag).and_return(true)
       allow(Flipper).to receive(:enabled?).with(feature_reject_unauthenticated).and_return(true)
 
       get :index
@@ -156,7 +157,7 @@ RSpec.describe 'Authenticator Flipper feature flagging', type: :controller do
     end
 
     it 'includes method, path, ip, and request_id in unauthenticated log' do
-      allow(Flipper).to receive(:enabled?).with(feature).and_return(true)
+      allow(Flipper).to receive(:enabled?).with(feature_flag).and_return(true)
       allow(Flipper).to receive(:enabled?).with(feature_reject_unauthenticated).and_return(false)
 
       expect(Rails.logger).to receive(:warn) do |message|
@@ -171,27 +172,25 @@ RSpec.describe 'Authenticator Flipper feature flagging', type: :controller do
   end
 
   context 'bearer token authentication' do
-    let(:okta_flag) { :y25_661_enable_okta_authentication }
-
     it 'bypasses bearer authentication when Okta flag is disabled' do
-      allow(Flipper).to receive(:enabled?).with(feature).and_return(true)
-      allow(Flipper).to receive(:enabled?).with(okta_flag).and_return(false)
+      allow(Flipper).to receive(:enabled?).with(feature_flag).and_return(true)
+      allow(Flipper).to receive(:enabled?).with(feature_okta).and_return(false)
       request.headers['Authorization'] = 'Bearer any-token'
       post :create
       expect(response).to have_http_status(:ok)
     end
 
     it 'enforces bearer authentication when Okta flag is enabled and token is invalid' do
-      allow(Flipper).to receive(:enabled?).with(feature).and_return(true)
-      allow(Flipper).to receive(:enabled?).with(okta_flag).and_return(true)
+      allow(Flipper).to receive(:enabled?).with(feature_flag).and_return(true)
+      allow(Flipper).to receive(:enabled?).with(feature_okta).and_return(true)
       request.headers['Authorization'] = 'Bearer '
       post :create
       expect(response).to have_http_status(:unauthorized)
     end
 
     it 'permits bearer-authenticated non-GET requests when Okta flag is enabled and token is present' do
-      allow(Flipper).to receive(:enabled?).with(feature).and_return(true)
-      allow(Flipper).to receive(:enabled?).with(okta_flag).and_return(true)
+      allow(Flipper).to receive(:enabled?).with(feature_flag).and_return(true)
+      allow(Flipper).to receive(:enabled?).with(feature_okta).and_return(true)
       request.headers['Authorization'] = 'Bearer valid-token'
       post :create
       expect(response).to have_http_status(:ok)
